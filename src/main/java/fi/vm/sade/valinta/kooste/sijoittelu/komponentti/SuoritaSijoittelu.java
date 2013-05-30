d428def0f82a76eaee514ed6c5e908142ba8836a
kkammone <kari.kammonen@gofore.com>
2013-05-30 16:06:48 +0300

    rajapintojen refaktorointi - done

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/SuoritaSijoittelu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/SuoritaSijoittelu.java
@@ -1,8 +1,8 @@
 package fi.vm.sade.valinta.kooste.sijoittelu.komponentti;
 
-import fi.vm.sade.service.sijoittelu.schema.TarjontaTyyppi;
-import fi.vm.sade.service.sijoittelu.types.SijoitteleTyyppi;
-import fi.vm.sade.tarjonta.service.types.HakuTyyppi;
+
+import fi.vm.sade.service.valintatiedot.schema.HakuTyyppi;
+
 import org.apache.camel.language.Simple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,15 +28,10 @@ public class SuoritaSijoittelu {
     public void haeLahtotiedot(@Simple("${property.hakuOid}") String hakuOid) {
 
         LOG.info("KOOSTEPALVELU: Haetaan valintatiedot haulle {}", new Object[] {hakuOid});
-        List<fi.vm.sade.service.valintatiedot.schema.HakukohdeTyyppi> hakukohteet = valintatietoService.haeValintatiedot(hakuOid);
+        HakuTyyppi ht = valintatietoService.haeValintatiedot(hakuOid);
         LOG.info("Haettu valinnan tulokset");
-
-        SijoitteleTyyppi sijoittelu = new SijoitteleTyyppi();
-        sijoittelu.setTarjonta( new TarjontaTyyppi());
-        sijoittelu.getTarjonta().getHakukohde().addAll(hakukohteet);
-        sijoittelu.getTarjonta().setHaku(new HakuTyyppi());
-        sijoittelu.getTarjonta().getHaku().setOid(hakuOid);
-        sijoitteluService.sijoittele(sijoittelu);
+        sijoitteluService.sijoittele(ht);
+        LOG.info("Viety sijoittelulle valinnan tulokset");
     }
 }
 
