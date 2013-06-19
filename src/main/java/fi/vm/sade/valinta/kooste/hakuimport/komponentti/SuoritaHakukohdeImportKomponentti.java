e4d8997214d0a4ce81c9671529956ce6a5f0a8e8
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-06-19 15:47:22 +0300

    Merge branch 'master' of https://github.com/Opetushallitus/valintaperusteet
    Conflicts:
    	valintalaskentakoostepalvelu/src/main/java/fi/vm/sade/valinta/kooste/hakuimport/komponentti/HakukohdeDTO.java
    	valintaperusteet/valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/service/impl/HakukohdeImportServiceImpl.java

--- src/main/java/fi/vm/sade/valinta/kooste/hakuimport/komponentti/SuoritaHakukohdeImportKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/hakuimport/komponentti/SuoritaHakukohdeImportKomponentti.java
@@ -1,24 +1,14 @@
 package fi.vm.sade.valinta.kooste.hakuimport.komponentti;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 import fi.vm.sade.service.valintaperusteet.ValintaperusteService;
 import fi.vm.sade.service.valintaperusteet.schema.HakukohdeImportTyyppi;
 import fi.vm.sade.service.valintaperusteet.schema.HakukohdekoodiTyyppi;
 import fi.vm.sade.service.valintaperusteet.schema.HakukohteenValintakoeTyyppi;
 import fi.vm.sade.service.valintaperusteet.schema.MonikielinenTekstiTyyppi;
-import fi.vm.sade.tarjonta.service.types.HakukohdeTyyppi;
 import org.apache.camel.language.Simple;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Type;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * User: wuoti
@@ -38,22 +28,22 @@ public class SuoritaHakukohdeImportKomponentti {
 
         HakukohdeImportTyyppi importTyyppi = new HakukohdeImportTyyppi();
         Gson gson = new Gson();
-        HakukohdeDTO hakukohde = gson.fromJson(hakukohdeNimi,HakukohdeDTO.class);
+        HakukohdeDTO hakukohde = gson.fromJson(hakukohdeNimi, HakukohdeDTO.class);
 
-        for(String s : hakukohde.getTarjoajaNimi().keySet()) {
+        for (String s : hakukohde.getTarjoajaNimi().keySet()) {
             MonikielinenTekstiTyyppi m = new MonikielinenTekstiTyyppi();
             m.setLang(s);
             m.setText(hakukohde.getTarjoajaNimi().get(s));
             importTyyppi.getTarjoajaNimi().add(m);
         }
-        for(String s : hakukohde.getHakukohdeNimi().keySet()) {
+        for (String s : hakukohde.getHakukohdeNimi().keySet()) {
             MonikielinenTekstiTyyppi m = new MonikielinenTekstiTyyppi();
             m.setLang(s);
             m.setText(hakukohde.getHakukohdeNimi().get(s));
             importTyyppi.getHakukohdeNimi().add(m);
         }
 
-        for(String s : hakukohde.getHakuKausi().keySet()) {
+        for (String s : hakukohde.getHakuKausi().keySet()) {
             MonikielinenTekstiTyyppi m = new MonikielinenTekstiTyyppi();
             m.setLang(s);
             m.setText(hakukohde.getHakuKausi().get(s));
@@ -62,9 +52,9 @@ public class SuoritaHakukohdeImportKomponentti {
 
         importTyyppi.setHakuVuosi(hakukohde.getHakuVuosi());
 
-        hakukohde = gson.fromJson(hakukohdeData,HakukohdeDTO.class);
+        hakukohde = gson.fromJson(hakukohdeData, HakukohdeDTO.class);
 
-        for(String s : hakukohde.getOpetuskielet()) {
+        for (String s : hakukohde.getOpetuskielet()) {
             importTyyppi.getOpetuskielet().add(s);
         }
 
@@ -76,14 +66,14 @@ public class SuoritaHakukohdeImportKomponentti {
         importTyyppi.setHakuOid(hakukohde.getHakuOid());
         importTyyppi.setValinnanAloituspaikat(hakukohde.getValintojenAloituspaikatLkm());
 
-        for(ValintakoeDTO valinakoe : hakukohde.getValintakoes()) {
+        for (ValintakoeDTO valinakoe : hakukohde.getValintakoes()) {
             HakukohteenValintakoeTyyppi v = new HakukohteenValintakoeTyyppi();
             v.setOid(valinakoe.getOid());
             v.setTyyppiUri(valinakoe.getTyyppiUri());
             importTyyppi.getValintakoe().add(v);
         }
 
-       valintaperusteService.tuoHakukohde(importTyyppi);
+        valintaperusteService.tuoHakukohde(importTyyppi);
 
     }
 
