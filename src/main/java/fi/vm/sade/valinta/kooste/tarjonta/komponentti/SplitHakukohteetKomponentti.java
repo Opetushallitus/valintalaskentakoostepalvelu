f41a773e169fbb45e63961eec3d24f057edb23e1
jussijartamo <jussi@jartamo@gofore.com>
2013-08-13 16:39:43 +0300

    splitteri ei tarvi listaa. collection riittää.

--- src/main/java/fi/vm/sade/valinta/kooste/tarjonta/komponentti/SplitHakukohteetKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/tarjonta/komponentti/SplitHakukohteetKomponentti.java
@@ -1,20 +1,20 @@
 package fi.vm.sade.valinta.kooste.tarjonta.komponentti;
 
-import fi.vm.sade.tarjonta.service.types.HakukohdeTyyppi;
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+import fi.vm.sade.tarjonta.service.types.HakukohdeTyyppi;
+
 /**
- * User: wuoti
- * Date: 20.5.2013
- * Time: 13.11
+ * User: wuoti Date: 20.5.2013 Time: 13.11
  */
 @Component("splitHakukohteetKomponentti")
 public class SplitHakukohteetKomponentti {
 
-    public List<String> splitHakukohteet(List<HakukohdeTyyppi> hakukohteet) {
+    public List<String> splitHakukohteet(Collection<HakukohdeTyyppi> hakukohteet) {
         List<String> oids = new ArrayList<String>();
 
         for (HakukohdeTyyppi hk : hakukohteet) {
