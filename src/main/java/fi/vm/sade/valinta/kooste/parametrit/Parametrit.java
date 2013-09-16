9cd47293debf0ddaca2ef1521ff0178599143fdf
Tommi Hännikkälä <tommi.hannikkala@siili.fi>
2013-09-16 13:33:55 +0300

    Poistettu käyttämättömät parametrit.

--- src/main/java/fi/vm/sade/valinta/kooste/parametrit/Parametrit.java
+++ src/main/java/fi/vm/sade/valinta/kooste/parametrit/Parametrit.java
@@ -6,57 +6,18 @@ package fi.vm.sade.valinta.kooste.parametrit;
  * Time: 2:35 PM
  */
 public class Parametrit {
-    private long valintakoekutsutAlkupvm;
+    /*private long valintakoekutsutAlkupvm;
     private long valintakoekutsutLoppupvm;
     private long valintakoekutsutkouluillePvm;
     private long valintakoeAlkupvm;
-    private long valintakoeLoppupvm;
+    private long valintakoeLoppupvm;*/
     private long koetuloksetPvm;
-    private long valintajasijoitteluAlkupvm;
-    private long valintajasijoitteluLoppupvm;
+    /*private long valintajasijoitteluAlkupvm;
+    private long valintajasijoitteluLoppupvm;*/
     private long valintaesitysPvm;
     private long hakuAlkupvm;
     private long hakuLoppupvm;
 
-    public long getValintakoekutsutAlkupvm() {
-        return valintakoekutsutAlkupvm;
-    }
-
-    public void setValintakoekutsutAlkupvm(long valintakoekutsutAlkupvm) {
-        this.valintakoekutsutAlkupvm = valintakoekutsutAlkupvm;
-    }
-
-    public long getValintakoekutsutLoppupvm() {
-        return valintakoekutsutLoppupvm;
-    }
-
-    public void setValintakoekutsutLoppupvm(long valintakoekutsutLoppupvm) {
-        this.valintakoekutsutLoppupvm = valintakoekutsutLoppupvm;
-    }
-
-    public long getValintakoekutsutkouluillePvm() {
-        return valintakoekutsutkouluillePvm;
-    }
-
-    public void setValintakoekutsutkouluillePvm(long valintakoekutsutkouluillePvm) {
-        this.valintakoekutsutkouluillePvm = valintakoekutsutkouluillePvm;
-    }
-
-    public long getValintakoeAlkupvm() {
-        return valintakoeAlkupvm;
-    }
-
-    public void setValintakoeAlkupvm(long valintakoeAlkupvm) {
-        this.valintakoeAlkupvm = valintakoeAlkupvm;
-    }
-
-    public long getValintakoeLoppupvm() {
-        return valintakoeLoppupvm;
-    }
-
-    public void setValintakoeLoppupvm(long valintakoeLoppupvm) {
-        this.valintakoeLoppupvm = valintakoeLoppupvm;
-    }
 
     public long getKoetuloksetPvm() {
         return koetuloksetPvm;
@@ -64,22 +25,6 @@ public class Parametrit {
 
     public void setKoetuloksetPvm(long koetuloksetPvm) {
         this.koetuloksetPvm = koetuloksetPvm;
-    }
-
-    public long getValintajasijoitteluAlkupvm() {
-        return valintajasijoitteluAlkupvm;
-    }
-
-    public void setValintajasijoitteluAlkupvm(long valintajasijoitteluAlkupvm) {
-        this.valintajasijoitteluAlkupvm = valintajasijoitteluAlkupvm;
-    }
-
-    public long getValintajasijoitteluLoppupvm() {
-        return valintajasijoitteluLoppupvm;
-    }
-
-    public void setValintajasijoitteluLoppupvm(long valintajasijoitteluLoppupvm) {
-        this.valintajasijoitteluLoppupvm = valintajasijoitteluLoppupvm;
     }
 
     public long getValintaesitysPvm() {
