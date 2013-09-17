3e6412aae784c3d6db04d540508f6635bf3d69ed
jussijartamo <jussi@jartamo@gofore.com>
2013-09-17 14:27:33 +0300

    sijoittelun oma excel export poistettu ja tulos wsdl

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/SijoitteluAktivointiResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/SijoitteluAktivointiResource.java
@@ -1,21 +1,22 @@
 package fi.vm.sade.valinta.kooste.sijoittelu;
 
-import fi.vm.sade.valinta.kooste.parametrit.service.ParametriService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import fi.vm.sade.valinta.kooste.parametrit.service.ParametriService;
 
 /**
  *
  */
 @Controller
-@Path("sijoittelu")
+@Path("koostesijoittelu")
 public class SijoitteluAktivointiResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(SijoitteluAktivointiResource.class);
@@ -29,11 +30,11 @@ public class SijoitteluAktivointiResource {
     @GET
     @Path("aktivoi")
     public String aktivoiSijoittelu(@QueryParam("hakuOid") String hakuOid) {
-        if(!parametriService.valinnanhallintaEnabled(hakuOid)) {
+        if (!parametriService.valinnanhallintaEnabled(hakuOid)) {
             return "no privileges.";
         }
 
-        if(StringUtils.isBlank(hakuOid)) {
+        if (StringUtils.isBlank(hakuOid)) {
             return "get parameter 'hakuOid' required";
         } else {
             LOG.info("aktivoiSijoittelu haulle {}", hakuOid);
