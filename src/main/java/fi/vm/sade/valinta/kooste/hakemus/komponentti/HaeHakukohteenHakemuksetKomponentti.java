d04ef154a34a50b9bbc11f7b9798c417c423e869
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-10-09 12:52:12 +0300

    Haetaan vain aktiiviset ja puutteelliset hakemukset koostepalvelussa

--- src/main/java/fi/vm/sade/valinta/kooste/hakemus/komponentti/HaeHakukohteenHakemuksetKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/hakemus/komponentti/HaeHakukohteenHakemuksetKomponentti.java
@@ -1,7 +1,9 @@
 package fi.vm.sade.valinta.kooste.hakemus.komponentti;
 
-import java.util.List;
-
+import fi.vm.sade.valinta.kooste.exception.HakemuspalveluException;
+import fi.vm.sade.valinta.kooste.external.resource.haku.ApplicationResource;
+import fi.vm.sade.valinta.kooste.external.resource.haku.dto.HakemusList;
+import fi.vm.sade.valinta.kooste.external.resource.haku.dto.SuppeaHakemus;
 import org.apache.camel.Property;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,10 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import fi.vm.sade.valinta.kooste.exception.HakemuspalveluException;
-import fi.vm.sade.valinta.kooste.external.resource.haku.ApplicationResource;
-import fi.vm.sade.valinta.kooste.external.resource.haku.dto.HakemusList;
-import fi.vm.sade.valinta.kooste.external.resource.haku.dto.SuppeaHakemus;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * User: wuoti Date: 9.9.2013 Time: 9.05
@@ -22,6 +22,9 @@ import fi.vm.sade.valinta.kooste.external.resource.haku.dto.SuppeaHakemus;
 public class HaeHakukohteenHakemuksetKomponentti {
     private static final Logger LOG = LoggerFactory.getLogger(HaeHakukohteenHakemuksetKomponentti.class);
 
+    public static final String ACTIVE = "ACTIVE";
+    public static final String INCOMPLETE = "INCOMPLETE";
+
     @Autowired
     private ApplicationResource applicationResource;
     @Value("${valintalaskentakoostepalvelu.hakemus.rest.url}")
@@ -29,9 +32,9 @@ public class HaeHakukohteenHakemuksetKomponentti {
 
     public List<SuppeaHakemus> haeHakukohteenHakemukset(@Property("hakukohdeOid") String hakukohdeOid) {
         assert (SecurityContextHolder.getContext().getAuthentication() != null);
-        LOG.info("Haetaan HakemusList osoitteesta {}/applications?aoOid={}&start=0&rows={}", new Object[] {
-                applicationResourceUrl, hakukohdeOid, Integer.MAX_VALUE });
-        HakemusList hakemusList = applicationResource.findApplications(null, null, null, null, null, hakukohdeOid, 0,
+        LOG.info("Haetaan HakemusList osoitteesta {}/applications?aoOid={}&start=0&rows={}", new Object[]{
+                applicationResourceUrl, hakukohdeOid, Integer.MAX_VALUE});
+        HakemusList hakemusList = applicationResource.findApplications(null, Arrays.asList(ACTIVE, INCOMPLETE), null, null, null, hakukohdeOid, 0,
                 Integer.MAX_VALUE);
         if (hakemusList == null || hakemusList.getResults() == null) {
             throw new HakemuspalveluException("Hakemuspalvelu ei palauttanut hakemuksia hakukohteelle " + hakukohdeOid);
