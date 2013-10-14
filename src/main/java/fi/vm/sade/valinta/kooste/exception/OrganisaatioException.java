e070b54ad603cfbe13fa98402e5459edfdd6ad24
jussijartamo <jussi@jartamo@gofore.com>
2013-10-14 10:31:44 +0300

    kela-raportit

---
+++ src/main/java/fi/vm/sade/valinta/kooste/exception/OrganisaatioException.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.valinta.kooste.exception;
+
+public class OrganisaatioException extends RuntimeException {
+
+    public OrganisaatioException(String message) {
+        super(message);
+    }
+
+    private static final long serialVersionUID = 756088374667057661L;
+}
