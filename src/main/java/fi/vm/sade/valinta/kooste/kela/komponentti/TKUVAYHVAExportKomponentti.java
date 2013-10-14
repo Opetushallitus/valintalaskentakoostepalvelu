8a6072b56290ad4e7b4f45fbc2c7e4c4abc9dee0
jussijartamo <jussi@jartamo@gofore.com>
2013-10-14 10:31:44 +0300

    kela-raportit

--- src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/TKUVAYHVAExportKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/TKUVAYHVAExportKomponentti.java
@@ -6,18 +6,23 @@ import static fi.vm.sade.valinta.kooste.external.resource.haku.ApplicationResour
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Deque;
 import java.util.List;
 
 import org.apache.camel.Property;
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import fi.vm.sade.organisaatio.api.model.types.OrganisaatioDTO;
+import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVAALKU;
+import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVALOPPU;
 import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVAYHVA;
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakutoiveDTO;
@@ -50,18 +55,40 @@ public class TKUVAYHVAExportKomponentti {
             throw new SijoittelupalveluException(
                     "Haku ei sisällä koulutuspaikallisia hakijoita! Tarkista että sijoittelu on suoritettu haulle!");
         }
-
         String linjakoodi = "000";
-        List<InputStream> streams = new ArrayList<InputStream>();
+        Deque<InputStream> streams = new ArrayDeque<InputStream>();
         for (HakijaDTO hakija : hakijat) {
             for (HakutoiveDTO hakutoive : hakija.getHakutoiveet()) {
                 for (HakutoiveenValintatapajonoDTO valintatapajono : hakutoive.getHakutoiveenValintatapajonot()) {
                     if (HYVAKSYTTY.equals(valintatapajono.getTila())) {
                         TKUVAYHVA.Builder builder = new TKUVAYHVA.Builder();
                         // organisaatioService.findByOid(arg0)
-
-                        builder.setOppilaitos(organisaatioProxy.haeOrganisaatio(hakutoive.getTarjoajaOid())
-                                .getYhteishaunKoulukoodi());
+                        try {
+                            OrganisaatioDTO organisaatio = organisaatioProxy
+                                    .haeOrganisaatio(hakutoive.getTarjoajaOid());
+                            if (organisaatio == null) {
+                                // new
+                                // OrganisaatioException("Organisaatio ei palauttanut yhteishaun koulukoodia!");
+                                LOG.error("Yhteishaun koulukoodia ei voitu hakea organisaatiolle {}",
+                                        hakutoive.getTarjoajaOid());
+                                builder.setOppilaitos("0000");
+                            } else {
+                                if (organisaatio.getYhteishaunKoulukoodi() == null) {
+                                    // new
+                                    // OrganisaatioException("Organisaatio ei palauttanut yhteishaun koulukoodia!");
+                                    LOG.error("Yhteishaun koulukoodia ei voitu hakea organisaatiolle {}",
+                                            hakutoive.getTarjoajaOid());
+                                    builder.setOppilaitos("0000");
+                                } else {
+                                    builder.setOppilaitos(organisaatio.getYhteishaunKoulukoodi());
+                                }
+                            }
+                        } catch (Exception e) {
+                            LOG.error("Yhteishaun koulukoodia ei voitu hakea organisaatiolle {}",
+                                    hakutoive.getTarjoajaOid());
+                            // OrganisaatioException("Organisaatio ei palauttanut yhteishaun koulukoodia!");
+                            builder.setOppilaitos("0000");
+                        }
 
                         builder.setLinjakoodi(linjakoodi);
                         builder.setValintapaivamaara(new Date()); // TODO:
@@ -86,7 +113,7 @@ public class TKUVAYHVAExportKomponentti {
                         } catch (Exception e) {
                             LOG.error("Henkilötunnuksen hakeminen hakemuspalvelulta hakemukselle {} epäonnistui!",
                                     hakija.getHakemusOid());
-                            e.printStackTrace();
+                            // e.printStackTrace();
                             builder.setHenkilotunnus("XXXXXXXXXX");
                         }
                         builder.setLukuvuosi(lukuvuosi);
@@ -105,6 +132,11 @@ public class TKUVAYHVAExportKomponentti {
             }
 
         }
+        Integer count = streams.size();
+        streams.addFirst(new ByteArrayInputStream(new TKUVAALKU.Builder().setAjopaivamaara(new Date())
+                .setAineistonnimi(StringUtils.EMPTY).setOrganisaationimi(StringUtils.EMPTY).build().toByteArray()));
+        streams.addLast(new ByteArrayInputStream(new TKUVALOPPU.Builder().setAjopaivamaara(new Date())
+                .setTietuelukumaara(count).build().toByteArray()));
         return new SequenceInputStream(Collections.enumeration(streams));
     }
 }
