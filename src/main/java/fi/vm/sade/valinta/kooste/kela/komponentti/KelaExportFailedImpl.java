933fecd97e30e8570e5ac2b056b16eaf8f06442e
jussijartamo <jussi@jartamo@gofore.com>
2013-10-16 17:02:08 +0300

    kela-ftp-export vaihtoehto lisätty

---
+++ src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/KelaExportFailedImpl.java
@@ -0,0 +1,23 @@
+package fi.vm.sade.valinta.kooste.kela.komponentti;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fi.vm.sade.valinta.kooste.kela.KelaCache;
+import fi.vm.sade.valinta.kooste.kela.dto.KelaCacheDocument;
+
+@Component("failedKelaExport")
+public class KelaExportFailedImpl {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KelaExportFailedImpl.class);
+
+    @Autowired
+    private KelaCache kelaCache;
+
+    public void failedKelaExport() {
+        LOG.error("Kela export epäonnistui!");
+        kelaCache.addDocument(KelaCacheDocument.createErrorMessage("Kela-tiedoston luonti epäonnistui!"));
+    }
+}
---
+++ src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/KelaExportKomponentti.java
@@ -0,0 +1,54 @@
+package fi.vm.sade.valinta.kooste.kela.komponentti;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Deque;
+import java.util.List;
+
+import org.apache.camel.Body;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVAALKU;
+import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVALOPPU;
+import fi.vm.sade.rajapinnat.kela.tkuva.data.TKUVAYHVA;
+import fi.vm.sade.rajapinnat.kela.tkuva.util.KelaUtil;
+import fi.vm.sade.valinta.kooste.kela.KelaCache;
+import fi.vm.sade.valinta.kooste.kela.dto.KelaCacheDocument;
+
+@Component("kelaExportKomponentti")
+public class KelaExportKomponentti {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KelaExportKomponentti.class);
+
+    @Autowired
+    private KelaCache kelaCache;
+
+    public void exportKela(@Body List<TKUVAYHVA> data) throws IOException {
+        int count = data.size();
+        LOG.info("KELA-dokumentin luonti {} tietueelle", count);
+
+        Deque<InputStream> streams = new ArrayDeque<InputStream>();
+        for (TKUVAYHVA t : data) {
+            streams.add(new ByteArrayInputStream(t.toByteArray()));
+        }
+        streams.addFirst(new ByteArrayInputStream(new TKUVAALKU.Builder().setAjopaivamaara(new Date())
+                .setAineistonnimi(StringUtils.EMPTY).setOrganisaationimi(StringUtils.EMPTY).build().toByteArray()));
+        streams.addLast(new ByteArrayInputStream(new TKUVALOPPU.Builder().setAjopaivamaara(new Date())
+                .setTietuelukumaara(count).build().toByteArray()));
+        InputStream input = new SequenceInputStream(Collections.enumeration(streams));
+
+        kelaCache.addDocument(KelaCacheDocument.createFile(KelaUtil.createTiedostoNimiYhva14(new Date()), count,
+                IOUtils.toByteArray(input)));
+        LOG.info("Palautetaan onnistuneesti luotu KELA-tiedosto");
+    }
+}
