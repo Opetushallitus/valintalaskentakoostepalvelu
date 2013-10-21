29b868a276c29966742e78b8ae6da96a57122b34
jussijartamo <jussi@jartamo@gofore.com>
2013-10-21 13:40:59 +0300

    testit

--- src/test/java/fi/vm/sade/valinta/kooste/test/HaunValintalaskentaReititysTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/test/HaunValintalaskentaReititysTest.java
@@ -29,7 +29,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import fi.vm.sade.organisaatio.api.model.OrganisaatioService;
 import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
 import fi.vm.sade.service.valintalaskenta.ValintalaskentaService;
 import fi.vm.sade.service.valintaperusteet.ValintaperusteService;
@@ -46,6 +45,7 @@ import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.HakemusList;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.SuppeaHakemus;
 import fi.vm.sade.valinta.kooste.parametrit.service.ParametriService;
+import fi.vm.sade.valinta.kooste.tarjonta.OrganisaatioResource;
 import fi.vm.sade.valinta.kooste.valintalaskenta.ValintalaskentaAktivointiResource;
 
 /**
@@ -159,8 +159,8 @@ public class HaunValintalaskentaReititysTest {
     }
 
     @Bean
-    public OrganisaatioService getOrganisaatioServiceMock() {
-        return mock(OrganisaatioService.class);
+    public OrganisaatioResource getOrganisaatioResourceMock() {
+        return mock(OrganisaatioResource.class);
     }
 
     @Autowired
