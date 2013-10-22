53737f137124a0f8945d31258c18b749e55021de
jussijartamo <jussi@jartamo@gofore.com>
2013-10-22 13:33:30 +0300

    puuttuvat tiedostot

---
+++ src/main/java/fi/vm/sade/valinta/kooste/exception/KoodistoException.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.valinta.kooste.exception;
+
+public class KoodistoException extends RuntimeException {
+
+    public KoodistoException(String message) {
+        super(message);
+    }
+
+    private static final long serialVersionUID = 8624744448286430225L;
+}
