5192c2810bcf6d7bc0dc9090d23fc6d6b74317b4
jussijartamo <jussi@jartamo@gofore.com>
2013-10-24 12:27:18 +0300

    haetaan alkamisvuosi tarjonnalta

---
+++ src/main/java/fi/vm/sade/valinta/kooste/tarjonta/komponentti/HaeHakuTarjonnaltaKomponentti.java
@@ -0,0 +1,20 @@
+package fi.vm.sade.valinta.kooste.tarjonta.komponentti;
+
+import org.apache.camel.Property;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fi.vm.sade.tarjonta.service.resources.HakuResource;
+import fi.vm.sade.tarjonta.service.resources.dto.HakuDTO;
+
+@Component("hakuTarjonnaltaKomponentti")
+public class HaeHakuTarjonnaltaKomponentti {
+
+    @Autowired
+    private HakuResource hakuResource;
+
+    public HakuDTO getHaku(@Property("hakuOid") String hakuOid) {
+        HakuDTO haku = hakuResource.getByOID(hakuOid);
+        return haku;
+    }
+}
