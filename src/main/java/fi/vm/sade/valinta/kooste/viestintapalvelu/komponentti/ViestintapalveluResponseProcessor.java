da70e4d6b0a34873be1ec24585b1833112e99347
jussijartamo <jussi@jartamo@gofore.com>
2013-10-25 11:33:39 +0300

    error loggaus viestintapalvelun paluuviestiin

--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/komponentti/ViestintapalveluResponseProcessor.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/komponentti/ViestintapalveluResponseProcessor.java
@@ -4,12 +4,16 @@ import javax.ws.rs.core.Response;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import fi.vm.sade.valinta.kooste.exception.ViestintapalveluException;
 
 @Component("viestintapalveluProcessor")
 public class ViestintapalveluResponseProcessor implements Processor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ViestintapalveluResponseProcessor.class);
 
     @Override
     public void process(Exchange exchange) throws Exception {
@@ -19,6 +23,9 @@ public class ViestintapalveluResponseProcessor implements Processor {
                 throw new ViestintapalveluException("Sinulla ei ole käyttöoikeuksia viestintäpalveluun!");
             }
             if (response.getStatus() != Response.Status.ACCEPTED.getStatusCode()) {
+                LOG.error("Response {}, \r\n{}, \r\n{}, \r\n{}",
+                        new Object[] { response.getStatus(), response.getEntity(), response.getMetadata(), response });
+
                 throw new ViestintapalveluException("Viestintäpalvelu epäonnistui (status " + response.getStatus()
                         + ") osoitetarrojen luonnissa. Yritä uudelleen tai ota yhteyttä ylläpitoon!");
             }
