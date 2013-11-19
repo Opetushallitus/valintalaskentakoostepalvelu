07c3de1516845078a50a09fab241bd86b7126d5a
kkammone <kari.kammonen@gofore.com>
2013-11-19 10:08:19 +0200

    kelan vaatima paikan vastaanottaneet muutos

---
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/SijoitteluKaikkiPaikanVastaanottaneetKomponentti.java
@@ -0,0 +1,25 @@
+package fi.vm.sade.valinta.kooste.sijoittelu.komponentti;
+
+import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
+import fi.vm.sade.sijoittelu.tulos.resource.SijoitteluResource;
+import org.apache.camel.Property;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component("sijoitteluKaikkiPaikanVastaanottaneetKomponentti")
+public class SijoitteluKaikkiPaikanVastaanottaneetKomponentti {
+
+    @Autowired
+    private SijoitteluResource sijoitteluResource;
+
+    @Value("${valintalaskentakoostepalvelu.sijoittelu.rest.url}")
+    private String sijoitteluResourceUrl;
+
+    public Collection<HakijaDTO> vastaanottaneet(@Property("hakuOid") String hakuOid) {
+        return sijoitteluResource.vastaanottaneet(hakuOid, SijoitteluResource.LATEST);
+    }
+
+}
