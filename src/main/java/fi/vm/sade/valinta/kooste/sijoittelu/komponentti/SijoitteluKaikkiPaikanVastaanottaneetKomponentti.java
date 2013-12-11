b1070cb19c7f337b4bea0e5376c6b6448197b2e2
kkammone <kari.kammonen@gofore.com>
2013-12-11 15:27:39 +0200

    korjattu testit

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/SijoitteluKaikkiPaikanVastaanottaneetKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/SijoitteluKaikkiPaikanVastaanottaneetKomponentti.java
@@ -1,15 +1,14 @@
 package fi.vm.sade.valinta.kooste.sijoittelu.komponentti;
 
-import java.util.Collection;
-
+import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
+import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaPaginationObject;
+import fi.vm.sade.sijoittelu.tulos.resource.SijoitteluResource;
 import org.apache.camel.Property;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
-import fi.vm.sade.sijoittelu.tulos.dto.raportointi.PaginationObject;
-import fi.vm.sade.sijoittelu.tulos.resource.SijoitteluResource;
+import java.util.Collection;
 
 @Component("sijoitteluKaikkiPaikanVastaanottaneetKomponentti")
 public class SijoitteluKaikkiPaikanVastaanottaneetKomponentti implements SijoitteluKaikkiPaikanVastaanottaneet {
@@ -21,7 +20,7 @@ public class SijoitteluKaikkiPaikanVastaanottaneetKomponentti implements Sijoitt
     private String sijoitteluResourceUrl;
 
     public Collection<HakijaDTO> vastaanottaneet(@Property("hakuOid") String hakuOid) {
-        final PaginationObject<HakijaDTO> result = sijoitteluResource.hakemukset(hakuOid, SijoitteluResource.LATEST,
+        final HakijaPaginationObject result = sijoitteluResource.hakemukset(hakuOid, SijoitteluResource.LATEST,
                 null, null, true, null, null, null);
         return result.getResults();
     }
