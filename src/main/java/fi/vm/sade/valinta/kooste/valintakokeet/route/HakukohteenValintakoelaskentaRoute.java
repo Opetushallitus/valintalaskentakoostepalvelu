ad689e382eff3ac0f5968e6469bcc11710d4ca6b
jussijartamo <jussi@jartamo@gofore.com>
2014-01-02 11:50:37 +0200

    VT-672 async kutsu valintakoelaskentojen aloittamiselle. Pidetään sync operaatio yksikkötestejä varten, eli reitin aloittava säie voi viedä operaation loppuun.

--- src/main/java/fi/vm/sade/valinta/kooste/valintakokeet/route/HakukohteenValintakoelaskentaRoute.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintakokeet/route/HakukohteenValintakoelaskentaRoute.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.valinta.kooste.valintakokeet.route;
 
+import java.util.concurrent.Future;
+
 import org.apache.camel.Property;
 
 import fi.vm.sade.valinta.kooste.OPH;
@@ -11,4 +13,6 @@ public interface HakukohteenValintakoelaskentaRoute {
     final String DIRECT_HAKUKOHTEEN_VALINTAKOELASKENTA = "direct:kaynnistaHakukohteenValintakoelaskentaReitti";
 
     void aktivoiValintakoelaskenta(@Property(OPH.HAKUKOHDEOID) String hakukohdeOid);
+
+    Future<Void> aktivoiValintakoelaskentaAsync(@Property(OPH.HAKUKOHDEOID) String hakukohdeOid);
 }
