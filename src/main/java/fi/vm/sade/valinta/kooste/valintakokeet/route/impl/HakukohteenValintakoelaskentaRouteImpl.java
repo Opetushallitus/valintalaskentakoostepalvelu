7577ba1384e366e9fb9093b26ac2570130ec11ce
jussijartamo <jussi@jartamo@gofore.com>
2014-01-15 16:59:23 +0200

    loggaus valvomoon myös yksittäisessä hakukohde valintakoelaskennassa

--- src/main/java/fi/vm/sade/valinta/kooste/valintakokeet/route/impl/HakukohteenValintakoelaskentaRouteImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintakokeet/route/impl/HakukohteenValintakoelaskentaRouteImpl.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.valinta.kooste.valintakokeet.route.impl;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,8 @@ import fi.vm.sade.valinta.kooste.hakemus.komponentti.HakemusOidSplitter;
 import fi.vm.sade.valinta.kooste.security.SecurityPreprocessor;
 import fi.vm.sade.valinta.kooste.valintakokeet.komponentti.LaskeValintakoeosallistumisetHakemukselleKomponentti;
 import fi.vm.sade.valinta.kooste.valintakokeet.route.HakukohteenValintakoelaskentaRoute;
+import fi.vm.sade.valinta.kooste.valvomo.dto.Prosessi;
+import fi.vm.sade.valinta.kooste.valvomo.service.ValvomoAdminService;
 
 /**
  * 
@@ -27,9 +31,21 @@ public class HakukohteenValintakoelaskentaRouteImpl extends SpringRouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        from(hakukohteenValintakoelaskentaDeadLetterChannel())
+                .setHeader(
+                        "message",
+                        simple("[${property.authentication.name}] Valintakoelaskenta ei toimi. Hakukohteelle  ${property.hakukohdeOid},  ${property.hakemusOid}, haku ${property.hakuOid}"))
+                .to(fail());
+
         from(hakukohteenValintakoelaskenta())
         //
+                .errorHandler(deadLetterChannel(hakukohteenValintakoelaskentaDeadLetterChannel()))
+                //
                 .bean(new SecurityPreprocessor())
+                //
+                .process(luoProsessiHakukohteenValintakoelaskennalle())
+                //
+                .to(start())
                 //
                 .bean(haeHakukohteenHakemuksetKomponentti)
                 //
@@ -41,10 +57,47 @@ public class HakukohteenValintakoelaskentaRouteImpl extends SpringRouteBuilder {
                 //
                 .bean(laskeValintakoeosallistumisetHakemukselleKomponentti)
                 //
-                .end();
+                .end()
+                //
+                .to(finish());
     }
 
     private String hakukohteenValintakoelaskenta() {
         return HakukohteenValintakoelaskentaRoute.DIRECT_HAKUKOHTEEN_VALINTAKOELASKENTA;
+    }
+
+    private Processor luoProsessiHakukohteenValintakoelaskennalle() {
+        return new Processor() {
+            public void process(Exchange exchange) throws Exception {
+                String hakukohdeOid = exchange.getProperty(OPH.HAKUKOHDEOID, String.class);
+                exchange.setProperty(kuvaus(), "Valintakoelaskenta hakukohteelle " + hakukohdeOid);
+                exchange.setProperty(prosessi(), new Prosessi("Valintakoelaskenta", "Hakukohteelle " + hakukohdeOid,
+                        exchange.getProperty(OPH.HAKUOID, String.class)));
+            }
+        };
+    }
+
+    private static String kuvaus() {
+        return ValvomoAdminService.PROPERTY_VALVOMO_PROSESSIKUVAUS;
+    }
+
+    private static String prosessi() {
+        return ValvomoAdminService.PROPERTY_VALVOMO_PROSESSI;
+    }
+
+    private static String fail() {
+        return "bean:haunValintakoelaskentaValvomo?method=fail";
+    }
+
+    private static String start() {
+        return "bean:haunValintakoelaskentaValvomo?method=start";
+    }
+
+    private static String finish() {
+        return "bean:haunValintakoelaskentaValvomo?method=finish";
+    }
+
+    private static String hakukohteenValintakoelaskentaDeadLetterChannel() {
+        return "direct:kaynnistaHakukohteenValintakoelaskentaReitti_deadletterchannel";
     }
 }
