c8f951899f28c2f388ecaac161559dc6a722019e
jussijartamo <jussi@jartamo@gofore.com>
2014-02-06 15:22:47 +0200

    koekutsukirjereittiin korjauksia

--- src/main/java/fi/vm/sade/valinta/kooste/valintatieto/komponentti/ValintatietoHakukohteelleKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintatieto/komponentti/ValintatietoHakukohteelleKomponentti.java
@@ -1,8 +1,11 @@
 package fi.vm.sade.valinta.kooste.valintatieto.komponentti;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.camel.Property;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,8 @@ import fi.vm.sade.service.valintatiedot.schema.HakemusOsallistuminenTyyppi;
 @Component("valintatietoHakukohteelleKomponentti")
 public class ValintatietoHakukohteelleKomponentti {
 
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ValintatietoHakukohteelleKomponentti.class);
 	private ValintatietoService valintatietoService;
 
 	@Autowired
@@ -28,6 +33,14 @@ public class ValintatietoHakukohteelleKomponentti {
 		List<HakemusOsallistuminenTyyppi> osallistujat = valintatietoService
 				.haeValintatiedotHakukohteelle(valintakoeOids, hakukohdeOid);
 		if (osallistujat == null || osallistujat.isEmpty()) {
+			String oids = null;
+			if (osallistujat != null) {
+				oids = Arrays.toString(osallistujat.toArray());
+			}
+			LOG.error(
+					"Osallistumistietoja ei saatu hakukohteelle({}), kun valintakokeet[{}] oli kyseessa!",
+					new Object[] { hakukohdeOid, oids });
+
 			throw new RuntimeException(
 					"ValintatietoService palautti tyhjan osallistujajoukon.");
 		}
--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/route/impl/KoekutsukirjeRouteImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/route/impl/KoekutsukirjeRouteImpl.java
@@ -1,20 +1,23 @@
 package fi.vm.sade.valinta.kooste.viestintapalvelu.route.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.apache.camel.util.toolbox.FlexibleAggregationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Collections2;
+
 import fi.vm.sade.service.valintatiedot.schema.HakemusOsallistuminenTyyppi;
 import fi.vm.sade.service.valintatiedot.schema.Osallistuminen;
 import fi.vm.sade.service.valintatiedot.schema.ValintakoeOsallistuminenTyyppi;
 import fi.vm.sade.valinta.kooste.external.resource.haku.ApplicationResource;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
+import fi.vm.sade.valinta.kooste.security.SecurityPreprocessor;
 import fi.vm.sade.valinta.kooste.valintatieto.komponentti.ValintatietoHakukohteelleKomponentti;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.komponentti.KoekutsukirjeetKomponentti;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.resource.ViestintapalveluResource;
@@ -50,11 +53,38 @@ public class KoekutsukirjeRouteImpl extends SpringRouteBuilder {
 		//
 		from(koekutsukirjeet())
 		//
+				.bean(new SecurityPreprocessor())
+				//
 				.bean(valintatietoHakukohteelleKomponentti)
 				//
-				.split(body(), hakemusAggregation())
+				.process(new Processor() {
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						List<HakemusOsallistuminenTyyppi> unfiltered = (List<HakemusOsallistuminenTyyppi>) exchange
+								.getIn().getBody();
+						exchange.getOut()
+								.setBody(
+										Collections2
+												.filter(unfiltered,
+														new com.google.common.base.Predicate<HakemusOsallistuminenTyyppi>() {
+															public boolean apply(
+																	HakemusOsallistuminenTyyppi o) {
+																for (ValintakoeOsallistuminenTyyppi o1 : o
+																		.getOsallistumiset()) {
+																	if (Osallistuminen.OSALLISTUU
+																			.equals(o1
+																					.getOsallistuminen())) {
+																		return true;
+																	}
+																}
+																return false;
+															}
+														}));
+
+					}
+				})
 				//
-				// .filter(osallistuu())
+				.split(body(), hakemusAggregation())
 				// HakemusOsallistuminenTyyppi -> Hakemus
 				// Pitaisiko tehda oma convertteri yleisille kaannoksille?
 				.process(new Processor() {
@@ -86,19 +116,4 @@ public class KoekutsukirjeRouteImpl extends SpringRouteBuilder {
 				.accumulateInCollection(ArrayList.class);
 	}
 
-	private Predicate osallistuu() {
-		return new Predicate() {
-			public boolean matches(Exchange exchange) {
-				HakemusOsallistuminenTyyppi o = exchange.getIn().getBody(
-						HakemusOsallistuminenTyyppi.class);
-				for (ValintakoeOsallistuminenTyyppi o1 : o.getOsallistumiset()) {
-					if (Osallistuminen.OSALLISTUU
-							.equals(o1.getOsallistuminen())) {
-						return true;
-					}
-				}
-				return false;
-			}
-		};
-	}
 }
