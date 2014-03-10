46e9dd233d2d443b7bf366d6c2681e0a025ba54f
jussijartamo <jussi@jartamo@gofore.com>
2014-03-10 15:14:46 +0200

    mahdollisuus viedä post viestinä kelalle joukon hakuOideja

---
+++ src/main/java/fi/vm/sade/valinta/kooste/kela/dto/KelaHakuFiltteri.java
@@ -0,0 +1,16 @@
+package fi.vm.sade.valinta.kooste.kela.dto;
+
+import java.util.List;
+
+public class KelaHakuFiltteri {
+
+	private List<String> hakuOids;
+
+	public List<String> getHakuOids() {
+		return hakuOids;
+	}
+
+	public void setHakuOids(List<String> hakuOids) {
+		this.hakuOids = hakuOids;
+	}
+}
