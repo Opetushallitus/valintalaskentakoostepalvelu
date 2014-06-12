6c86d2b59cfb0c01988bb86f6aa7c7bb9db5280d
jussijartamo <jussi@jartamo@gofore.com>
2014-06-12 17:11:12 +0300

    ruotsinkieliset hyväksymiskirjeet korjattu

--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/dto/MetaHakukohde.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/dto/MetaHakukohde.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.valinta.kooste.viestintapalvelu.dto;
 
+import fi.vm.sade.valinta.kooste.util.KieliUtil;
+
 /**
  * 
  * @author Jussi Jartamo
@@ -9,32 +11,39 @@ package fi.vm.sade.valinta.kooste.viestintapalvelu.dto;
  */
 public class MetaHakukohde {
 
-    private final Teksti hakukohdeNimi;
-    private final Teksti tarjoajaNimi;
-    private final String hakukohteenKieli;
+	private final Teksti hakukohdeNimi;
+	private final Teksti tarjoajaNimi;
+	private final String hakukohteenKieli;
 
-    public MetaHakukohde(Teksti hakukohdeNimi, Teksti tarjoajaNimi) {
-        this.hakukohdeNimi = hakukohdeNimi;
-        this.tarjoajaNimi = tarjoajaNimi;
-        this.hakukohteenKieli = null;
-    }
+	public MetaHakukohde(Teksti hakukohdeNimi, Teksti tarjoajaNimi) {
+		this.hakukohdeNimi = hakukohdeNimi;
+		this.tarjoajaNimi = tarjoajaNimi;
+		this.hakukohteenKieli = null;
+	}
 
-    public MetaHakukohde(Teksti hakukohdeNimi, Teksti tarjoajaNimi, String hakukohteenKieli) {
-        this.hakukohdeNimi = hakukohdeNimi;
-        this.tarjoajaNimi = tarjoajaNimi;
-        this.hakukohteenKieli = hakukohteenKieli;
-    }
+	public MetaHakukohde(Teksti hakukohdeNimi, Teksti tarjoajaNimi,
+			String hakukohteenKieli) {
+		this.hakukohdeNimi = hakukohdeNimi;
+		this.tarjoajaNimi = tarjoajaNimi;
+		this.hakukohteenKieli = hakukohteenKieli;
+	}
 
-    public String getHakukohteenKieli() {
-        return hakukohteenKieli;
-    }
+	public String getHakukohteenKieli() {
+		if (hakukohteenKieli == null) {
+			if (hakukohdeNimi == null) {
+				return KieliUtil.SUOMI;
+			}
+			return getHakukohdeNimi().getKieli();
+		}
+		return hakukohteenKieli;
+	}
 
-    public Teksti getHakukohdeNimi() {
-        return hakukohdeNimi;
-    }
+	public Teksti getHakukohdeNimi() {
+		return hakukohdeNimi;
+	}
 
-    public Teksti getTarjoajaNimi() {
-        return tarjoajaNimi;
-    }
+	public Teksti getTarjoajaNimi() {
+		return tarjoajaNimi;
+	}
 
 }
--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/route/impl/KirjeetHakukohdeCache.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/route/impl/KirjeetHakukohdeCache.java
@@ -1,28 +1,35 @@
 package fi.vm.sade.valinta.kooste.viestintapalvelu.route.impl;
 
+import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Sets;
 
-import fi.vm.sade.tarjonta.service.resources.dto.HakukohdeNimiRDTO;
-import fi.vm.sade.valinta.kooste.tarjonta.komponentti.HaeHakukohdeNimiTarjonnaltaKomponentti;
+import fi.vm.sade.tarjonta.service.resources.HakukohdeResource;
+import fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO;
+import fi.vm.sade.valinta.kooste.util.KieliUtil;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.MetaHakukohde;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Teksti;
 
 @Component
 public class KirjeetHakukohdeCache {
-
+	private final Logger LOG = LoggerFactory
+			.getLogger(KirjeetHakukohdeCache.class);
 	private final Cache<String, MetaHakukohde> metaHakukohdeCache = CacheBuilder
 			.newBuilder().softValues().expireAfterWrite(12, TimeUnit.HOURS)
 			.build();
 
+	// private HaeHakukohdeNimiTarjonnaltaKomponentti tarjontaProxy;
 	@Autowired
-	private HaeHakukohdeNimiTarjonnaltaKomponentti tarjontaProxy;
+	private HakukohdeResource hakukohdeResource;
 
 	public MetaHakukohde haeHakukohde(final String hakukohdeOid)
 			throws Exception {
@@ -31,12 +38,28 @@ public class KirjeetHakukohdeCache {
 				new Callable<MetaHakukohde>() {
 					@Override
 					public MetaHakukohde call() throws Exception {
-						HakukohdeNimiRDTO nimi = tarjontaProxy
-								.haeHakukohdeNimi(hakukohdeOid);
-						Teksti hakukohdeNimi = new Teksti(nimi
+						HakukohdeDTO hakukohde = hakukohdeResource
+								.getByOID(hakukohdeOid);
+						Teksti hakukohdeNimi = new Teksti(hakukohde
 								.getHakukohdeNimi());
-						Teksti tarjoajaNimi = new Teksti(nimi.getTarjoajaNimi());
-						return new MetaHakukohde(hakukohdeNimi, tarjoajaNimi);
+						Teksti tarjoajaNimi = new Teksti(hakukohde
+								.getTarjoajaNimi());
+						Collection<String> preferoitukieli = Sets.newTreeSet();
+						for (String opetuskieli : hakukohde.getOpetuskielet()) {
+							preferoitukieli.add(KieliUtil
+									.normalisoiKielikoodi(opetuskieli));
+						}
+						if (preferoitukieli.contains(KieliUtil.SUOMI)) {
+							return new MetaHakukohde(hakukohdeNimi,
+									tarjoajaNimi, KieliUtil.SUOMI);
+						} else if (preferoitukieli.contains(KieliUtil.RUOTSI)) {
+							return new MetaHakukohde(hakukohdeNimi,
+									tarjoajaNimi, KieliUtil.RUOTSI);
+						}
+						return new MetaHakukohde(hakukohdeNimi, tarjoajaNimi,
+								KieliUtil.SUOMI);
+						// LOG.error("preferoitukieli on {}", preferoitukieli);
+
 					}
 
 				});
