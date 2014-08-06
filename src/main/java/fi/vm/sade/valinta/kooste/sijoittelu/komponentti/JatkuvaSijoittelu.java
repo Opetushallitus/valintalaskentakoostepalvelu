a96276df7aed882f8fab7946cb8a2242e4316429
kallem <kalle.manninen@gofore.com>
2014-08-06 13:19:06 +0300

    VT-920 Jatkuva sijoittelu takaisin päälle palvelun käynnistymisen yhteydessä

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/JatkuvaSijoittelu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/JatkuvaSijoittelu.java
@@ -1,32 +1,39 @@
 package fi.vm.sade.valinta.kooste.sijoittelu.komponentti;
 
-import fi.vm.sade.valinta.seuranta.resource.SijoittelunSeurantaResource;
-import fi.vm.sade.valinta.seuranta.sijoittelu.dto.SijoitteluDto;
+
+import fi.vm.sade.valinta.kooste.sijoittelu.Sijoittelu;
+import fi.vm.sade.valinta.kooste.sijoittelu.resource.SijoitteluResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  */
 @Component("jatkuvaSijoittelu")
 public class JatkuvaSijoittelu {
+    public static Map<String, Sijoittelu> SIJOITTELU_HAUT = new HashMap<String, Sijoittelu>();
 
     private static final Logger LOG = LoggerFactory.getLogger(JatkuvaSijoittelu.class);
 
     @Autowired
-    private SijoittelunSeurantaResource sijoittelunSeurantaResource;
+    private SijoitteluResource sijoitteluResource;
 
     public void suorita() {
         LOG.debug("JATKUVA SIJOITTELU KÄYNNISTETTY");
-        for (SijoitteluDto sijoittelu : sijoittelunSeurantaResource.hae())  {
+        for (Sijoittelu sijoittelu : SIJOITTELU_HAUT.values()) {
             if(sijoittelu.isAjossa()) {
                 LOG.debug("JATKUVA SIJOITTELU: {}", sijoittelu.getHakuOid());
                 try {
-                    sijoittelunSeurantaResource.merkkaaSijoittelunAjossaTila(sijoittelu.getHakuOid(), true);
+//                    HakuTyyppi ht = valintatietoService.haeValintatiedot(sijoittelu.getHakuOid());
+//                    LOG.info("Haettu valinnan tulokset");
+//                    sijoitteluService.sijoittele(ht);
+                    sijoitteluResource.sijoittele(sijoittelu.getHakuOid());
                     LOG.info("Viety sijoittelulle valinnan tulokset");
                 } catch(Exception e) {
                     LOG.error("JATKUVA SIJOITTELU", e);
