d6743f2f6810210ace2feff796f7765f9d64b999
jussijartamo <jussi@jartamo@gofore.com>
2014-08-18 13:33:26 +0300

    VT-920 Sijoittelukäynnistetään jos se on jatkuvassa sijoittelussa

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/JatkuvaSijoittelu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/komponentti/JatkuvaSijoittelu.java
@@ -1,12 +1,14 @@
 package fi.vm.sade.valinta.kooste.sijoittelu.komponentti;
 
+import javax.annotation.Resource;
+
 import fi.vm.sade.valinta.seuranta.resource.SijoittelunSeurantaResource;
 import fi.vm.sade.valinta.seuranta.sijoittelu.dto.SijoitteluDto;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 /**
  *
@@ -14,27 +16,31 @@ import org.springframework.stereotype.Component;
 @Component("jatkuvaSijoittelu")
 public class JatkuvaSijoittelu {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JatkuvaSijoittelu.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(JatkuvaSijoittelu.class);
 
-    @Autowired
-    private SijoittelunSeurantaResource sijoittelunSeurantaResource;
+	@Autowired
+	private SijoittelunSeurantaResource sijoittelunSeurantaResource;
+	@Autowired
+	private fi.vm.sade.valinta.kooste.sijoittelu.resource.SijoitteluResource sijoitteluResource;
 
-    public void suorita() {
-        LOG.debug("JATKUVA SIJOITTELU KÄYNNISTETTY");
-        for (SijoitteluDto sijoittelu : sijoittelunSeurantaResource.hae())  {
-            if(sijoittelu.isAjossa()) {
-                LOG.debug("JATKUVA SIJOITTELU: {}", sijoittelu.getHakuOid());
-                try {
-                    sijoittelunSeurantaResource.merkkaaSijoittelunAjossaTila(sijoittelu.getHakuOid(), true);
-                    LOG.info("Viety sijoittelulle valinnan tulokset");
-                } catch(Exception e) {
-                    LOG.error("JATKUVA SIJOITTELU", e);
-                    sijoittelunSeurantaResource.merkkaaSijoittelunAjossaVirhe(sijoittelu.getHakuOid(), e.getMessage());
-                }
-            }
-        }
-        LOG.debug("JATKUVA SIJOITTELU LOPETETTU");
-    }
+	public void suorita() {
+		LOG.debug("JATKUVA SIJOITTELU KÄYNNISTETTY");
+		for (SijoitteluDto sijoittelu : sijoittelunSeurantaResource.hae()) {
+			if (sijoittelu.isAjossa()) {
+				LOG.debug("JATKUVA SIJOITTELU: {}", sijoittelu.getHakuOid());
+				try {
+					sijoitteluResource.sijoittele(sijoittelu.getHakuOid());
+					sijoittelunSeurantaResource.merkkaaSijoittelunAjossaTila(
+							sijoittelu.getHakuOid(), true);
+					LOG.info("Viety sijoittelulle valinnan tulokset");
+				} catch (Exception e) {
+					LOG.error("JATKUVA SIJOITTELU", e);
+					sijoittelunSeurantaResource.merkkaaSijoittelunAjossaVirhe(
+							sijoittelu.getHakuOid(), e.getMessage());
+				}
+			}
+		}
+		LOG.debug("JATKUVA SIJOITTELU LOPETETTU");
+	}
 }
-
-
