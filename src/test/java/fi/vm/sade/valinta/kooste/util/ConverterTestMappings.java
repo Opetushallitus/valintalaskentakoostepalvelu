afdb5d4e1837d22f7d6f22f010aae4102e9b0ac5
jussijartamo <jussi@jartamo@gofore.com>
2014-10-07 08:53:42 +0300

    VT-1013

--- src/test/java/fi/vm/sade/valinta/kooste/util/ConverterTestMappings.java
+++ src/test/java/fi/vm/sade/valinta/kooste/util/ConverterTestMappings.java
@@ -1,17 +1,56 @@
 package fi.vm.sade.valinta.kooste.util;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 
 import com.google.common.collect.Maps;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
+import fi.vm.sade.valinta.kooste.external.resource.haku.dto.ApplicationAdditionalDataDTO;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Eligibility;
+import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
+import fi.vm.sade.valintalaskenta.domain.dto.HakemusDTO;
 
 public class ConverterTestMappings {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ConverterTestMappings.class);
+
+	@Test
+	public void testaaEligibilitiesOikeallaDatalla()
+			throws JsonSyntaxException, IOException {
+		List<Hakemus> hakemukset = new Gson().fromJson(IOUtils
+				.toString(new ClassPathResource("listfull2_eligibilities.json")
+						.getInputStream()), new TypeToken<List<Hakemus>>() {
+		}.getType());
+		Hakemus hakemus = hakemukset.stream()
+				.filter(h -> "1.2.246.562.11.00000977230".equals(h.getOid()))
+				.distinct().iterator().next();
+		HakemusDTO dto = Converter.hakemusToHakemusDTO(hakemus);
+		// LOG.error("\r\n{}", new GsonBuilder().setPrettyPrinting().create()
+		// .toJson(dto));
+		Assert.assertTrue(dto
+				.getAvaimet()
+				.stream()
+				.filter(pari -> "preference1-Koulutus-id-eligibility"
+						.equals(pari.getAvain())
+						&& "NOT_CHECKED".equals(pari.getArvo())).distinct()
+				.iterator().hasNext());
+
+	}
 
 	@Test
 	public void testaaEligibilitiesMappaustaNullArvoilla() {
