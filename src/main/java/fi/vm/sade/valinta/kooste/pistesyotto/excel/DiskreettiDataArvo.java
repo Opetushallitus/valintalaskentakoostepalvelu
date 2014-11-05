fd5ee23d579838a194557f1e4476875feab7715b
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2014-11-05 15:23:43 +0200

    VT-1052

--- src/main/java/fi/vm/sade/valinta/kooste/pistesyotto/excel/DiskreettiDataArvo.java
+++ src/main/java/fi/vm/sade/valinta/kooste/pistesyotto/excel/DiskreettiDataArvo.java
@@ -3,6 +3,7 @@ package fi.vm.sade.valinta.kooste.pistesyotto.excel;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -14,6 +15,8 @@ public class DiskreettiDataArvo extends TilaDataArvo {
 
 	private final Logger LOG = LoggerFactory.getLogger(DiskreettiDataArvo.class);
 	private final Set<String> arvot;
+	private final Set<Double> numerot;
+	private final Map<Double,String> konversio;
 	private final String tunniste;
 	private final String osallistuminenTunniste;
 
@@ -22,6 +25,15 @@ public class DiskreettiDataArvo extends TilaDataArvo {
 			String asetettuTila, String osallistuminenTunniste) {
 		super(tilaKonvertteri, asetettuTila);
 		this.arvot = Sets.newHashSet(arvot);
+		this.konversio = arvot.stream().filter(a -> {
+			try {
+			Double.parseDouble(a);
+			return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}).collect(Collectors.toMap(a -> Double.parseDouble(a), a -> a));
+		this.numerot = this.konversio.keySet();
 		this.tunniste = tunniste;
 		this.osallistuminenTunniste = osallistuminenTunniste;
 	}
@@ -31,7 +43,16 @@ public class DiskreettiDataArvo extends TilaDataArvo {
 	}
 
 	private boolean tarkistaArvo(String arvo) {
-		return arvot.contains(arvo);
+		if(arvot.contains(arvo)) {
+			return true;
+		} else {
+			try {
+				double d = Double.parseDouble(arvo);
+				return konversio.containsKey(d);
+			}catch(Exception e) {
+				return false;
+			}
+		}
 	}
 
 	private boolean isAsetettu(String arvo) {
@@ -49,8 +70,18 @@ public class DiskreettiDataArvo extends TilaDataArvo {
 				lopullinenTila = PistesyottoExcel.VAKIO_MERKITSEMATTA;
 			}
 		}
-		return new PistesyottoArvo(arvo, lopullinenTila, isValidi(arvo)
+		return new PistesyottoArvo(konvertoi(arvo), lopullinenTila, isValidi(arvo)
 				&& isValidiTila(tila), tunniste, osallistuminenTunniste);
+	}
+	private String konvertoi(String arvo) {
+		try {
+			double d = Double.parseDouble(arvo);
+			if(konversio.containsKey(d)) {
+				return konversio.get(d);
+			}
+		}catch(Exception e) {
+		}
+		return arvo;
 	}
 
 }
