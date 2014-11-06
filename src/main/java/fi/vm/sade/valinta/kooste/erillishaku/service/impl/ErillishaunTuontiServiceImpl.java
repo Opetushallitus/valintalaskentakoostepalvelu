d55cdfdecddbd66ff242b637eaff542a6d557dae
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2014-11-06 10:50:28 +0200

    VT-1046

--- src/main/java/fi/vm/sade/valinta/kooste/erillishaku/service/impl/ErillishaunTuontiServiceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/erillishaku/service/impl/ErillishaunTuontiServiceImpl.java
@@ -137,6 +137,7 @@ public class ErillishaunTuontiServiceImpl implements ErillishaunTuontiService {
 						LOG.error("Taulukkolaskentatiedostosta ei saatu poimittua yhtaan hakijaa sijoitteluun tuotavaksi!");
 						throw new RuntimeException("Taulukkolaskentatiedostosta ei saatu poimittua yhtaan hakijaa sijoitteluun tuotavaksi!");
 					}
+					prosessi.valmistui("ok");
 				},
 				poikkeus-> {
 					LOG.error("Erillishaun tuonti keskeytyi virheeseen {}. {}", poikkeus.getMessage(), Arrays.toString(poikkeus.getStackTrace()));
