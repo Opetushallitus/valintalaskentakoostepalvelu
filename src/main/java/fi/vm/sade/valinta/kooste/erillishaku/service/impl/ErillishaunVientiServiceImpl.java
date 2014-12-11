4c400d438e221ee8b6b37ae686d22142d3b48791
Mikko Kaistinen <mikko.kaistinen@reaktor.fi>
2014-12-11 09:49:32 +0200

    ErillishaunVientiServiceImpl and HakemusWrapper refactored

--- src/main/java/fi/vm/sade/valinta/kooste/erillishaku/service/impl/ErillishaunVientiServiceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/erillishaku/service/impl/ErillishaunVientiServiceImpl.java
@@ -1,25 +1,6 @@
 package fi.vm.sade.valinta.kooste.erillishaku.service.impl;
 
-import static rx.Observable.from;
-import static rx.Observable.zip;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-
-import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import rx.schedulers.Schedulers;
-import fi.vm.sade.sijoittelu.domain.Hakukohde;
 import fi.vm.sade.sijoittelu.domain.Valintatulos;
-import fi.vm.sade.sijoittelu.tulos.dto.HakemusDTO;
 import fi.vm.sade.sijoittelu.tulos.dto.HakukohdeDTO;
 import fi.vm.sade.sijoittelu.tulos.resource.SijoitteluResource;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.HakuV1RDTO;
@@ -35,134 +16,145 @@ import fi.vm.sade.valinta.kooste.external.resource.tarjonta.TarjontaAsyncResourc
 import fi.vm.sade.valinta.kooste.util.HakemusWrapper;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.KirjeProsessi;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Teksti;
+import org.apache.commons.collections.MapUtils;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rx.schedulers.Schedulers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import static rx.Observable.from;
+import static rx.Observable.zip;
 
 /**
- * 
  * @author Jussi Jartamo
- * 
  */
 @Service
 public class ErillishaunVientiServiceImpl implements ErillishaunVientiService {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(ErillishaunVientiServiceImpl.class);
-	private final SijoitteluAsyncResource sijoitteluAsyncResource;
-	private final TarjontaAsyncResource hakuV1AsyncResource;
-	private final ApplicationAsyncResource applicationAsyncResource;
-	private final DokumenttiResource dokumenttiResource;
-	@Autowired
-	public ErillishaunVientiServiceImpl(
-			ApplicationAsyncResource applicationAsyncResource,
-			SijoitteluAsyncResource sijoitteluAsyncResource,
-			TarjontaAsyncResource hakuV1AsyncResource
-			,DokumenttiResource dokumenttiResource) {
-		this.sijoitteluAsyncResource = sijoitteluAsyncResource;
-		this.hakuV1AsyncResource = hakuV1AsyncResource;
-		this.applicationAsyncResource = applicationAsyncResource;
-		this.dokumenttiResource = dokumenttiResource;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(ErillishaunVientiServiceImpl.class);
+    private final SijoitteluAsyncResource sijoitteluAsyncResource;
+    private final TarjontaAsyncResource hakuV1AsyncResource;
+    private final ApplicationAsyncResource applicationAsyncResource;
+    private final DokumenttiResource dokumenttiResource;
 
-	@Override
-	public void vie(KirjeProsessi prosessi, ErillishakuDTO erillishaku) {
-		Future<List<Hakemus>> hakemusFuture = applicationAsyncResource
-				.getApplicationsByOid(erillishaku.getHakuOid(),
-						erillishaku.getHakukohdeOid());
-		
-		Future<HakukohdeDTO> hakukohdeFuture = sijoitteluAsyncResource
-				.getLatestHakukohdeBySijoittelu(erillishaku.getHakuOid(),
-						erillishaku.getHakukohdeOid());
-		
-		Future<List<Valintatulos>> valintatulosFuture = sijoitteluAsyncResource
-				.getValintatuloksetHakukohteelle(
-						erillishaku.getHakukohdeOid(), erillishaku.getValintatapajonoOid());
-		Future<HakuV1RDTO> hakuFuture = hakuV1AsyncResource.haeHaku(erillishaku
-				.getHakuOid());
-		Future<fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO> tarjontaHakukohdeFuture = hakuV1AsyncResource
-				.haeHakukohde(erillishaku.getHakukohdeOid());
-		zip(
-				from(hakemusFuture),
-				from(hakuFuture),
-				from(tarjontaHakukohdeFuture),
-				(hakemukset, haku, tarjontaHakukohde) -> {
-					String hakuNimi = new Teksti(haku.getNimi()).getTeksti();
-					String hakukohdeNimi = new Teksti(tarjontaHakukohde
-							.getHakukohdeNimi()).getTeksti();
-					String tarjoajaNimi = new Teksti(tarjontaHakukohde
-							.getTarjoajaNimi()).getTeksti();
-					Map<String,Hakemus> oidToHakemus = hakemukset.stream().collect(Collectors.toMap(
-							h -> h.getOid(),h -> h
-							));
-					Map<String, Valintatulos> valintatulokset;
-					HakukohdeDTO hakukohde;
-					try {
-						valintatulokset = valintatulosFuture.get().stream()
-								.filter(v -> erillishaku.getValintatapajonoOid().equals(v.getValintatapajonoOid()))
-								.collect(Collectors.toMap(
-								v -> v.getHakemusOid(), v-> v));
-						hakukohde = hakukohdeFuture.get();
-					} catch (Exception e1) {
-						LOG.error("Sijoittelusta ei saatu tietoja hakukohteelle vientia varten!"
-								+ "Resurssi /sijoittelu/{}/sijoitteluajo/{}/hakukohde/{} "
-								+ "{}: {}", erillishaku.getHakuOid(),
-								erillishaku.getHakukohdeOid(),SijoitteluResource.LATEST, e1.getMessage(), Arrays.toString(e1.getStackTrace()));
-						throw new RuntimeException(e1);
-					}
-					
-					if (valintatulokset == null || valintatulokset.isEmpty()) {
-						// ei viela tuloksia, joten tehdaan tuonti haetuista
-						// hakemuksista
-						LOG.error("Hakemuksia ei ole viela tuotu ensimmaistakaan kertaa talle hakukohteelle! Generoidaan hakemuksista excel...");
-						List<ErillishakuRivi> rivit = hakemukset.stream().map(hakemus -> {
-							HakemusWrapper wrapper = new HakemusWrapper(hakemus);
-							ErillishakuRivi r = new ErillishakuRivi(wrapper.getSukunimi(), 
-									wrapper.getEtunimi(), wrapper.getHenkilotunnus(), wrapper.getSyntymaaika(), "HYLATTY", "","", false);
-							return r;
-						}).collect(Collectors.toList());
-						return  new ErillishakuExcel(erillishaku.getHakutyyppi(), hakuNimi, hakukohdeNimi,
-								tarjoajaNimi, rivit);
-					} else {
-						LOG.error("Muodostetaan Excel valintaperusteista!");
-						List<ErillishakuRivi> erillishakurivit = hakukohde
-								.getValintatapajonot().stream()
-								.flatMap(v -> v.getHakemukset().stream())
-								//
-								.map(h -> {
-									HakemusWrapper h0 = new HakemusWrapper(oidToHakemus.get(h.getHakemusOid()));
-									String hakemuksenTila = "";
-									if(h.getTila() != null) {
-										hakemuksenTila = h.getTila().toString();
-									}
-									Valintatulos tulos = valintatulokset.get(h.getHakemusOid());
-									ErillishakuRivi e =new ErillishakuRivi(h
-											.getSukunimi(), h.getEtunimi(),h0.getHenkilotunnus(),h0.getSyntymaaika(),
-											hakemuksenTila,tulos.getTila().toString(),tulos.getIlmoittautumisTila().toString(),
-											tulos.getJulkaistavissa());
-									return e;
-								}).collect(Collectors.toList());
+    @Autowired
+    public ErillishaunVientiServiceImpl(
+            ApplicationAsyncResource applicationAsyncResource,
+            SijoitteluAsyncResource sijoitteluAsyncResource,
+            TarjontaAsyncResource hakuV1AsyncResource
+            , DokumenttiResource dokumenttiResource) {
+        this.sijoitteluAsyncResource = sijoitteluAsyncResource;
+        this.hakuV1AsyncResource = hakuV1AsyncResource;
+        this.applicationAsyncResource = applicationAsyncResource;
+        this.dokumenttiResource = dokumenttiResource;
+    }
 
-						return new ErillishakuExcel(erillishaku.getHakutyyppi(), hakuNimi, hakukohdeNimi,
-								tarjoajaNimi, erillishakurivit);
-					}
+    @Override
+    public void vie(KirjeProsessi prosessi, ErillishakuDTO erillishaku) {
+        Future<List<Hakemus>> hakemusFuture = applicationAsyncResource
+                .getApplicationsByOid(erillishaku.getHakuOid(), erillishaku.getHakukohdeOid());
 
-				})
-		//
-				.subscribeOn(Schedulers.newThread())
-				//
-				.subscribe(e -> {
-					LOG.warn("Aloitetaan dokumenttipalveluun tallennus");
-					String uuid = UUID.randomUUID().toString();
-					dokumenttiResource.tallenna(uuid, "erillishaku.xlsx", DateTime.now().plusHours(1).toDate().getTime(), 
-							Arrays.asList("erillishaku"), "application/octet-stream", e.getExcel().vieXlsx());
-					prosessi.vaiheValmistui();
-					prosessi.valmistui(uuid);
-				},
-				poikkeus -> {
-					LOG.error("Erillishaun vienti keskeytyi virheeseen {}. {}", poikkeus.getMessage(), Arrays.toString(poikkeus.getStackTrace()));
-					prosessi.keskeyta();
-				},
-				() -> {
-					LOG.warn("Erillishaun vienti onnistui!");
-					prosessi.keskeyta();
-				});
-	}
+        Future<HakukohdeDTO> hakukohdeFuture = sijoitteluAsyncResource
+                .getLatestHakukohdeBySijoittelu(erillishaku.getHakuOid(), erillishaku.getHakukohdeOid());
+
+        Future<List<Valintatulos>> valintatulosFuture = sijoitteluAsyncResource
+                .getValintatuloksetHakukohteelle(erillishaku.getHakukohdeOid(), erillishaku.getValintatapajonoOid());
+
+        Future<HakuV1RDTO> hakuFuture = hakuV1AsyncResource.haeHaku(erillishaku.getHakuOid());
+
+        Future<fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO> tarjontaHakukohdeFuture = hakuV1AsyncResource
+                .haeHakukohde(erillishaku.getHakukohdeOid());
+
+        zip(
+                from(hakemusFuture),
+                from(hakuFuture),
+                from(tarjontaHakukohdeFuture),
+                (hakemukset, haku, tarjontaHakukohde) -> {
+                    String hakuNimi = new Teksti(haku.getNimi()).getTeksti();
+                    String hakukohdeNimi = new Teksti(tarjontaHakukohde
+                            .getHakukohdeNimi()).getTeksti();
+                    String tarjoajaNimi = new Teksti(tarjontaHakukohde
+                            .getTarjoajaNimi()).getTeksti();
+                    Map<String, Hakemus> oidToHakemus = hakemukset.stream().collect(Collectors.toMap(
+                            h -> h.getOid(), h -> h
+                    ));
+                    Map<String, Valintatulos> valintatulokset;
+                    HakukohdeDTO hakukohde;
+                    try {
+                        valintatulokset = valintatulosFuture.get().stream()
+                                .filter(v -> erillishaku.getValintatapajonoOid().equals(v.getValintatapajonoOid()))
+                                .collect(Collectors.toMap(
+                                        v -> v.getHakemusOid(), v -> v));
+                        hakukohde = hakukohdeFuture.get();
+                    } catch (Exception e1) {
+                        LOG.error("Sijoittelusta ei saatu tietoja hakukohteelle vientia varten!"
+                                        + "Resurssi /sijoittelu/{}/sijoitteluajo/{}/hakukohde/{} "
+                                        + "{}: {}", erillishaku.getHakuOid(),
+                                erillishaku.getHakukohdeOid(), SijoitteluResource.LATEST, e1.getMessage(), Arrays.toString(e1.getStackTrace()));
+                        throw new RuntimeException(e1);
+                    }
+
+                    if (MapUtils.isEmpty(valintatulokset)) {
+                        // ei viela tuloksia, joten tehdaan tuonti haetuista hakemuksista
+                        LOG.error("Hakemuksia ei ole viela tuotu ensimmaistakaan kertaa talle hakukohteelle! Generoidaan hakemuksista excel...");
+                        List<ErillishakuRivi> rivit = hakemukset.stream().map(hakemus -> {
+                            HakemusWrapper wrapper = new HakemusWrapper(hakemus);
+                            ErillishakuRivi r = new ErillishakuRivi(wrapper.getSukunimi(),
+                                    wrapper.getEtunimi(), wrapper.getHenkilotunnus(), wrapper.getSyntymaaika(), "HYLATTY", "", "", false);
+                            return r;
+                        }).collect(Collectors.toList());
+                        return new ErillishakuExcel(erillishaku.getHakutyyppi(), hakuNimi, hakukohdeNimi,
+                                tarjoajaNimi, rivit);
+                    } else {
+                        LOG.error("Muodostetaan Excel valintaperusteista!");
+                        List<ErillishakuRivi> erillishakurivit = hakukohde
+                                .getValintatapajonot().stream()
+                                .flatMap(v -> v.getHakemukset().stream())
+                                        //
+                                .map(h -> {
+                                    HakemusWrapper h0 = new HakemusWrapper(oidToHakemus.get(h.getHakemusOid()));
+                                    String hakemuksenTila = "";
+                                    if (h.getTila() != null) {
+                                        hakemuksenTila = h.getTila().toString();
+                                    }
+                                    Valintatulos tulos = valintatulokset.get(h.getHakemusOid());
+                                    ErillishakuRivi e = new ErillishakuRivi(h
+                                            .getSukunimi(), h.getEtunimi(), h0.getHenkilotunnus(), h0.getSyntymaaika(),
+                                            hakemuksenTila, tulos.getTila().toString(), tulos.getIlmoittautumisTila().toString(),
+                                            tulos.getJulkaistavissa());
+                                    return e;
+                                }).collect(Collectors.toList());
+
+                        return new ErillishakuExcel(erillishaku.getHakutyyppi(), hakuNimi, hakukohdeNimi,
+                                tarjoajaNimi, erillishakurivit);
+                    }
+
+                })
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(e -> {
+                            LOG.warn("Aloitetaan dokumenttipalveluun tallennus");
+                            String uuid = UUID.randomUUID().toString();
+                            dokumenttiResource.tallenna(uuid, "erillishaku.xlsx", DateTime.now().plusHours(1).toDate().getTime(),
+                                    Arrays.asList("erillishaku"), "application/octet-stream", e.getExcel().vieXlsx());
+                            prosessi.vaiheValmistui();
+                            prosessi.valmistui(uuid);
+                        },
+                        poikkeus -> {
+                            LOG.error("Erillishaun vienti keskeytyi virheeseen {}. {}", poikkeus.getMessage(), Arrays.toString(poikkeus.getStackTrace()));
+                            prosessi.keskeyta();
+                        },
+                        () -> {
+                            LOG.warn("Erillishaun vienti onnistui!");
+                            prosessi.keskeyta();
+                        });
+    }
 }
--- src/main/java/fi/vm/sade/valinta/kooste/util/HakemusWrapper.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/HakemusWrapper.java
@@ -1,177 +1,157 @@
 package fi.vm.sade.valinta.kooste.util;
 
+import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
+import fi.vm.sade.valinta.kooste.hakemus.dto.Yhteystiedot;
+import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Osoite;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.TreeMap;
 
-import org.apache.commons.lang.StringUtils;
-
-import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
-import fi.vm.sade.valinta.kooste.hakemus.dto.Yhteystiedot;
-import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Osoite;
-
 /**
- * 
  * @author Jussi Jartamo
- * 
+ *         <p/>
  *         Hakemustietojen luku hakemustietueesta vikasietoisesti
  */
 public class HakemusWrapper {
 
-	private final Hakemus hakemus;
-	private Map<String, String> henkilotiedot = null;
-	private Map<String, String> lisatiedot = null;
-	private Map<String, String> hakutoiveet = null;
-	private final static String ETUNIMET = "Etunimet";
-	private final static String KUTSUMANIMI = "Kutsumanimi";
-	private final static String SUKUNIMI = "Sukunimi";
-	private final static String ASIOINTIKIELI = "asiointikieli";
-	private final static String LUPAJULKAISUUN = "lupaJulkaisu";
-	private final static String HETU = "Henkilotunnus";
-	private final static String SAHKOPOSTI = "Sähköposti";
-	private final static String SYNTYMAAIKA = "syntymaaika";
-	private Yhteystiedot yhteystiedot = null;
-	public HakemusWrapper(Hakemus hakemus) {
-		if(hakemus == null) {
-			this.henkilotiedot = Collections.emptyMap();
-			this.lisatiedot = Collections.emptyMap();
-			this.hakutoiveet = Collections.emptyMap();
-		}
-		this.hakemus = hakemus;
-	}
-	public Osoite getOsoite() {
-		return OsoiteHakemukseltaUtil.osoiteHakemuksesta(hakemus, null, null);
-	}
-	public String getPuhelinnumero() {
-		if(yhteystiedot == null) {
-		this.yhteystiedot = Yhteystiedot
-				.yhteystiedotHakemukselta(hakemus);
-		}
-		return yhteystiedot.getPuhelinnumerotAsString();
-	}
-	
-	public String getSahkopostiOsoite() {
-		getHenkilotiedot();
-		return Optional.ofNullable(henkilotiedot.get(SAHKOPOSTI)).orElse(
-				StringUtils.EMPTY);
-	}
+    private final Hakemus hakemus;
+    private Map<String, String> henkilotiedot = null;
+    private Map<String, String> lisatiedot = null;
+    private Map<String, String> hakutoiveet = null;
+    private final static String ETUNIMET = "Etunimet";
+    private final static String KUTSUMANIMI = "Kutsumanimi";
+    private final static String SUKUNIMI = "Sukunimi";
+    private final static String ASIOINTIKIELI = "asiointikieli";
+    private final static String LUPAJULKAISUUN = "lupaJulkaisu";
+    private final static String HETU = "Henkilotunnus";
+    private final static String SAHKOPOSTI = "Sähköposti";
+    private final static String SYNTYMAAIKA = "syntymaaika";
+    private Yhteystiedot yhteystiedot = null;
 
-	public String getHenkilotunnusTaiSyntymaaika() {
-		getHenkilotiedot();
-		return Optional.ofNullable(henkilotiedot.get(HETU)).orElse(
-				Optional.ofNullable(henkilotiedot.get(SYNTYMAAIKA)).orElse(
-						StringUtils.EMPTY));
-	}
-	public String getSyntymaaika() {
-		getHenkilotiedot();
-		return Optional.ofNullable(henkilotiedot.get(SYNTYMAAIKA)).orElse(
-						StringUtils.EMPTY);
-	}
-	public String getHenkilotunnus() {
-		getHenkilotiedot();
-		return Optional.ofNullable(henkilotiedot.get(HETU)).orElse(
-				StringUtils.EMPTY);
-	}
+    public HakemusWrapper(Hakemus hakemus) {
+        if (hakemus == null) {
+            this.henkilotiedot = Collections.emptyMap();
+            this.lisatiedot = Collections.emptyMap();
+            this.hakutoiveet = Collections.emptyMap();
+        }
+        this.hakemus = hakemus;
+    }
 
-	public Integer getHakutoiveenPrioriteetti(String hakukohdeOid) {
-		getHakutoiveet();
+    public Osoite getOsoite() {
+        return OsoiteHakemukseltaUtil.osoiteHakemuksesta(hakemus, null, null);
+    }
 
-		if (hakutoiveet.containsValue(hakukohdeOid)) {
-			for (Entry<String, String> s : hakutoiveet.entrySet()) {
-				if (hakukohdeOid.equals(s.getValue())) {
-					try {
-						String value = s.getKey().split("preference")[1]
-								.split("-")[0];
-						return Integer.parseInt(value);
-					} catch (Exception e) {
+    public String getPuhelinnumero() {
+        if (yhteystiedot == null) {
+            this.yhteystiedot = Yhteystiedot
+                    .yhteystiedotHakemukselta(hakemus);
+        }
+        return yhteystiedot.getPuhelinnumerotAsString();
+    }
 
-					}
-				}
-			}
-		}
-		return null;
-	}
+    public String getSahkopostiOsoite() {
+        getHenkilotiedot();
+        return Optional.ofNullable(henkilotiedot.get(SAHKOPOSTI)).orElse(
+                StringUtils.EMPTY);
+    }
 
-	public String getEtunimi() {
-		getHenkilotiedot(); // lazy load henkilotiedot
-		if (henkilotiedot.containsKey(KUTSUMANIMI)) {
-			return henkilotiedot.get(KUTSUMANIMI);
-		} else if (henkilotiedot.containsKey(ETUNIMET)) {
-			return henkilotiedot.get(ETUNIMET);
-		} else {
-			return StringUtils.EMPTY;
-		}
-	}
+    public String getHenkilotunnusTaiSyntymaaika() {
+        getHenkilotiedot();
+        return Optional.ofNullable(henkilotiedot.get(HETU)).orElse(
+                Optional.ofNullable(henkilotiedot.get(SYNTYMAAIKA)).orElse(
+                        StringUtils.EMPTY));
+    }
 
-	public String getSukunimi() {
-		getHenkilotiedot(); // lazy load henkilotiedot
-		if (henkilotiedot.containsKey(SUKUNIMI)) {
-			return henkilotiedot.get(SUKUNIMI);
-		} else {
-			return StringUtils.EMPTY;
-		}
-	}
+    public String getSyntymaaika() {
+        getHenkilotiedot();
+        return Optional.ofNullable(henkilotiedot.get(SYNTYMAAIKA)).orElse(
+                StringUtils.EMPTY);
+    }
 
-	public boolean getLupaJulkaisuun() {
-		getLisatiedot(); // lazy load henkilotiedot
-		if (lisatiedot.containsKey(LUPAJULKAISUUN)) {
-			String l = lisatiedot.get(LUPAJULKAISUUN);
-			try {
-				return Boolean.TRUE.equals(Boolean.valueOf(l));
-			} catch (Exception e) {
-			}
-		}
-		return false;
-	}
+    public String getHenkilotunnus() {
+        getHenkilotiedot();
+        return Optional.ofNullable(henkilotiedot.get(HETU)).orElse(
+                StringUtils.EMPTY);
+    }
 
-	public String getAsiointikieli() {
-		getLisatiedot(); // lazy load henkilotiedot
-		if (lisatiedot.containsKey(ASIOINTIKIELI)) {
-			return KieliUtil
-					.normalisoiKielikoodi(lisatiedot.get(ASIOINTIKIELI));
-		} else {
-			return KieliUtil.SUOMI;
-		}
-	}
+    public Integer getHakutoiveenPrioriteetti(String hakukohdeOid) {
+        getHakutoiveet();
 
-	public Map<String, String> getLisatiedot() {
-		if (lisatiedot == null) {
-			lisatiedot = // hakemus.getAnswers().getHenkilotiedot();
-			new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
-			try {
-				lisatiedot.putAll(hakemus.getAnswers().getLisatiedot());
-			} catch (Exception e) {
+        if (hakutoiveet.containsValue(hakukohdeOid)) {
+            for (Entry<String, String> s : hakutoiveet.entrySet()) {
+                if (hakukohdeOid.equals(s.getValue())) {
+                    String value = s.getKey().split("preference")[1].split("-")[0];
+                    return NumberUtils.createInteger(value);
+                }
+            }
+        }
+        return null;
+    }
 
-			}
-		}
-		return lisatiedot;
-	}
+    public String getEtunimi() {
+        getHenkilotiedot(); // lazy load henkilotiedot
+        if (henkilotiedot.containsKey(KUTSUMANIMI)) {
+            return henkilotiedot.get(KUTSUMANIMI);
+        } else if (henkilotiedot.containsKey(ETUNIMET)) {
+            return henkilotiedot.get(ETUNIMET);
+        } else {
+            return StringUtils.EMPTY;
+        }
+    }
 
-	public Map<String, String> getHenkilotiedot() {
-		if (henkilotiedot == null) {
-			henkilotiedot = // hakemus.getAnswers().getHenkilotiedot();
-			new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
-			try {
-				henkilotiedot.putAll(hakemus.getAnswers().getHenkilotiedot());
-			} catch (Exception e) {
+    public String getSukunimi() {
+        getHenkilotiedot(); // lazy load henkilotiedot
+        if (henkilotiedot.containsKey(SUKUNIMI)) {
+            return henkilotiedot.get(SUKUNIMI);
+        } else {
+            return StringUtils.EMPTY;
+        }
+    }
 
-			}
-		}
-		return henkilotiedot;
-	}
+    public boolean getLupaJulkaisuun() {
+        getLisatiedot(); // lazy load henkilotiedot
+        if (lisatiedot.containsKey(LUPAJULKAISUUN)) {
+            String l = lisatiedot.get(LUPAJULKAISUUN);
+            return Boolean.TRUE.equals(Boolean.valueOf(l));
+        }
+        return false;
+    }
 
-	public Map<String, String> getHakutoiveet() {
-		if (hakutoiveet == null) {
-			hakutoiveet = // hakemus.getAnswers().getHenkilotiedot();
-			new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
-			try {
-				hakutoiveet.putAll(hakemus.getAnswers().getHakutoiveet());
-			} catch (Exception e) {
+    public String getAsiointikieli() {
+        getLisatiedot(); // lazy load henkilotiedot
+        if (lisatiedot.containsKey(ASIOINTIKIELI)) {
+            return KieliUtil.normalisoiKielikoodi(lisatiedot.get(ASIOINTIKIELI));
+        } else {
+            return KieliUtil.SUOMI;
+        }
+    }
 
-			}
-		}
-		return hakutoiveet;
-	}
+    public Map<String, String> getLisatiedot() {
+        if (lisatiedot == null) {
+            lisatiedot = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+            lisatiedot.putAll(hakemus.getAnswers().getLisatiedot());
+        }
+        return lisatiedot;
+    }
+
+    public Map<String, String> getHenkilotiedot() {
+        if (henkilotiedot == null) {
+            henkilotiedot = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+            henkilotiedot.putAll(hakemus.getAnswers().getHenkilotiedot());
+        }
+        return henkilotiedot;
+    }
+
+    public Map<String, String> getHakutoiveet() {
+        if (hakutoiveet == null) {
+            hakutoiveet = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+            hakutoiveet.putAll(hakemus.getAnswers().getHakutoiveet());
+        }
+        return hakutoiveet;
+    }
 }
