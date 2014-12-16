b82be4951a25a6b50662660b9868436d54ebaba4
Juha Paananen <juha.paananen@gmail.com>
2014-12-16 15:32:39 +0200

    dummy test from ErillisHakuResource

---
+++ src/test/java/fi/vm/sade/valinta/kooste/erillishaku/resource/ErillishakuResourceTest.java
@@ -0,0 +1,17 @@
+package fi.vm.sade.valinta.kooste.erillishaku.resource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fi.vm.sade.valinta.kooste.ValintaKoosteTomcat;
+
+public class ErillishakuResourceTest {
+    @Before
+    public void startServer() {
+        ValintaKoosteTomcat.startShared();
+    }
+    @Test
+    public void asdf() {
+
+    }
+}
