4941c163bbdfd9490f278d2e396c63edea280fb1
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-01-09 11:43:14 +0200

    VT-1146

--- src/main/java/fi/vm/sade/valinta/kooste/security/HasOrganization.java
+++ src/main/java/fi/vm/sade/valinta/kooste/security/HasOrganization.java
@@ -3,5 +3,8 @@ package fi.vm.sade.valinta.kooste.security;
 /**
  * Created by jussija on 09/01/15.
  */
-public class HasOrganization {
+
+public interface HasOrganization {
+
+    public boolean hasOrganization(String tarjoajaOid) throws Exception;
 }
