fa10dadf70ca4708b22c428cf26b3434f748ceba
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-01-09 11:43:14 +0200

    VT-1146

--- src/main/java/fi/vm/sade/valinta/kooste/security/HasOrganizationImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/security/HasOrganizationImpl.java
@@ -1,7 +1,32 @@
 package fi.vm.sade.valinta.kooste.security;
 
+import fi.vm.sade.valinta.kooste.OrganisaatioOikeuksienTarkistus;
+import fi.vm.sade.valinta.kooste.external.resource.organisaatio.OrganisaatioAsyncResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
 /**
  * Created by jussija on 09/01/15.
  */
-public class HasOrganizationImpl {
+@Service("hasOrganization")
+@Scope("session")
+public class HasOrganizationImpl implements HasOrganization {
+    private static final Logger LOG = LoggerFactory.getLogger(HasOrganizationImpl.class);
+    @Autowired
+    private OrganisaatioAsyncResource organisaatioAsyncResource;
+
+    public boolean hasOrganization(String tarjoajaOid) throws Exception {
+        LOG.error("Has Organisation tarkistus!");
+        try {
+            return OrganisaatioOikeuksienTarkistus.tarkistaKayttooikeudet(organisaatioAsyncResource.haeOrganisaationOidKetju(tarjoajaOid).get());
+        } catch(Exception e) {
+            LOG.error("Has Organisation tarkistus epäonnistui! {} {}", e.getMessage(), Arrays.asList(e.getStackTrace()));
+            throw e;
+        }
+    }
 }
