d0c9349c0deb58df5982be32ac511b1da0f52182
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-02-05 10:14:31 +0200

    alustava asynkroninen auth util

---
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/authentication/AuthorizationAsyncResource.java
@@ -0,0 +1,7 @@
+package fi.vm.sade.valinta.kooste.external.resource.authentication;
+
+/**
+ * @author Jussi Jartamo
+ */
+public interface AuthorizationAsyncResource {
+}
---
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/authentication/impl/AuthorizationAsyncResourceImpl.java
@@ -0,0 +1,7 @@
+package fi.vm.sade.valinta.kooste.external.resource.authentication.impl;
+
+/**
+ * @author Jussi Jartamo
+ */
+public class AuthorizationAsyncResourceImpl {
+}
