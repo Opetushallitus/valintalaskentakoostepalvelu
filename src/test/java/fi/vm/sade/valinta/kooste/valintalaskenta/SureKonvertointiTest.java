e1b1799035af9c009ff35592aa591f3ff34ac449
kjsaila <kalle.saila@gofore.com>
2015-04-02 13:26:07 +0300

    Konvertointitesti

--- src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/SureKonvertointiTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/SureKonvertointiTest.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametriDTO;
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
 import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.dto.*;
+import fi.vm.sade.valinta.kooste.util.sure.YoToAvainSuoritustietoDTOConverter;
 import fi.vm.sade.valintalaskenta.domain.dto.AvainArvoDTO;
+import fi.vm.sade.valintalaskenta.domain.dto.AvainMetatiedotDTO;
 import org.apache.commons.io.IOUtils;
 import org.joda.time.DateTime;
 import org.junit.Assert;
@@ -93,5 +96,48 @@ public class SureKonvertointiTest {
 			}
 		}
 	}
+
+
+    @Test
+    public void testaaSureKonvertointiKaikkiAIneet() throws JsonSyntaxException,
+            IOException {
+        List<Oppija> oppijat = new Gson().fromJson(IOUtils
+                .toString(new ClassPathResource("sure_kaikki_aineet.json")
+                        .getInputStream()), new TypeToken<List<Oppija>>() {
+        }.getType());
+
+        Oppija oppija = oppijat.get(0);
+        final List<AvainMetatiedotDTO> konvertoitu = YoToAvainSuoritustietoDTOConverter.convert(oppija);
+
+        final List<String> aineet = Arrays.asList("UE", "UO", "ET", "FF", "PS",
+                "HI", "FY", "KE", "BI", "GE", "TE", "YH",
+                "REAALI", "AINEREAALI", "PITKA_KIELI", "KESKIPITKA_KIELI", "LYHYT_KIELI", "AIDINKIELI",
+                "O", "O5", "A", "A5", "BA", "BB", "CA", "CB", "CC", "DC", "EA", "EB", "EC", "FA", "FB",
+                "FC", "GA", "GB", "GC", "HA", "HB", "I", "W", "IC", "QC", "J", "KC", "L1", "L7", "M", "N",
+                "PA", "PB", "PC", "RR", "RO", "RY", "SA", "SB", "SC", "S9", "TA", "TB", "TC", "VA", "VB",
+                "VC", "Z", "UE", "UO", "ET", "FF", "PS", "HI", "FY", "KE", "BI", "GE", "TE", "YH");
+
+
+        aineet.forEach(a -> {
+            final long count = konvertoitu.stream().filter(k -> k.getAvain().equals(a)).count();
+            Assert.assertEquals(count, 1);
+        });
+
+        final int ainereaali = konvertoitu.stream().filter(k -> k.getAvain().equals("AINEREAALI")).findFirst().get().getMetatiedot().size();
+        final int reaali = konvertoitu.stream().filter(k -> k.getAvain().equals("REAALI")).findFirst().get().getMetatiedot().size();
+        final int pitka_kieli = konvertoitu.stream().filter(k -> k.getAvain().equals("PITKA_KIELI")).findFirst().get().getMetatiedot().size();
+        final int keskipitka_kieli = konvertoitu.stream().filter(k -> k.getAvain().equals("KESKIPITKA_KIELI")).findFirst().get().getMetatiedot().size();
+        final int lyhyt_kieli = konvertoitu.stream().filter(k -> k.getAvain().equals("LYHYT_KIELI")).findFirst().get().getMetatiedot().size();
+        final int aidinkieli = konvertoitu.stream().filter(k -> k.getAvain().equals("AIDINKIELI")).findFirst().get().getMetatiedot().size();
+
+        Assert.assertEquals(12, ainereaali);
+        Assert.assertEquals(3, reaali);
+        Assert.assertEquals(9, pitka_kieli);
+        Assert.assertEquals(8, keskipitka_kieli);
+        Assert.assertEquals(10, lyhyt_kieli);
+        Assert.assertEquals(7, aidinkieli);
+
+
+    }
 
 }
