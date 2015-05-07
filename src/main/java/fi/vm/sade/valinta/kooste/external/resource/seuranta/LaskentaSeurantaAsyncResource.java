b9f2db6965539b0bacdbf2615d5f83f53c7e99b8
Kari Husa <kari.husa@reaktor.fi>
2015-05-07 14:23:04 +0300

    Format and remove unused imports

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/seuranta/LaskentaSeurantaAsyncResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/seuranta/LaskentaSeurantaAsyncResource.java
@@ -1,30 +1,14 @@
 package fi.vm.sade.valinta.kooste.external.resource.seuranta;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.client.InvocationCallback;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import fi.vm.sade.valinta.kooste.external.resource.Callback;
 import fi.vm.sade.valinta.seuranta.dto.HakukohdeDto;
 import fi.vm.sade.valinta.seuranta.dto.HakukohdeTila;
 import fi.vm.sade.valinta.seuranta.dto.IlmoitusDto;
 import fi.vm.sade.valinta.seuranta.dto.LaskentaDto;
 import fi.vm.sade.valinta.seuranta.dto.LaskentaTila;
 import fi.vm.sade.valinta.seuranta.dto.LaskentaTyyppi;
-import fi.vm.sade.valinta.seuranta.dto.YhteenvetoDto;
 
 /**
  * 
@@ -33,28 +17,25 @@ import fi.vm.sade.valinta.seuranta.dto.YhteenvetoDto;
  */
 public interface LaskentaSeurantaAsyncResource {
 
-	void laskenta(String uuid, Consumer<LaskentaDto> callback,
-			Consumer<Throwable> failureCallback);
+	void laskenta(String uuid, Consumer<LaskentaDto> callback, Consumer<Throwable> failureCallback);
 
-	void resetoiTilat(String uuid, Consumer<LaskentaDto> callback,
-			Consumer<Throwable> failureCallback);
+	void resetoiTilat(String uuid, Consumer<LaskentaDto> callback, Consumer<Throwable> failureCallback);
 
-	void luoLaskenta(String hakuOid, LaskentaTyyppi tyyppi,
+	void luoLaskenta(
+			String hakuOid,
+			LaskentaTyyppi tyyppi,
 			Boolean erillishaku,
-			Integer valinnanvaihe, Boolean valintakoelaskenta,
-			List<HakukohdeDto> hakukohdeOids, Consumer<String> callback,
+			Integer valinnanvaihe,
+			Boolean valintakoelaskenta,
+			List<HakukohdeDto> hakukohdeOids,
+			Consumer<String> callback,
 			Consumer<Throwable> failureCallback);
 
-	//void haeAsync(String hakuOid, Consumer<Collection<YhteenvetoDto>> callback);
+	void lisaaIlmoitusHakukohteelle(String uuid, String hakukohdeOid, IlmoitusDto ilmoitus);
 
-	void lisaaIlmoitusHakukohteelle(String uuid, String hakukohdeOid,
-			IlmoitusDto ilmoitus);
+	void merkkaaHakukohteenTila(String uuid, String hakukohdeOid, HakukohdeTila tila);
 
-	void merkkaaHakukohteenTila(String uuid, String hakukohdeOid,
-			HakukohdeTila tila);
-
-	void merkkaaLaskennanTila(String uuid, LaskentaTila tila,
-			HakukohdeTila hakukohdetila);
+	void merkkaaLaskennanTila(String uuid, LaskentaTila tila, HakukohdeTila hakukohdetila);
 
 	void merkkaaLaskennanTila(String uuid, LaskentaTila tila);
 }
