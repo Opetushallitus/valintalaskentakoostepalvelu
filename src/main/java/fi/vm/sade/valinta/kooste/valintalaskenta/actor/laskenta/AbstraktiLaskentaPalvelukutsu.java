b3f1b6e1deb55142dd841ff8bbfca1746eddf370
Kari Husa <kari.husa@reaktor.fi>
2015-05-26 10:40:46 +0300

    Format, remove unused imports and simplify

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/AbstraktiLaskentaPalvelukutsu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/AbstraktiLaskentaPalvelukutsu.java
@@ -1,13 +1,9 @@
 package fi.vm.sade.valinta.kooste.valintalaskenta.actor.laskenta;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.UuidHakukohdeJaOrganisaatio;
@@ -15,13 +11,8 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
-
-import fi.vm.sade.valinta.kooste.external.resource.haku.dto.ApplicationAdditionalDataDTO;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
 import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.dto.Oppija;
-import fi.vm.sade.valinta.kooste.util.Converter;
-import fi.vm.sade.valinta.kooste.util.OppijaToAvainArvoDTOConverter;
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.laskenta.palvelukutsu.AbstraktiPalvelukutsu;
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.laskenta.palvelukutsu.Palvelukutsu;
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.laskenta.palvelukutsu.PalvelukutsuLaskuri;
@@ -30,6 +21,7 @@ import fi.vm.sade.valinta.kooste.valintalaskenta.util.HakemuksetConverterUtil;
 import fi.vm.sade.valinta.seuranta.dto.HakukohdeTila;
 import fi.vm.sade.valintalaskenta.domain.dto.HakemusDTO;
 
+
 /**
  * @author Jussi Jartamo
  *         <p/>
@@ -37,38 +29,28 @@ import fi.vm.sade.valintalaskenta.domain.dto.HakemusDTO;
  *         Varsinainen laskennan toteuttaja tekee laskennan ja laskentaDTO:n
  *         luonnin.
  */
-public abstract class AbstraktiLaskentaPalvelukutsu extends
-        AbstraktiPalvelukutsu implements LaskentaPalvelukutsu {
-    private final Logger LOG = LoggerFactory
-            .getLogger(AbstraktiLaskentaPalvelukutsu.class);
+public abstract class AbstraktiLaskentaPalvelukutsu extends AbstraktiPalvelukutsu implements LaskentaPalvelukutsu {
+    private final Logger LOG = LoggerFactory.getLogger(AbstraktiLaskentaPalvelukutsu.class);
+
     private final Collection<PalvelukutsuJaPalvelukutsuStrategia> palvelukutsut;
     private final Consumer<Palvelukutsu> laskuri;
     private final ParametritDTO parametritDTO;
     private volatile HakukohdeTila tila = HakukohdeTila.TEKEMATTA;
     private final AtomicReference<Consumer<LaskentaPalvelukutsu>> takaisinkutsu;
 
-    public AbstraktiLaskentaPalvelukutsu(
-            ParametritDTO parametritDTO,
-            UuidHakukohdeJaOrganisaatio kuvaus,
-            Collection<PalvelukutsuJaPalvelukutsuStrategia> palvelukutsut) {
+    public AbstraktiLaskentaPalvelukutsu(ParametritDTO parametritDTO, UuidHakukohdeJaOrganisaatio kuvaus, Collection<PalvelukutsuJaPalvelukutsuStrategia> palvelukutsut) {
         super(kuvaus);
         this.parametritDTO = parametritDTO;
         this.palvelukutsut = palvelukutsut;
         this.takaisinkutsu = new AtomicReference<>();
-        final PalvelukutsuLaskuri palvelukutsulaskuri = new PalvelukutsuLaskuri(
-                palvelukutsut.size());
+        final PalvelukutsuLaskuri palvelukutsulaskuri = new PalvelukutsuLaskuri(palvelukutsut.size());
         this.laskuri = pk -> {
             yksiVaiheValmistui();
             if (takaisinkutsu.get() == null) {
                 return;
             }
-            if (pk.onkoPeruutettu()) { // peruutetaan laskenta talle
-                // hakukohteelle
-                try {
-                    peruuta();
-                } catch (Exception e) {
-
-                }
+            if (pk.onkoPeruutettu()) {
+                peruuta();
                 try {
                     takaisinkutsu.getAndUpdate(tk -> {
                         if (tk != null) {
@@ -77,9 +59,7 @@ public abstract class AbstraktiLaskentaPalvelukutsu extends
                         return null;
                     });
                 } catch (Exception e) {
-                    LOG.error(
-                            "Laskentapalvelukutsun takaisinkutsu epaonnistui {}",
-                            e.getMessage());
+                    LOG.error("Laskentapalvelukutsun takaisinkutsu epaonnistui {}", e.getMessage());
                     throw e;
                 }
 
@@ -87,10 +67,7 @@ public abstract class AbstraktiLaskentaPalvelukutsu extends
                 int yhteensa = palvelukutsulaskuri.getYhteensa();
                 int laskuriNyt = palvelukutsulaskuri.palvelukutsuSaapui();
 
-                LOG.info("Saatiin {} (uuid={},hakukohde/tunniste={}): {}/{}",
-                        StringUtils.rightPad(pk.getClass()
-                                .getSimpleName(), 38), getUuid(), getHakukohdeOid(), (-laskuriNyt)
-                                + yhteensa, yhteensa);
+                LOG.info("Saatiin {} (uuid={},hakukohde/tunniste={}): {}/{}", StringUtils.rightPad(pk.getClass().getSimpleName(), 38), getUuid(), getHakukohdeOid(), (-laskuriNyt) + yhteensa, yhteensa);
                 if (laskuriNyt == 0) {
                     tila = HakukohdeTila.VALMIS;
                     try {
@@ -101,15 +78,12 @@ public abstract class AbstraktiLaskentaPalvelukutsu extends
                             return null;
                         });
                     } catch (Exception e) {
-                        LOG.error(
-                                "Laskentapalvelukutsun takaisinkutsu epaonnistui {}",
-                                e.getMessage());
+                        LOG.error("Laskentapalvelukutsun takaisinkutsu epaonnistui {}", e.getMessage());
                         throw e;
                     }
                 } else if (laskuriNyt < 0) {
                     LOG.error("Laskenta sai enemman paluuarvoja palvelukutsuista kuin kutsuja tehtiin!");
-                    throw new RuntimeException(
-                            "Laskenta sai enemman paluuarvoja palvelukutsuista kuin kutsuja tehtiin!");
+                    throw new RuntimeException("Laskenta sai enemman paluuarvoja palvelukutsuista kuin kutsuja tehtiin!");
                 }
             }
         };
@@ -129,32 +103,24 @@ public abstract class AbstraktiLaskentaPalvelukutsu extends
     public void peruuta() {
         try {
             super.peruuta();
-            palvelukutsut.forEach(pk -> pk.peruuta());
+            palvelukutsut.forEach(PalvelukutsuJaPalvelukutsuStrategia::peruuta);
         } catch (Exception e) {
-            LOG.error(
-                    "AbstraktiLaskentaPalvelukutsun peruutus epaonnistui! {}",
-                    e.getMessage());
+            LOG.error("AbstraktiLaskentaPalvelukutsun peruutus epaonnistui! {}", e.getMessage());
         }
     }
 
     @Override
     public HakukohdeTila getHakukohdeTila() {
-        if (onkoPeruutettu()) {
-            return HakukohdeTila.KESKEYTETTY;
-        }
-        return tila;
+        return onkoPeruutettu() ? HakukohdeTila.KESKEYTETTY : tila;
     }
 
     public void laitaTyojonoon(Consumer<LaskentaPalvelukutsu> takaisinkutsu) {
         LOG.info("Laitetaan tyot tyojonoon hakukohteelle {}", getHakukohdeOid());
         this.takaisinkutsu.set(takaisinkutsu);
-        palvelukutsut.forEach(tyojono -> tyojono
-                .laitaPalvelukutsuTyojonoon(laskuri));
+        palvelukutsut.forEach(tyojono -> tyojono.laitaPalvelukutsuTyojonoon(laskuri));
     }
 
-    protected List<HakemusDTO> muodostaHakemuksetDTO(String hakukohdeOid,
-                                                     List<Hakemus> hakemukset, List<Oppija> oppijat) {
-        return HakemuksetConverterUtil.muodostaHakemuksetDTO(hakukohdeOid,
-                hakemukset, oppijat, parametritDTO);
+    protected List<HakemusDTO> muodostaHakemuksetDTO(String hakukohdeOid, List<Hakemus> hakemukset, List<Oppija> oppijat) {
+        return HakemuksetConverterUtil.muodostaHakemuksetDTO(hakukohdeOid, hakemukset, oppijat, parametritDTO);
     }
 }
