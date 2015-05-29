213019390800a11ead9018b1aed5ceb880b4010d
Kari Husa <kari.husa@reaktor.fi>
2015-05-29 09:51:51 +0300

    Simplify, format and remove unused imports

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/palvelukutsu/ValintaperusteetPalvelukutsu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/palvelukutsu/ValintaperusteetPalvelukutsu.java
@@ -3,7 +3,6 @@ package fi.vm.sade.valinta.kooste.valintalaskenta.actor.laskenta.palvelukutsu;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import fi.vm.sade.service.valintaperusteet.dto.ValintatapajonoJarjestyskriteereillaDTO;
@@ -12,24 +11,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetDTO;
-import fi.vm.sade.valinta.kooste.external.resource.Peruutettava;
 import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
-import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.HakukohdeJaOrganisaatio;
+
 
 /**
  * @author Jussi Jartamo
  */
-public class ValintaperusteetPalvelukutsu extends AbstraktiPalvelukutsu
-        implements Palvelukutsu {
-    private final Logger LOG = LoggerFactory
-            .getLogger(ValintaperusteetPalvelukutsu.class);
+public class ValintaperusteetPalvelukutsu extends AbstraktiPalvelukutsu implements Palvelukutsu {
+    private final Logger LOG = LoggerFactory.getLogger(ValintaperusteetPalvelukutsu.class);
+
     private final Integer valinnanVaiheJarjestysluku;
     private final ValintaperusteetAsyncResource valintaperusteetAsyncResource;
     private final AtomicReference<List<ValintaperusteetDTO>> valintaperusteet;
 
-    public ValintaperusteetPalvelukutsu(UuidHakukohdeJaOrganisaatio hakukohdeOid,
-                                        Integer valinnanVaiheJarjestysluku,
-                                        ValintaperusteetAsyncResource valintaperusteetAsyncResource) {
+    public ValintaperusteetPalvelukutsu(UuidHakukohdeJaOrganisaatio hakukohdeOid, Integer valinnanVaiheJarjestysluku, ValintaperusteetAsyncResource valintaperusteetAsyncResource) {
         super(hakukohdeOid);
         this.valinnanVaiheJarjestysluku = valinnanVaiheJarjestysluku;
         this.valintaperusteetAsyncResource = valintaperusteetAsyncResource;
@@ -42,37 +37,31 @@ public class ValintaperusteetPalvelukutsu extends AbstraktiPalvelukutsu
     }
 
     public Palvelukutsu teePalvelukutsu(Consumer<Palvelukutsu> takaisinkutsu) {
-        aloitaPalvelukutsuJosPalvelukutsuaEiOlePeruutettu(new Supplier<Peruutettava>() {
-            public Peruutettava get() {
-                return valintaperusteetAsyncResource
-                        .haeValintaperusteet(
-                                getHakukohdeOid(),
-                                valinnanVaiheJarjestysluku,
-                                valintaperusteet -> {
-                                    if (valintaperusteet == null) {
-                                        LOG.error("Valintaperusteetpalvelu palautti null datajoukon!");
-                                        failureCallback(takaisinkutsu);
-                                        return;
-                                    }
+        aloitaPalvelukutsuJosPalvelukutsuaEiOlePeruutettu(() -> valintaperusteetAsyncResource.haeValintaperusteet(
+                getHakukohdeOid(),
+                valinnanVaiheJarjestysluku,
+                valintaperusteet -> {
+                    if (valintaperusteet == null) {
+                        LOG.error("Valintaperusteetpalvelu palautti null datajoukon!");
+                        failureCallback(takaisinkutsu);
+                        return;
+                    }
 
-                                    // Filteröidään pois jonot, joissa ei käytetä laskentaa
-                                    final List<ValintaperusteetDTO> filteroidyt = valintaperusteet.stream().map(vp -> {
-                                        List<ValintatapajonoJarjestyskriteereillaDTO> laskentaJonot = vp.getValinnanVaihe()
-                                                .getValintatapajono()
-                                                .stream()
-                                                .filter(j -> j.getKaytetaanValintalaskentaa() == null || j.getKaytetaanValintalaskentaa())
-                                                .collect(Collectors.toList());
-                                        vp.getValinnanVaihe().setValintatapajono(laskentaJonot);
-                                        return vp;
-                                    }).collect(Collectors.toList());
+                    // Filteröidään pois jonot, joissa ei käytetä laskentaa
+                    final List<ValintaperusteetDTO> filteroidyt = valintaperusteet.stream()
+                            .map(vp -> {
+                                List<ValintatapajonoJarjestyskriteereillaDTO> laskentaJonot = vp.getValinnanVaihe().getValintatapajono().stream()
+                                        .filter(j -> j.getKaytetaanValintalaskentaa() == null || j.getKaytetaanValintalaskentaa())
+                                        .collect(Collectors.toList());
+                                vp.getValinnanVaihe().setValintatapajono(laskentaJonot);
+                                return vp;
+                            }).collect(Collectors.toList());
 
-                                    ValintaperusteetPalvelukutsu.this.valintaperusteet
-                                            .set(filteroidyt);
-                                    takaisinkutsu
-                                            .accept(ValintaperusteetPalvelukutsu.this);
-                                }, failureCallback(takaisinkutsu));
-            }
-        });
+                    ValintaperusteetPalvelukutsu.this.valintaperusteet.set(filteroidyt);
+                    takaisinkutsu.accept(ValintaperusteetPalvelukutsu.this);
+                },
+                failureCallback(takaisinkutsu))
+        );
         return this;
     }
 
