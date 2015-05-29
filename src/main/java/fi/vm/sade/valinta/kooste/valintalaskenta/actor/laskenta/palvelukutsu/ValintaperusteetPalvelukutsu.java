6862078f2d8978654dbc2ff10c46290f83f5c011
Kari Husa <kari.husa@reaktor.fi>
2015-05-29 09:50:03 +0300

    Tabs to spaces

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/palvelukutsu/ValintaperusteetPalvelukutsu.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/laskenta/palvelukutsu/ValintaperusteetPalvelukutsu.java
@@ -17,45 +17,43 @@ import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.Valintaperus
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.HakukohdeJaOrganisaatio;
 
 /**
- * 
  * @author Jussi Jartamo
- * 
  */
 public class ValintaperusteetPalvelukutsu extends AbstraktiPalvelukutsu
-		implements Palvelukutsu {
-	private final Logger LOG = LoggerFactory
-			.getLogger(ValintaperusteetPalvelukutsu.class);
-	private final Integer valinnanVaiheJarjestysluku;
-	private final ValintaperusteetAsyncResource valintaperusteetAsyncResource;
-	private final AtomicReference<List<ValintaperusteetDTO>> valintaperusteet;
+        implements Palvelukutsu {
+    private final Logger LOG = LoggerFactory
+            .getLogger(ValintaperusteetPalvelukutsu.class);
+    private final Integer valinnanVaiheJarjestysluku;
+    private final ValintaperusteetAsyncResource valintaperusteetAsyncResource;
+    private final AtomicReference<List<ValintaperusteetDTO>> valintaperusteet;
 
-	public ValintaperusteetPalvelukutsu(UuidHakukohdeJaOrganisaatio hakukohdeOid,
-			Integer valinnanVaiheJarjestysluku,
-			ValintaperusteetAsyncResource valintaperusteetAsyncResource) {
-		super(hakukohdeOid);
-		this.valinnanVaiheJarjestysluku = valinnanVaiheJarjestysluku;
-		this.valintaperusteetAsyncResource = valintaperusteetAsyncResource;
-		this.valintaperusteet = new AtomicReference<>();
-	}
+    public ValintaperusteetPalvelukutsu(UuidHakukohdeJaOrganisaatio hakukohdeOid,
+                                        Integer valinnanVaiheJarjestysluku,
+                                        ValintaperusteetAsyncResource valintaperusteetAsyncResource) {
+        super(hakukohdeOid);
+        this.valinnanVaiheJarjestysluku = valinnanVaiheJarjestysluku;
+        this.valintaperusteetAsyncResource = valintaperusteetAsyncResource;
+        this.valintaperusteet = new AtomicReference<>();
+    }
 
-	@Override
-	public void vapautaResurssit() {
-		valintaperusteet.set(null);
-	}
+    @Override
+    public void vapautaResurssit() {
+        valintaperusteet.set(null);
+    }
 
-	public Palvelukutsu teePalvelukutsu(Consumer<Palvelukutsu> takaisinkutsu) {
-		aloitaPalvelukutsuJosPalvelukutsuaEiOlePeruutettu(new Supplier<Peruutettava>() {
-			public Peruutettava get() {
-				return valintaperusteetAsyncResource
-						.haeValintaperusteet(
-								getHakukohdeOid(),
-								valinnanVaiheJarjestysluku,
-								valintaperusteet -> {
-									if (valintaperusteet == null) {
-										LOG.error("Valintaperusteetpalvelu palautti null datajoukon!");
-										failureCallback(takaisinkutsu);
-										return;
-									}
+    public Palvelukutsu teePalvelukutsu(Consumer<Palvelukutsu> takaisinkutsu) {
+        aloitaPalvelukutsuJosPalvelukutsuaEiOlePeruutettu(new Supplier<Peruutettava>() {
+            public Peruutettava get() {
+                return valintaperusteetAsyncResource
+                        .haeValintaperusteet(
+                                getHakukohdeOid(),
+                                valinnanVaiheJarjestysluku,
+                                valintaperusteet -> {
+                                    if (valintaperusteet == null) {
+                                        LOG.error("Valintaperusteetpalvelu palautti null datajoukon!");
+                                        failureCallback(takaisinkutsu);
+                                        return;
+                                    }
 
                                     // Filteröidään pois jonot, joissa ei käytetä laskentaa
                                     final List<ValintaperusteetDTO> filteroidyt = valintaperusteet.stream().map(vp -> {
@@ -68,18 +66,18 @@ public class ValintaperusteetPalvelukutsu extends AbstraktiPalvelukutsu
                                         return vp;
                                     }).collect(Collectors.toList());
 
-									ValintaperusteetPalvelukutsu.this.valintaperusteet
-											.set(filteroidyt);
-									takaisinkutsu
-											.accept(ValintaperusteetPalvelukutsu.this);
-								}, failureCallback(takaisinkutsu));
-			}
-		});
-		return this;
-	}
+                                    ValintaperusteetPalvelukutsu.this.valintaperusteet
+                                            .set(filteroidyt);
+                                    takaisinkutsu
+                                            .accept(ValintaperusteetPalvelukutsu.this);
+                                }, failureCallback(takaisinkutsu));
+            }
+        });
+        return this;
+    }
 
-	public List<ValintaperusteetDTO> getValintaperusteet() {
-		return valintaperusteet.get();
-	}
+    public List<ValintaperusteetDTO> getValintaperusteet() {
+        return valintaperusteet.get();
+    }
 
 }
