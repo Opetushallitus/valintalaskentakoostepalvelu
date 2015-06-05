6691ed0afd305394cbc8653f1ea0d4f1d1e850b8
Juha Paananen <juha.paananen@gmail.com>
2015-06-05 12:30:49 +0300

    allow using real spring context in ValintaKoosteJetty, when run standalone without system properties

--- src/test/java/fi/vm/sade/valinta/kooste/ValintaKoosteJetty.java
+++ src/test/java/fi/vm/sade/valinta/kooste/ValintaKoosteJetty.java
@@ -7,23 +7,34 @@ import fi.vm.sade.integrationtest.util.PortChecker;
 import fi.vm.sade.integrationtest.util.ProjectRootFinder;
 
 /**
- * @author Jussi Jartamo
+ *  Starts valintalaskentakoostepalvelu locally.
+ *
+ *  Use -Dvalintakooste.profile=it for local mocks.
  */
 public class ValintaKoosteJetty {
     public final static int port = Integer.parseInt(System.getProperty("valintakooste.port", String.valueOf(PortChecker.findFreeLocalPort())));
     private static Server server = new Server(port);
 
     public static void main(String[] args) throws Exception{
-        startShared();
+        startShared("it".equals(System.getProperty("valintakooste.profile")));
     }
 
     public static void startShared() {
+        startShared(true);
+    }
+
+    public static void startShared(boolean useMocks) {
         try {
             if (server.isStopped()) {
-                String root = ProjectRootFinder.findProjectRoot() + "/valintalaskentakoostepalvelu";
+                String root =  ProjectRootFinder.findProjectRoot() + "/valintalaskentakoostepalvelu";
                 WebAppContext wac = new WebAppContext();
-                wac.setResourceBase(root);
-                wac.setDescriptor(root + "/src/test/resources/it-profile-web.xml");
+                if (useMocks) {
+                    wac.setResourceBase(root);
+                    wac.setDescriptor(root + "/src/test/resources/it-profile-web.xml");
+                } else {
+                    wac.setResourceBase(root + "/src/main/webapp");
+                }
+
                 wac.setContextPath("/valintalaskentakoostepalvelu");
                 wac.setParentLoaderPriority(true);
                 server.setHandler(wac);
@@ -34,9 +45,4 @@ public class ValintaKoosteJetty {
             throw new RuntimeException(t);
         }
     }
-
-    public static void stop() throws Exception {
-        server.stop();
-    }
-
 }
