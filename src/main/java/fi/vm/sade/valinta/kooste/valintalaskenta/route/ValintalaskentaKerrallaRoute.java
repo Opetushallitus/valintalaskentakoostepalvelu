c0028628ea98b286558b8312c70be95153e479c7
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 09:14:31 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/route/ValintalaskentaKerrallaRoute.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/route/ValintalaskentaKerrallaRoute.java
@@ -4,10 +4,8 @@ import fi.vm.sade.tarjonta.service.resources.v1.dto.HakuV1RDTO;
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
 import fi.vm.sade.valinta.kooste.valintalaskenta.dto.LaskentaStartParams;
 
-/**
- * @author Jussi Jartamo.
- */
 public interface ValintalaskentaKerrallaRoute {
-	void suoritaValintalaskentaKerralla(final HakuV1RDTO haku, final ParametritDTO parametritDTO,LaskentaStartParams laskentaStartParams);
-	void workAvailable();
+    void suoritaValintalaskentaKerralla(final HakuV1RDTO haku, final ParametritDTO parametritDTO, LaskentaStartParams laskentaStartParams);
+
+    void workAvailable();
 }
--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/route/ValintalaskentaKerrallaRouteValvomo.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/route/ValintalaskentaKerrallaRouteValvomo.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 import fi.vm.sade.valinta.kooste.valintalaskenta.dto.Laskenta;
 
-/**
- * @author Jussi Jartamo.
- */
 public interface ValintalaskentaKerrallaRouteValvomo {
+    Laskenta fetchLaskenta(String uuid);
 
-	Laskenta fetchLaskenta(String uuid);
-
-	List<Laskenta> runningLaskentas();
+    List<Laskenta> runningLaskentas();
 }
