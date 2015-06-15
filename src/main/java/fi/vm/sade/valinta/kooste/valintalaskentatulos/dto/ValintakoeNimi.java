de7be87414f84dc2300a5624c8facd44e7818a81
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 09:28:04 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/dto/ValintakoeNimi.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/dto/ValintakoeNimi.java
@@ -2,19 +2,19 @@ package fi.vm.sade.valinta.kooste.valintalaskentatulos.dto;
 
 public class ValintakoeNimi {
 
-	private final String nimi;
-	private final String selvitettyTunniste;
+    private final String nimi;
+    private final String selvitettyTunniste;
 
-	public ValintakoeNimi(String nimi, String selvitettyTunniste) {
-		this.nimi = nimi;
-		this.selvitettyTunniste = selvitettyTunniste;
-	}
+    public ValintakoeNimi(String nimi, String selvitettyTunniste) {
+        this.nimi = nimi;
+        this.selvitettyTunniste = selvitettyTunniste;
+    }
 
-	public String getNimi() {
-		return nimi;
-	}
+    public String getNimi() {
+        return nimi;
+    }
 
-	public String getSelvitettyTunniste() {
-		return selvitettyTunniste;
-	}
+    public String getSelvitettyTunniste() {
+        return selvitettyTunniste;
+    }
 }
--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/dto/ValintakoeRivi.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/dto/ValintakoeRivi.java
@@ -16,131 +16,125 @@ import fi.vm.sade.valinta.kooste.hakemus.dto.Yhteystiedot;
 import fi.vm.sade.valinta.kooste.util.ExcelExportUtil;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Osoite;
 
-/**
- * 
- * @author Jussi Jartamo
- * 
- */
 public class ValintakoeRivi implements Comparable<ValintakoeRivi> {
+    private final String sukunimi;
+    private final String etunimet;
+    private final String hakemusOid;
+    private final String postitoimipaikka;
+    private final String asuinmaaEnglanniksi;
+    private final Osoite osoite;
+    private final Yhteystiedot yhteystiedot;
+    private final Date paivamaara;
+    private final Map<String, String> osallistumistiedot;
+    private final boolean osallistuuEdesYhteen;
 
-	private final String sukunimi;
-	private final String etunimet;
-	private final String hakemusOid;
-	private final String postitoimipaikka;
-	private final String asuinmaaEnglanniksi;
-	private final Osoite osoite;
-	private final Yhteystiedot yhteystiedot;
-	private final Date paivamaara;
-	private final Map<String, String> osallistumistiedot;
-	private final boolean osallistuuEdesYhteen;
+    private final HakemusWrapper wrapper;
 
-	private final HakemusWrapper wrapper;
+    public ValintakoeRivi(String sukunimi,
+                          String etunimet,
+                          String postitoimipaikka,
+                          String asuinmaaEnglanniksi,
+                          HakemusWrapper wrapper,
+                          String hakemusOid,
+                          Date paivamaara, Map<String, String> osallistumistiedot,
+                          Osoite osoite, Yhteystiedot yhteystiedot,
+                          boolean osallistuuEdesYhteen) {
+        this.sukunimi = StringUtils.trimToEmpty(sukunimi);
+        this.etunimet = StringUtils.trimToEmpty(etunimet);
+        this.asuinmaaEnglanniksi = asuinmaaEnglanniksi;
+        this.postitoimipaikka = postitoimipaikka;
+        this.hakemusOid = hakemusOid;
+        this.paivamaara = paivamaara;
+        this.yhteystiedot = yhteystiedot;
+        this.osoite = osoite;
+        this.osallistumistiedot = osallistumistiedot;
+        this.osallistuuEdesYhteen = osallistuuEdesYhteen;
+        this.wrapper = wrapper;
+    }
 
-	public ValintakoeRivi(String sukunimi,
-						  String etunimet,
-						  String postitoimipaikka,
-						  String asuinmaaEnglanniksi,
-			HakemusWrapper wrapper,
-						  String hakemusOid,
-			Date paivamaara, Map<String, String> osallistumistiedot,
-			Osoite osoite, Yhteystiedot yhteystiedot,
-			boolean osallistuuEdesYhteen) {
-		this.sukunimi = StringUtils.trimToEmpty(sukunimi);
-		this.etunimet = StringUtils.trimToEmpty(etunimet);
-		this.asuinmaaEnglanniksi = asuinmaaEnglanniksi;
-		this.postitoimipaikka = postitoimipaikka;
-		this.hakemusOid = hakemusOid;
-		this.paivamaara = paivamaara;
-		this.yhteystiedot = yhteystiedot;
-		this.osoite = osoite;
-		this.osallistumistiedot = osallistumistiedot;
-		this.osallistuuEdesYhteen = osallistuuEdesYhteen;
-		this.wrapper = wrapper;
-	}
+    @Override
+    public int compareTo(ValintakoeRivi o) {
+        int i = sukunimi.toUpperCase().compareTo(o.sukunimi.toUpperCase());
+        if (i == 0) {
+            return etunimet.toUpperCase().compareTo(o.etunimet.toUpperCase());
+        }
+        return i;
+    }
 
-	@Override
-	public int compareTo(ValintakoeRivi o) {
-		int i = sukunimi.toUpperCase().compareTo(o.sukunimi.toUpperCase());
-		if (i == 0) {
-			return etunimet.toUpperCase().compareTo(o.etunimet.toUpperCase());
-		}
-		return i;
-	}
+    public HakemusWrapper getWrapper() {
+        return wrapper;
+    }
 
-	public HakemusWrapper getWrapper() {
-		return wrapper;
-	}
+    public boolean isOsallistuuEdesYhteen() {
+        return osallistuuEdesYhteen;
+    }
 
-	public boolean isOsallistuuEdesYhteen() {
-		return osallistuuEdesYhteen;
-	}
+    public String getHakemusOid() {
+        return hakemusOid;
+    }
 
-	public String getHakemusOid() {
-		return hakemusOid;
-	}
+    public String getEtunimet() {
+        return etunimet;
+    }
 
-	public String getEtunimet() {
-		return etunimet;
-	}
+    public String getSukunimi() {
+        return sukunimi;
+    }
 
-	public String getSukunimi() {
-		return sukunimi;
-	}
+    public Date getPaivamaara() {
+        return paivamaara;
+    }
 
-	public Date getPaivamaara() {
-		return paivamaara;
-	}
+    public Map<String, String> getOsallistumistiedot() {
+        return osallistumistiedot;
+    }
 
-	public Map<String, String> getOsallistumistiedot() {
-		return osallistumistiedot;
-	}
+    public String[] toArray(List<String> valintakoeOids) {
+        ArrayList<String> rivi = new ArrayList<String>();
+        StringBuilder b = new StringBuilder();
+        b.append(sukunimi).append(", ").append(etunimet);
+        String pvm;
+        if (paivamaara != null) {
+            pvm = ExcelExportUtil.DATE_FORMAT.format(paivamaara);
+        } else {
+            pvm = StringUtils.EMPTY;
+        }
+        rivi.addAll(Arrays.asList(
+                sukunimi,
+                etunimet,
+                wrapper.getHenkilotunnus(),
+                wrapper.getSyntymaaika(),
+                wrapper.getSukupuoli(),
+                wrapper.getSuomalainenLahiosoite(),
+                wrapper.getSuomalainenPostinumero(),
+                postitoimipaikka,
+                wrapper.getUlkomainenLahiosoite(),
+                wrapper.getUlkomainenPostinumero(),
+                wrapper.getKaupunkiUlkomaa(),
+                asuinmaaEnglanniksi,
+                wrapper.getKansalaisuus(),
+                wrapper.getKansallinenId(),
+                wrapper.getPassinnumero(),
+                yhteystiedot.getSahkoposti(),
+                yhteystiedot.getPuhelinnumerotAsString(), hakemusOid, pvm));
+        // boolean osallistuuEdesYhteen = false;
+        for (String valintakoeOid : valintakoeOids) {
+            String o = osallistumistiedot.get(valintakoeOid);
+            if (o == null) {
+                rivi.add("----");
+            } else {
+                rivi.add(o);
+            }
+        }
+        return rivi.toArray(new String[]{});
+    }
 
-	public String[] toArray(List<String> valintakoeOids) {
-		ArrayList<String> rivi = new ArrayList<String>();
-		StringBuilder b = new StringBuilder();
-		b.append(sukunimi).append(", ").append(etunimet);
-		String pvm;
-		if (paivamaara != null) {
-			pvm = ExcelExportUtil.DATE_FORMAT.format(paivamaara);
-		} else {
-			pvm = StringUtils.EMPTY;
-		}
-		rivi.addAll(Arrays.asList(
-				sukunimi,
-				etunimet,
-				wrapper.getHenkilotunnus(),
-				wrapper.getSyntymaaika(),
-				wrapper.getSukupuoli(),
-				wrapper.getSuomalainenLahiosoite(),
-				wrapper.getSuomalainenPostinumero(),
-				postitoimipaikka,
-				wrapper.getUlkomainenLahiosoite(),
-				wrapper.getUlkomainenPostinumero(),
-				wrapper.getKaupunkiUlkomaa(),
-				asuinmaaEnglanniksi,
-				wrapper.getKansalaisuus(),
-				wrapper.getKansallinenId(),
-				wrapper.getPassinnumero(),
-				yhteystiedot.getSahkoposti(),
-				yhteystiedot.getPuhelinnumerotAsString(), hakemusOid, pvm));
-		// boolean osallistuuEdesYhteen = false;
-		for (String valintakoeOid : valintakoeOids) {
-			String o = osallistumistiedot.get(valintakoeOid);
-			if (o == null) {
-				rivi.add("----");
-			} else {
-				rivi.add(o);
-			}
-		}
-		return rivi.toArray(new String[] {});
-	}
-
-	public ValintakoeRivi merge(ValintakoeRivi v) {
-		Map<String, String> m = Maps.newHashMap(osallistumistiedot);
-		m.putAll(v.getOsallistumistiedot());
-		return new ValintakoeRivi(sukunimi, etunimet, postitoimipaikka, asuinmaaEnglanniksi, wrapper, hakemusOid, v.paivamaara,
-				m, osoite, yhteystiedot, osallistuuEdesYhteen
-						|| v.osallistuuEdesYhteen);
-	}
+    public ValintakoeRivi merge(ValintakoeRivi v) {
+        Map<String, String> m = Maps.newHashMap(osallistumistiedot);
+        m.putAll(v.getOsallistumistiedot());
+        return new ValintakoeRivi(sukunimi, etunimet, postitoimipaikka, asuinmaaEnglanniksi, wrapper, hakemusOid, v.paivamaara,
+                m, osoite, yhteystiedot, osallistuuEdesYhteen
+                || v.osallistuuEdesYhteen);
+    }
 
 }
