f83ad56232c64bbeaa578ea6c1ea4ed8e7746dea
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 11:17:27 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/resource/DokumenttiProsessiResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/resource/DokumenttiProsessiResource.java
@@ -21,9 +21,6 @@ import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.DokumenttiProsessi;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.komponentti.DokumenttiProsessiKomponentti;
 
 /**
- * 
- * @author Jussi Jartamo
- * 
  *         Palauttaa prosessoidut dokumentit resursseina
  */
 @Controller("DokumenttiProsessiResource")
@@ -32,29 +29,27 @@ import fi.vm.sade.valinta.kooste.viestintapalvelu.komponentti.DokumenttiProsessi
 @Api(value = "/dokumenttiprosessi", description = "Dokumenttien luontiin liittyvää palautetta käyttäjälle")
 public class DokumenttiProsessiResource {
 
-	@Autowired
-	private DokumenttiProsessiKomponentti dokumenttiProsessiKomponentti;
+    @Autowired
+    private DokumenttiProsessiKomponentti dokumenttiProsessiKomponentti;
 
-	@GET
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Palauttaa dokumenttiprosessin id:lle jos sellainen on muistissa", response = Response.class)
-	public DokumenttiProsessi hae(@PathParam("id") String id) {
-		return dokumenttiProsessiKomponentti.haeProsessi(id);
-	}
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Palauttaa dokumenttiprosessin id:lle jos sellainen on muistissa", response = Response.class)
+    public DokumenttiProsessi hae(@PathParam("id") String id) {
+        return dokumenttiProsessiKomponentti.haeProsessi(id);
+    }
 
-	@POST
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Ilmoittaa poikkeuksen prosessiin", response = Response.class)
-	public Response poikkeus(@PathParam("id") String id,
-			@QueryParam("poikkeus") String poikkeus) {
-		DokumenttiProsessi d = dokumenttiProsessiKomponentti.haeProsessi(id);
-		if (d != null) {
-			d.getPoikkeukset().add(
-					new Poikkeus("Dokumenttiprosessiresurssi",
-							"Poikkeuksen ilmoitus", poikkeus));
-		}
-		return Response.ok().build();
-	}
+    @POST
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Ilmoittaa poikkeuksen prosessiin", response = Response.class)
+    public Response poikkeus(@PathParam("id") String id,
+                             @QueryParam("poikkeus") String poikkeus) {
+        DokumenttiProsessi d = dokumenttiProsessiKomponentti.haeProsessi(id);
+        if (d != null) {
+            d.getPoikkeukset().add(new Poikkeus("Dokumenttiprosessiresurssi", "Poikkeuksen ilmoitus", poikkeus));
+        }
+        return Response.ok().build();
+    }
 }
