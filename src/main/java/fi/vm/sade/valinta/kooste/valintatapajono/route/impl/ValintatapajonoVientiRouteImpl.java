4115023d2074948c734479ecd1b6339ef2fc228c
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 09:48:00 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/valintatapajono/route/impl/ValintatapajonoVientiRouteImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/valintatapajono/route/impl/ValintatapajonoVientiRouteImpl.java
@@ -26,241 +26,134 @@ import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.Teksti;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.route.impl.AbstractDokumenttiRouteBuilder;
 import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.ValintatietoValinnanvaiheDTO;
 
-/**
- * 
- * @author Jussi Jartamo
- * 
- */
 @Component
-public class ValintatapajonoVientiRouteImpl extends
-		AbstractDokumenttiRouteBuilder {
-	private static final Logger LOG = LoggerFactory
-			.getLogger(ValintatapajonoVientiRouteImpl.class);
+public class ValintatapajonoVientiRouteImpl extends AbstractDokumenttiRouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(ValintatapajonoVientiRouteImpl.class);
 
-	private final ApplicationResource applicationResource;
-	private final DokumenttiResource dokumenttiResource;
-	private final HaeHakukohdeNimiTarjonnaltaKomponentti hakukohdeTarjonnalta;
-	private final HaeHakuTarjonnaltaKomponentti hakuTarjonnalta;
-	private final HakukohdeResource hakukohdeResource;
+    private final ApplicationResource applicationResource;
+    private final DokumenttiResource dokumenttiResource;
+    private final HaeHakukohdeNimiTarjonnaltaKomponentti hakukohdeTarjonnalta;
+    private final HaeHakuTarjonnaltaKomponentti hakuTarjonnalta;
+    private final HakukohdeResource hakukohdeResource;
 
-	@Autowired
-	public ValintatapajonoVientiRouteImpl(
-			ApplicationResource applicationResource,
-			DokumenttiResource dokumenttiResource,
-			HaeHakukohdeNimiTarjonnaltaKomponentti hakukohdeTarjonnalta,
-			HaeHakuTarjonnaltaKomponentti hakuTarjonnalta,
-			HakukohdeResource hakukohdeResource) {
-		this.applicationResource = applicationResource;
-		this.dokumenttiResource = dokumenttiResource;
-		this.hakukohdeTarjonnalta = hakukohdeTarjonnalta;
-		this.hakuTarjonnalta = hakuTarjonnalta;
-		this.hakukohdeResource = hakukohdeResource;
+    @Autowired
+    public ValintatapajonoVientiRouteImpl(
+            ApplicationResource applicationResource,
+            DokumenttiResource dokumenttiResource,
+            HaeHakukohdeNimiTarjonnaltaKomponentti hakukohdeTarjonnalta,
+            HaeHakuTarjonnaltaKomponentti hakuTarjonnalta,
+            HakukohdeResource hakukohdeResource) {
+        this.applicationResource = applicationResource;
+        this.dokumenttiResource = dokumenttiResource;
+        this.hakukohdeTarjonnalta = hakukohdeTarjonnalta;
+        this.hakuTarjonnalta = hakuTarjonnalta;
+        this.hakukohdeResource = hakukohdeResource;
+    }
 
-	}
-
-	@Override
-	public void configure() throws Exception {
-		Endpoint valintatapajonoVienti = endpoint(ValintatapajonoVientiRoute.SEDA_VALINTATAPAJONO_VIENTI);
-		Endpoint luontiEpaonnistui = endpoint("direct:valintatapajono_vienti_deadletterchannel");
-		from(valintatapajonoVienti)
-		//
-				.errorHandler(
-				//
-						deadLetterChannel(luontiEpaonnistui)
-								//
-								.maximumRedeliveries(0)
-								.logExhaustedMessageHistory(true)
-								.logExhausted(true).logStackTrace(true)
-								// hide retry/handled stacktrace
-								.logRetryStackTrace(false).logHandled(false))
-				//
-				.process(new Processor() {
-
-					@Override
-					public void process(Exchange exchange) throws Exception {
-						dokumenttiprosessi(exchange).setKokonaistyo(
-						// haun nimi ja hakukohteen nimi
-								1 + 1 +
-								// osallistumistiedot + valintaperusteet +
-								// hakemuspistetiedot
-										1 + 1
-										// luonti
-										+ 1
-										// dokumenttipalveluun vienti
-										+ 1);
-						String hakuOid = hakuOid(exchange);
-						String hakukohdeOid = hakukohdeOid(exchange);
-						String hakuNimi = new Teksti(hakuTarjonnalta.getHaku(
-								hakuOid).getNimi()).getTeksti();
-						dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
-						HakukohdeDTO hnimi = hakukohdeTarjonnalta
-								.haeHakukohdeNimi(hakukohdeOid);
-						dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
-						// String tarjoajaOid = hnimi.getTarjoajaOid();
-						String hakukohdeNimi = new Teksti(hnimi
-								.getHakukohdeNimi()).getTeksti();
-						// String tarjoajaNimi = new
-						// Teksti(hnimi.getTarjoajaNimi()).getTeksti();
-						//
-						//
-						//
-						String valintatapajonoOid = valintatapajonoOid(exchange);
-						if (hakukohdeOid == null || hakuOid == null
-								|| valintatapajonoOid == null) {
-							LOG.error(
-									"Pakolliset tiedot reitille puuttuu hakuOid = {}, hakukohdeOid = {}, valintatapajonoOid = {}",
-									hakuOid, hakukohdeOid, valintatapajonoOid);
-
-							dokumenttiprosessi(exchange).getPoikkeukset().add(
-									new Poikkeus(Poikkeus.KOOSTEPALVELU,
-											"Puutteelliset lähtötiedot"));
-							throw new RuntimeException(
-									"Pakolliset tiedot reitille puuttuu hakuOid, hakukohdeOid, valintatapajonoOid");
-						}
-						final List<Hakemus> hakemukset;
-
-						try {
-							hakemukset = applicationResource
-									.getApplicationsByOid(
-											hakuOid,
-											hakukohdeOid,
-											ApplicationResource.ACTIVE_AND_INCOMPLETE,
-											ApplicationResource.MAX);
-							LOG.debug("Saatiin hakemukset {}",
-									hakemukset.size());
-							dokumenttiprosessi(exchange)
-									.inkrementoiTehtyjaToita();
-						} catch (Exception e) {
-							LOG.error("Hakemuspalvelun virhe: {}\r\n{}",
-									e.getMessage(),
-									Arrays.toString(e.getStackTrace()));
-
-							dokumenttiprosessi(exchange)
-									.getPoikkeukset()
-									.add(new Poikkeus(
-											Poikkeus.HAKU,
-											"Hakemuspalvelulta ei saatu hakemuksia hakukohteelle",
-											""));
-							throw e;
-						}
-						if (hakemukset.isEmpty()) {
-							LOG.error("Nolla hakemusta!");
-
-							dokumenttiprosessi(exchange).getPoikkeukset().add(
-									new Poikkeus(Poikkeus.HAKU,
-											"Hakukohteella ei ole hakemuksia!",
-											""));
-							throw new RuntimeException(
-									"Hakukohteelle saatiin tyhjä hakemusjoukko!");
-						}
-						final List<ValintatietoValinnanvaiheDTO> valinnanvaiheet;
-						try {
-							valinnanvaiheet = hakukohdeResource
-									.hakukohde(hakukohdeOid);
-							dokumenttiprosessi(exchange)
-									.inkrementoiTehtyjaToita();
-						} catch (Exception e) {
-							LOG.error("Valinnanvaiheiden haku virhe: {}\r\n{}",
-									e.getMessage(),
-									Arrays.toString(e.getStackTrace()));
-
-							dokumenttiprosessi(exchange)
-									.getPoikkeukset()
-									.add(new Poikkeus(
-											Poikkeus.VALINTALASKENTA,
-											"Valintalaskennalta ei saatu valinnanvaiheita",
-											""));
-							throw e;
-						}
-						// if (valinnanvaiheet.isEmpty()) {
-						// LOG.error("Nolla valinnanvaihetta!");
-						//
-						// dokumenttiprosessi(exchange)
-						// .getPoikkeukset()
-						// .add(new Poikkeus(
-						// Poikkeus.VALINTALASKENTA,
-						// "Hakukohteelle ei löytynyt valinnanvaiheita!",
-						// ""));
-						// throw new RuntimeException(
-						// "Hakukohteelle ei löytynyt valinnanvaiheita!");
-						// }
-						InputStream xlsx;
-						try {
-							ValintatapajonoExcel valintatapajonoExcel = new ValintatapajonoExcel(
-									hakuOid, hakukohdeOid, valintatapajonoOid,
-									hakuNimi, hakukohdeNimi,
-									//
-									valinnanvaiheet, hakemukset);
-							xlsx = valintatapajonoExcel.getExcel().vieXlsx();
-							dokumenttiprosessi(exchange)
-									.inkrementoiTehtyjaToita();
-						} catch (Exception e) {
-							LOG.error(
-									"Valintatapajono excelin luonti virhe: {}\r\n{}",
-									e.getMessage(),
-									Arrays.toString(e.getStackTrace()));
-
-							dokumenttiprosessi(exchange)
-									.getPoikkeukset()
-									.add(new Poikkeus(
-											Poikkeus.KOOSTEPALVELU,
-											"Valintatapajono exceliä ei saatu luotua!",
-											""));
-							throw e;
-						}
-						try {
-							String id = generateId();
-							Long expirationTime = defaultExpirationDate()
-									.getTime();
-							List<String> tags = dokumenttiprosessi(exchange)
-									.getTags();
-							// LOG.error("Excelin tallennus");
-							dokumenttiResource.tallenna(id,
-									"valintatapajono.xlsx", expirationTime,
-									tags, "application/octet-stream", xlsx);
-							dokumenttiprosessi(exchange).setDokumenttiId(id);
-							dokumenttiprosessi(exchange)
-									.inkrementoiTehtyjaToita();
-						} catch (Exception e) {
-							LOG.error(
-									"Dokumenttipalveluun vienti virhe: {}\r\n{}",
-									e.getMessage(),
-									Arrays.toString(e.getStackTrace()));
-
-							dokumenttiprosessi(exchange)
-									.getPoikkeukset()
-									.add(new Poikkeus(
-											Poikkeus.DOKUMENTTIPALVELU,
-											"Dokumenttipalveluun ei saatu vietyä taulukkolaskentatiedostoa!",
-											""));
-							throw e;
-						}
-					}
-
-				})
-				//
-				.stop();
-
-		/**
-		 * DEAD LETTER CHANNEL
-		 */
-		from(luontiEpaonnistui)
-		//
-				.process(new Processor() {
-					public void process(Exchange exchange) throws Exception {
-						String syy;
-						if (exchange.getException() == null) {
-							syy = "Valintatapajonon taulukkolaskentaan vienti epäonnistui. Ota yheys ylläpitoon.";
-						} else {
-							syy = exchange.getException().getMessage();
-						}
-						dokumenttiprosessi(exchange).getPoikkeukset().add(
-								new Poikkeus(Poikkeus.KOOSTEPALVELU,
-										"Valintatapajonon vienti", syy));
-					}
-				})
-				//
-				.stop();
-	}
-
+    @Override
+    public void configure() throws Exception {
+        Endpoint valintatapajonoVienti = endpoint(ValintatapajonoVientiRoute.SEDA_VALINTATAPAJONO_VIENTI);
+        Endpoint luontiEpaonnistui = endpoint("direct:valintatapajono_vienti_deadletterchannel");
+        from(valintatapajonoVienti)
+                .errorHandler(
+                        deadLetterChannel(luontiEpaonnistui)
+                                .maximumRedeliveries(0)
+                                .logExhaustedMessageHistory(true)
+                                .logExhausted(true).logStackTrace(true)
+                                // hide retry/handled stacktrace
+                                .logRetryStackTrace(false).logHandled(false))
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        dokumenttiprosessi(exchange).setKokonaistyo(
+                                // haun nimi ja hakukohteen nimi
+                                1 + 1 +
+                                        // osallistumistiedot + valintaperusteet +
+                                        // hakemuspistetiedot
+                                        1 + 1
+                                        // luonti
+                                        + 1
+                                        // dokumenttipalveluun vienti
+                                        + 1);
+                        String hakuOid = hakuOid(exchange);
+                        String hakukohdeOid = hakukohdeOid(exchange);
+                        String hakuNimi = new Teksti(hakuTarjonnalta.getHaku(hakuOid).getNimi()).getTeksti();
+                        dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        HakukohdeDTO hnimi = hakukohdeTarjonnalta.haeHakukohdeNimi(hakukohdeOid);
+                        dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        String hakukohdeNimi = new Teksti(hnimi.getHakukohdeNimi()).getTeksti();
+                        String valintatapajonoOid = valintatapajonoOid(exchange);
+                        if (hakukohdeOid == null || hakuOid == null || valintatapajonoOid == null) {
+                            LOG.error("Pakolliset tiedot reitille puuttuu hakuOid = {}, hakukohdeOid = {}, valintatapajonoOid = {}", hakuOid, hakukohdeOid, valintatapajonoOid);
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.KOOSTEPALVELU, "Puutteelliset lähtötiedot"));
+                            throw new RuntimeException("Pakolliset tiedot reitille puuttuu hakuOid, hakukohdeOid, valintatapajonoOid");
+                        }
+                        final List<Hakemus> hakemukset;
+                        try {
+                            hakemukset = applicationResource.getApplicationsByOid(hakuOid, hakukohdeOid,
+                                    ApplicationResource.ACTIVE_AND_INCOMPLETE, ApplicationResource.MAX);
+                            LOG.debug("Saatiin hakemukset {}", hakemukset.size());
+                            dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        } catch (Exception e) {
+                            LOG.error("Hakemuspalvelun virhe: {}\r\n{}", e.getMessage(), Arrays.toString(e.getStackTrace()));
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.HAKU, "Hakemuspalvelulta ei saatu hakemuksia hakukohteelle", ""));
+                            throw e;
+                        }
+                        if (hakemukset.isEmpty()) {
+                            LOG.error("Nolla hakemusta!");
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.HAKU, "Hakukohteella ei ole hakemuksia!", ""));
+                            throw new RuntimeException("Hakukohteelle saatiin tyhjä hakemusjoukko!");
+                        }
+                        final List<ValintatietoValinnanvaiheDTO> valinnanvaiheet;
+                        try {
+                            valinnanvaiheet = hakukohdeResource.hakukohde(hakukohdeOid);
+                            dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        } catch (Exception e) {
+                            LOG.error("Valinnanvaiheiden haku virhe: {}\r\n{}", e.getMessage(), Arrays.toString(e.getStackTrace()));
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.VALINTALASKENTA, "Valintalaskennalta ei saatu valinnanvaiheita", ""));
+                            throw e;
+                        }
+                        InputStream xlsx;
+                        try {
+                            ValintatapajonoExcel valintatapajonoExcel = new ValintatapajonoExcel(hakuOid, hakukohdeOid, valintatapajonoOid,
+                                    hakuNimi, hakukohdeNimi, valinnanvaiheet, hakemukset);
+                            xlsx = valintatapajonoExcel.getExcel().vieXlsx();
+                            dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        } catch (Exception e) {
+                            LOG.error("Valintatapajono excelin luonti virhe: {}\r\n{}", e.getMessage(), Arrays.toString(e.getStackTrace()));
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.KOOSTEPALVELU, "Valintatapajono exceliä ei saatu luotua!", ""));
+                            throw e;
+                        }
+                        try {
+                            String id = generateId();
+                            Long expirationTime = defaultExpirationDate().getTime();
+                            List<String> tags = dokumenttiprosessi(exchange).getTags();
+                            dokumenttiResource.tallenna(id, "valintatapajono.xlsx", expirationTime, tags, "application/octet-stream", xlsx);
+                            dokumenttiprosessi(exchange).setDokumenttiId(id);
+                            dokumenttiprosessi(exchange).inkrementoiTehtyjaToita();
+                        } catch (Exception e) {
+                            LOG.error("Dokumenttipalveluun vienti virhe: {}\r\n{}", e.getMessage(), Arrays.toString(e.getStackTrace()));
+                            dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.DOKUMENTTIPALVELU, "Dokumenttipalveluun ei saatu vietyä taulukkolaskentatiedostoa!", ""));
+                            throw e;
+                        }
+                    }
+                })
+                .stop();
+        /**
+         * DEAD LETTER CHANNEL
+         */
+        from(luontiEpaonnistui)
+                .process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        String syy;
+                        if (exchange.getException() == null) {
+                            syy = "Valintatapajonon taulukkolaskentaan vienti epäonnistui. Ota yheys ylläpitoon.";
+                        } else {
+                            syy = exchange.getException().getMessage();
+                        }
+                        dokumenttiprosessi(exchange).getPoikkeukset().add(new Poikkeus(Poikkeus.KOOSTEPALVELU, "Valintatapajonon vienti", syy));
+                    }
+                })
+                .stop();
+    }
 }
