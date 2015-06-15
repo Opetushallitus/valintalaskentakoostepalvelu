b3594586b4f66f3920a9cc6e041438a7b9708a66
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 07:33:54 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/sijoitteluntulos/route/impl/SijoittelunTulosConfig.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoitteluntulos/route/impl/SijoittelunTulosConfig.java
@@ -12,31 +12,22 @@ import fi.vm.sade.valinta.kooste.sijoitteluntulos.route.SijoittelunTulosOsoiteta
 import fi.vm.sade.valinta.kooste.sijoitteluntulos.route.SijoittelunTulosTaulukkolaskentaRoute;
 import org.springframework.context.annotation.Profile;
 
-/**
- * 
- * @author Jussi Jartamo
- * 
- */
 @Profile("default")
 @Configuration
 public class SijoittelunTulosConfig {
-	@Bean
-	public SijoittelunTulosOsoitetarratRoute getSijoittelunTulosOsoitetarratRoute(
-			@Value(SijoittelunTulosOsoitetarratRoute.SEDA_SIJOITTELUNTULOS_OSOITETARRAT_HAULLE) String osoitetarrat,
-			@Qualifier("javaDslCamelContext") CamelContext context)
-			throws Exception {
-		return ProxyWithAnnotationHelper.createProxy(
-				context.getEndpoint(osoitetarrat),
-				SijoittelunTulosOsoitetarratRoute.class);
-	}
+    @Bean
+    public SijoittelunTulosOsoitetarratRoute getSijoittelunTulosOsoitetarratRoute(
+            @Value(SijoittelunTulosOsoitetarratRoute.SEDA_SIJOITTELUNTULOS_OSOITETARRAT_HAULLE) String osoitetarrat,
+            @Qualifier("javaDslCamelContext") CamelContext context)
+            throws Exception {
+        return ProxyWithAnnotationHelper.createProxy(context.getEndpoint(osoitetarrat), SijoittelunTulosOsoitetarratRoute.class);
+    }
 
-	@Bean
-	public SijoittelunTulosTaulukkolaskentaRoute getSijoittelunTulosTaulukkolaskentaRoute(
-			@Value(SijoittelunTulosTaulukkolaskentaRoute.SEDA_SIJOITTELUNTULOS_TAULUKKOLASKENTA_HAULLE) String taulukkolaskenta,
-			@Qualifier("javaDslCamelContext") CamelContext context)
-			throws Exception {
-		return ProxyWithAnnotationHelper.createProxy(
-				context.getEndpoint(taulukkolaskenta),
-				SijoittelunTulosTaulukkolaskentaRoute.class);
-	}
+    @Bean
+    public SijoittelunTulosTaulukkolaskentaRoute getSijoittelunTulosTaulukkolaskentaRoute(
+            @Value(SijoittelunTulosTaulukkolaskentaRoute.SEDA_SIJOITTELUNTULOS_TAULUKKOLASKENTA_HAULLE) String taulukkolaskenta,
+            @Qualifier("javaDslCamelContext") CamelContext context)
+            throws Exception {
+        return ProxyWithAnnotationHelper.createProxy(context.getEndpoint(taulukkolaskenta), SijoittelunTulosTaulukkolaskentaRoute.class);
+    }
 }
