b5ca004fab7cf91ec8e6d102e56e26cac1c601f4
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 08:07:26 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/util/HenkilotunnusTarkistusUtil.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/HenkilotunnusTarkistusUtil.java
@@ -5,39 +5,32 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author Jussi Jartamo
- */
 public class HenkilotunnusTarkistusUtil {
     private static final Logger LOG = LoggerFactory.getLogger(HenkilotunnusTarkistusUtil.class);
     private final static char[] TARKISTUSMERKKI =
-            {'0','1','2','3','4','5','6','7','8','9',
-                    'A','B','C','D','E','F','H','J','K','L',
-                    'M','N','P','R','S','T','U','V','W','X','Y'};
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L',
+                    'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
 
-    /**
-     * @param henkilotunnus
-     * @return true jos henkilötunnus on rakenteellisesti oikein ja tarkistusmerkki täsmää
-     */
     public static boolean tarkistaHenkilotunnus(String henkilotunnus) {
-        if(henkilotunnus == null || henkilotunnus.length() != 11) {
+        if (henkilotunnus == null || henkilotunnus.length() != 11) {
             return false; // ei vastaa henkilötunnusta sisällön koolta
         }
-        String syntymaaika = henkilotunnus.substring(0,6);
-        String numero = henkilotunnus.substring(7,10);
+        String syntymaaika = henkilotunnus.substring(0, 6);
+        String numero = henkilotunnus.substring(7, 10);
         int tarkistettavaNumero;
         try {
             tarkistettavaNumero = Integer.parseInt(syntymaaika + numero);
-        }catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             return false;
         }
-        return TARKISTUSMERKKI[(tarkistettavaNumero%31)]==henkilotunnus.charAt(10);
+        return TARKISTUSMERKKI[(tarkistettavaNumero % 31)] == henkilotunnus.charAt(10);
     }
 
     public static Sukupuoli palautaSukupuoli(String henkilotunnus) {
-        if(!tarkistaHenkilotunnus(henkilotunnus)) {
+        if (!tarkistaHenkilotunnus(henkilotunnus)) {
             return Sukupuoli.EI_SUKUPUOLTA;
-        } else if(Integer.parseInt(""+henkilotunnus.charAt(9)) % 2 == 0) {
+        } else if (Integer.parseInt("" + henkilotunnus.charAt(9)) % 2 == 0) {
             return Sukupuoli.NAINEN;
         } else {
             return Sukupuoli.MIES;
--- src/main/java/fi/vm/sade/valinta/kooste/util/Kieli.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/Kieli.java
@@ -6,29 +6,26 @@ import java.util.Set;
 import com.google.common.collect.Sets;
 
 /**
- * 
- * @author Jussi Jartamo
- * 
  *         Yhteinen utiliteetti tarjonnasta tulevan kielikoodien
  *         transformointiin ja preferointiin.
  */
 public class Kieli {
 
-	private final String kieli;
+    private final String kieli;
 
-	public Kieli(String kielikoodi) {
-		this.kieli = KieliUtil.normalisoiKielikoodi(kielikoodi);
-	}
+    public Kieli(String kielikoodi) {
+        this.kieli = KieliUtil.normalisoiKielikoodi(kielikoodi);
+    }
 
-	public Kieli(Collection<String> kielikoodit) {
-		Set<String> kielet = Sets.newHashSet();
-		for (String kielikoodi : kielikoodit) {
-			kielet.add(KieliUtil.normalisoiKielikoodi(kielikoodi));
-		}
-		this.kieli = KieliUtil.preferoi(kielet);
-	}
+    public Kieli(Collection<String> kielikoodit) {
+        Set<String> kielet = Sets.newHashSet();
+        for (String kielikoodi : kielikoodit) {
+            kielet.add(KieliUtil.normalisoiKielikoodi(kielikoodi));
+        }
+        this.kieli = KieliUtil.preferoi(kielet);
+    }
 
-	public String getKieli() {
-		return this.kieli;
-	}
+    public String getKieli() {
+        return this.kieli;
+    }
 }
