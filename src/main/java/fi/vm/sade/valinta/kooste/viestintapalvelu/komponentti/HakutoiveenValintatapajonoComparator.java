dd95c476abe7a54be1fda11c6752c2787a489cc5
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 11:01:24 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/komponentti/HakutoiveenValintatapajonoComparator.java
+++ src/main/java/fi/vm/sade/valinta/kooste/viestintapalvelu/komponentti/HakutoiveenValintatapajonoComparator.java
@@ -5,31 +5,20 @@ import java.util.Comparator;
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakutoiveenValintatapajonoDTO;
 
 /**
- * 
- * @author Jussi Jartamo
- * 
  *         Sort descending hakutoiveet by valintatapajono priority
  */
-public class HakutoiveenValintatapajonoComparator implements
-		Comparator<HakutoiveenValintatapajonoDTO> {
+public class HakutoiveenValintatapajonoComparator implements Comparator<HakutoiveenValintatapajonoDTO> {
+    public static final HakutoiveenValintatapajonoComparator DESCENDING = new HakutoiveenValintatapajonoComparator(-1);
+    public static final HakutoiveenValintatapajonoComparator ASCENDING = new HakutoiveenValintatapajonoComparator(1);
+    public static final HakutoiveenValintatapajonoComparator DEFAULT = ASCENDING;
 
-	public static final HakutoiveenValintatapajonoComparator DESCENDING = new HakutoiveenValintatapajonoComparator(
-			-1);
-	public static final HakutoiveenValintatapajonoComparator ASCENDING = new HakutoiveenValintatapajonoComparator(
-			1);
-	public static final HakutoiveenValintatapajonoComparator DEFAULT = ASCENDING;
+    private final int multiplier;
 
-	private final int multiplier;
+    private HakutoiveenValintatapajonoComparator(int multiplier) {
+        this.multiplier = multiplier;
+    }
 
-	private HakutoiveenValintatapajonoComparator(int multiplier) {
-		this.multiplier = multiplier;
-	}
-
-	public int compare(HakutoiveenValintatapajonoDTO o1,
-			HakutoiveenValintatapajonoDTO o2) {
-
-		return multiplier
-				* o1.getValintatapajonoPrioriteetti().compareTo(
-						o2.getValintatapajonoPrioriteetti());
-	}
+    public int compare(HakutoiveenValintatapajonoDTO o1, HakutoiveenValintatapajonoDTO o2) {
+        return multiplier * o1.getValintatapajonoPrioriteetti().compareTo(o2.getValintatapajonoPrioriteetti());
+    }
 }
