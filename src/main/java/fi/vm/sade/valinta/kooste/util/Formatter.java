d4addead606b804e9d5381927f24d4d421980d84
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 08:03:00 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/util/Formatter.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/Formatter.java
@@ -9,64 +9,61 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.FastDateFormat;
 
 public class Formatter {
-	public static final FastDateFormat PVMFORMATTER = FastDateFormat
-			.getInstance("dd.MM.yyyy HH:mm");
-	public static final NumberFormat NUMERO_FORMAATTI = NumberFormat
-			.getInstance(new Locale("FI"));
-	public static final String ARVO_EROTIN = " / ";
-	public static final String ARVO_VALI = " ";
-	public static final String ARVO_VAKIO = "-";
+    public static final FastDateFormat PVMFORMATTER = FastDateFormat.getInstance("dd.MM.yyyy HH:mm");
+    public static final NumberFormat NUMERO_FORMAATTI = NumberFormat.getInstance(new Locale("FI"));
+    public static final String ARVO_EROTIN = " / ";
+    public static final String ARVO_VALI = " ";
+    public static final String ARVO_VAKIO = "-";
 
-	public static String suomennaNumero(BigDecimal arvo) {
-		return suomennaNumero(arvo, StringUtils.EMPTY);
-	}
+    public static String suomennaNumero(BigDecimal arvo) {
+        return suomennaNumero(arvo, StringUtils.EMPTY);
+    }
 
-	public static String suomennaNumero(Number arvo) {
-		if (arvo == null) {
-			return StringUtils.EMPTY;
-		} else {
-			return NUMERO_FORMAATTI.format(arvo);
-		}
+    public static String suomennaNumero(Number arvo) {
+        if (arvo == null) {
+            return StringUtils.EMPTY;
+        } else {
+            return NUMERO_FORMAATTI.format(arvo);
+        }
 
-	}
+    }
 
-	public static String paivamaara(Date paivamaara) {
-		if (paivamaara == null) {
-			return StringUtils.EMPTY;
-		} else {
-			return PVMFORMATTER.format(paivamaara);
-		}
-	}
+    public static String paivamaara(Date paivamaara) {
+        if (paivamaara == null) {
+            return StringUtils.EMPTY;
+        } else {
+            return PVMFORMATTER.format(paivamaara);
+        }
+    }
 
-	public static String suomennaNumero(BigDecimal arvo, String vakioArvo) {
-		if (arvo == null) {
-			return vakioArvo;
-		}
-		return NUMERO_FORMAATTI.format(arvo);
-	}
+    public static String suomennaNumero(BigDecimal arvo, String vakioArvo) {
+        if (arvo == null) {
+            return vakioArvo;
+        }
+        return NUMERO_FORMAATTI.format(arvo);
+    }
 
-	public static String suomennaNumero(Integer arvo, String vakioArvo) {
-		if (arvo == null) {
-			return vakioArvo;
-		}
-		return NUMERO_FORMAATTI.format(arvo);
-	}
+    public static String suomennaNumero(Integer arvo, String vakioArvo) {
+        if (arvo == null) {
+            return vakioArvo;
+        }
+        return NUMERO_FORMAATTI.format(arvo);
+    }
 
-	public static String suomennaNumero(Integer arvo) {
-		return suomennaNumero(arvo, StringUtils.EMPTY);
-	}
+    public static String suomennaNumero(Integer arvo) {
+        return suomennaNumero(arvo, StringUtils.EMPTY);
+    }
 
-	public static String humanReadableByteCount(long bytes) {
-		return humanReadableByteCount(bytes, true);
-	}
+    public static String humanReadableByteCount(long bytes) {
+        return humanReadableByteCount(bytes, true);
+    }
 
-	public static String humanReadableByteCount(long bytes, boolean si) {
-		int unit = si ? 1000 : 1024;
-		if (bytes < unit)
-			return bytes + " B";
-		int exp = (int) (Math.log(bytes) / Math.log(unit));
-		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1)
-				+ (si ? "" : "i");
-		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
-	}
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit)
+            return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
 }
--- src/main/java/fi/vm/sade/valinta/kooste/util/HakemusOsallistuminenComparator.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/HakemusOsallistuminenComparator.java
@@ -4,34 +4,31 @@ import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.HakemusOsallistuminenD
 
 import java.util.Comparator;
 
-public class HakemusOsallistuminenComparator implements
-		Comparator<HakemusOsallistuminenDTO> {
+public class HakemusOsallistuminenComparator implements Comparator<HakemusOsallistuminenDTO> {
 
-	@Override
-	public int compare(HakemusOsallistuminenDTO o1,
-			HakemusOsallistuminenDTO o2) {
-		String o1sukunimi = o1.getSukunimi().toUpperCase();
-		if (o1 == null || o2 == null || o1sukunimi == null
-				|| o2.getSukunimi() == null) {
-			return 0;
-		} else {
-			int c = o1sukunimi.compareTo(o2.getSukunimi().toUpperCase());
-			if (c == 0) {
-				String o1etunimi = o1.getEtunimi().toUpperCase();
-				if (o1etunimi == null || o2.getEtunimi() == null) {
-					return 0;
-				} else {
-					return o1etunimi.compareTo(o2.getEtunimi().toUpperCase());
-				}
-			} else {
-				return c;
-			}
-		}
-	}
+    @Override
+    public int compare(HakemusOsallistuminenDTO o1, HakemusOsallistuminenDTO o2) {
+        String o1sukunimi = o1.getSukunimi().toUpperCase();
+        if (o1 == null || o2 == null || o1sukunimi == null
+                || o2.getSukunimi() == null) {
+            return 0;
+        } else {
+            int c = o1sukunimi.compareTo(o2.getSukunimi().toUpperCase());
+            if (c == 0) {
+                String o1etunimi = o1.getEtunimi().toUpperCase();
+                if (o1etunimi == null || o2.getEtunimi() == null) {
+                    return 0;
+                } else {
+                    return o1etunimi.compareTo(o2.getEtunimi().toUpperCase());
+                }
+            } else {
+                return c;
+            }
+        }
+    }
 
-	private HakemusOsallistuminenComparator() {
+    private HakemusOsallistuminenComparator() {
+    }
 
-	}
-
-	public static HakemusOsallistuminenComparator DEFAULT = new HakemusOsallistuminenComparator();
+    public static HakemusOsallistuminenComparator DEFAULT = new HakemusOsallistuminenComparator();
 }
