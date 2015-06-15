729c291db6fff9f16026e12e5c02f6dcc4d4d7c7
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 07:47:19 +0300

    remove javadoc and refactor

--- src/main/java/fi/vm/sade/valinta/kooste/tarjonta/api/OrganisaatioResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/tarjonta/api/OrganisaatioResource.java
@@ -13,34 +13,18 @@ import javax.ws.rs.core.MediaType;
 import fi.vm.sade.organisaatio.resource.dto.OrganisaatioRDTO;
 
 /**
- * 
- * @author Jussi Jartamo
- * 
- *         Kopio rajapinnasta
- *         fi.vm.sade.organisaatio.resource.OrganisaatioResource ilman metodia
+ *         Kopio rajapinnasta fi.vm.sade.organisaatio.resource.OrganisaatioResource ilman metodia
  *         searchBasic koska @QueryParam(empty) (eli @QueryParam("")) toimii
  *         vaan yhteen suuntaan.
  */
 @Path("/organisaatio")
 public interface OrganisaatioResource {
 
-    public String OID_SEPARATOR = "/";
-
-    // @GET
-    // @Path("/hae")
-    // @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
-    // public OrganisaatioHakutulos
-    // searchBasic(@QueryParam("")OrganisaatioSearchCriteria q);
-
     /**
      * NOTE: USED BY SECURITY FRAMEWORK - DON'T CHANGE
-     * 
      * Find oids of organisaatio's parents, result oids start from root, ends to
      * given oid itself, and are separated by '/'.
-     * 
-     * @param oid
-     * @return oid/path/form/root
-     * @throws Exception
+     *
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -54,33 +38,23 @@ public interface OrganisaatioResource {
 
     /**
      * Get list of Organisaatio oids mathching the query.
-     * 
+     * <p/>
      * Search terms:
      * <ul>
-     * <li>searchTerms=type=KOULUTUSTOIMIJA / OPPILAITOS / TOIMIPISTE ==
-     * OrganisaatioTyyppi.name()</li>
+     * <li>searchTerms=type=KOULUTUSTOIMIJA / OPPILAITOS / TOIMIPISTE == OrganisaatioTyyppi.name()</li>
      * </ul>
-     * 
-     * @param searchTerms
-     * @param count
-     * @param startIndex
-     * @param lastModifiedBefore
-     * @param lastModifiedSince
-     * @return
+     *
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public List<String> search(@QueryParam("searchTerms") String searchTerms, @QueryParam("count") int count,
-            @QueryParam("startIndex") int startIndex, @QueryParam("lastModifiedBefore") Date lastModifiedBefore,
-            @QueryParam("lastModifiedSince") Date lastModifiedSince);
+                               @QueryParam("startIndex") int startIndex, @QueryParam("lastModifiedBefore") Date lastModifiedBefore,
+                               @QueryParam("lastModifiedSince") Date lastModifiedSince);
 
     /**
      * Organisaatio DTO as JSON.
-     * 
-     * @param oid
-     *            OID or Y-TUNNUS or VIRASTOTUNNUS or OPETUSPISTEKOODI or
-     *            TOIMIPISTEKOODI
-     * @return
+     *
+     * @param oid OID or Y-TUNNUS or VIRASTOTUNNUS or OPETUSPISTEKOODI or TOIMIPISTEKOODI
      */
     @GET
     @Path("{oid}")
