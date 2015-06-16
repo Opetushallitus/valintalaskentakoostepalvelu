dce0cb40dec96eb460a004779b852e27a700018c
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-16 12:48:45 +0300

    stylize

--- src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/route/SijoittelunValvonta.java
+++ src/main/java/fi/vm/sade/valinta/kooste/sijoittelu/route/SijoittelunValvonta.java
@@ -3,7 +3,5 @@ package fi.vm.sade.valinta.kooste.sijoittelu.route;
 import fi.vm.sade.valinta.kooste.sijoittelu.dto.Sijoittelu;
 
 public interface SijoittelunValvonta {
-
     Sijoittelu haeAktiivinenSijoitteluHaulle(String hakuOid);
-
 }
