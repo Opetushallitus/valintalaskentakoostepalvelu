998f4d3627228f8aa8c5e8eda742ba2cbc8d0592
Tiina Kiuru <tiina.kiuru@reaktor.fi>
2015-07-01 16:39:27 +0300

    Test for haku with several valintajono with different applications

--- src/test/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/SijoittelunTulosExcelKomponenttiTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/valintalaskentatulos/SijoittelunTulosExcelKomponenttiTest.java
@@ -1,5 +1,6 @@
 package fi.vm.sade.valinta.kooste.valintalaskentatulos;
 
+import fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Answers;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
 import fi.vm.sade.sijoittelu.tulos.dto.HakemusDTO;
@@ -29,6 +30,10 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SijoittelunTulosExcelKomponenttiTest {
+    private final String HAKEMUS1 = "hakemus1";
+    private final String HAKEMUS2 = "hakemus2";
+    private final String HAKEMUS3 = "hakemus3";
+
     @Mock
     private KoodistoCachedAsyncResource koodistoResource;
 
@@ -47,30 +52,63 @@ public class SijoittelunTulosExcelKomponenttiTest {
         HakukohdeDTO hakukohde = new HakukohdeDTO();
         ValintatapajonoDTO jono = new ValintatapajonoDTO();
         HakemusDTO hakemusDTO = new HakemusDTO();
-        hakemusDTO.setHakemusOid("hakemus979");
+        hakemusDTO.setHakemusOid(HAKEMUS1);
         jono.setHakemukset(Arrays.asList(hakemusDTO));
-        hakukohde.setValintatapajonot(Arrays.asList(jono));
+        hakukohde.setValintatapajonot(Arrays.asList(createValintatapajonot("jono1", Arrays.asList(HAKEMUS1))));
         Answers answers = new Answers();
         HashMap<String, String> henkilotiedot = new HashMap<>();
         henkilotiedot.put("Kutsumanimi", "Pekka");
         henkilotiedot.put("Etunimet", "Pekka Johannes");
         henkilotiedot.put("Sukunimi", "Alamäki");
         answers.setHenkilotiedot(henkilotiedot);
-        Hakemus hakemus = new Hakemus("", "", answers, new HashMap<>(), new ArrayList<>(), "hakemus979", "", "");
+        Hakemus hakemus = new Hakemus("", "", answers, new HashMap<>(), new ArrayList<>(), HAKEMUS1, "", "");
         InputStream inputStream = excelKomponentti.luoXls(new ArrayList<>(), "FI", "Konetekniikka", "Aalto yliopisto", "hakukohde1", Arrays.asList(hakemus), hakukohde);
         Collection<Rivi> rivit = ExcelImportUtil.importHSSFExcel(inputStream);
-        assertTrue(cellTexts(rivit, "hakemus979").contains("Alamäki, Pekka"));
+        assertTrue(cellTexts(rivit, HAKEMUS1).contains("Alamäki, Pekka"));
+    }
+
+    @Test
+    public void addsEachHakemusOnlyOnce() throws Throwable {
+        HakukohdeDTO hakukohde = new HakukohdeDTO();
+        ValintatapajonoDTO jono1 = createValintatapajonot("jono1", Arrays.asList(HAKEMUS1, HAKEMUS2));
+        ValintatapajonoDTO jono2 = createValintatapajonot("jono2", Arrays.asList(HAKEMUS1, HAKEMUS2, HAKEMUS3));
+        Hakemus hakemus1 = new Hakemus("", "", new Answers(), new HashMap<>(), new ArrayList<>(), HAKEMUS1, "", "");
+        Hakemus hakemus2 = new Hakemus("", "", new Answers(), new HashMap<>(), new ArrayList<>(), HAKEMUS2, "", "");
+        Hakemus hakemus3 = new Hakemus("", "", new Answers(), new HashMap<>(), new ArrayList<>(), HAKEMUS3, "", "");
+        hakukohde.setValintatapajonot(Arrays.asList(jono1, jono2));
+        InputStream inputStream = excelKomponentti.luoXls(new ArrayList<>(), "FI", "Konetekniikka", "Aalto yliopisto", "hakukohde1", Arrays.asList(hakemus1, hakemus2, hakemus3), hakukohde);
+        Collection<Rivi> rivit = ExcelImportUtil.importHSSFExcel(inputStream);
+        assertEquals(1, rowsContaining(HAKEMUS1, rivit).size());
+        assertEquals(1, rowsContaining(HAKEMUS2, rivit).size());
+        assertEquals(1, rowsContaining(HAKEMUS3, rivit).size());
     }
 
     private List<String> cellTexts(Collection<Rivi> rivit, String oid) {
         return rivit.stream()
-                .filter(containsHakemus(oid))
+                .filter(containsText(oid))
                 .findFirst().get().getSolut().stream()
                 .map(cell -> cell.toTeksti().getTeksti())
                 .collect(Collectors.toList());
     }
 
-    private Predicate<Rivi> containsHakemus(String oid) {
+    private Collection<Rivi> rowsContaining(String text, Collection<Rivi> rivit) {
+        return rivit.stream().filter(containsText(text)).collect(Collectors.toList());
+    }
+
+    private Predicate<Rivi> containsText(String oid) {
         return rivi -> rivi.getSolut().stream().anyMatch(solu -> solu.toTeksti().getTeksti().equals(oid));
+    }
+
+    private ValintatapajonoDTO createValintatapajonot(String jonoOid, List<String> hakemusOids) {
+        ValintatapajonoDTO jono = new ValintatapajonoDTO();
+        jono.setOid(jonoOid);
+        List<HakemusDTO> hakemusDTOs = hakemusOids.stream().map(oid -> {
+            HakemusDTO hakemusDTO = new HakemusDTO();
+            hakemusDTO.setHakemusOid(oid);
+            hakemusDTO.setTila(HakemuksenTila.HYVAKSYTTY);
+            return hakemusDTO;
+        }).collect(Collectors.toList());
+        jono.setHakemukset(hakemusDTOs);
+        return jono;
     }
 }
