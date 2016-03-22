38e7919270723e2fef92e9a1c91f06f7e0a6b813
Harri Hämäläinen <hhamalai@iki.fi>
2016-03-22 12:13:24 +0200

    Fetch postitoimipaikka from koodisto to HakemusDTO

--- src/main/java/fi/vm/sade/valinta/kooste/hakemukset/resource/HakemuksetResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/hakemukset/resource/HakemuksetResource.java
@@ -6,10 +6,13 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.Hakemus;
 import fi.vm.sade.valinta.kooste.external.resource.hakuapp.ApplicationAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.koodisto.KoodistoCachedAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.koodisto.dto.Koodi;
 import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
 import fi.vm.sade.valinta.kooste.hakemukset.dto.HakemusDTO;
 import fi.vm.sade.valinta.kooste.hakemukset.dto.HakukohdeDTO;
 import fi.vm.sade.valinta.kooste.hakemukset.dto.ValintakoeDTO;
+import fi.vm.sade.valinta.kooste.util.KieliUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,15 +21,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import rx.Observable;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -44,6 +46,10 @@ public class HakemuksetResource {
 
     @Autowired
     private ValintaperusteetAsyncResource valintaperusteetAsyncResource;
+
+    @Autowired
+    private KoodistoCachedAsyncResource koodistoCachedAsyncResource;
+    Map<String, Koodi> postCodes = koodistoCachedAsyncResource.haeKoodisto(KoodistoCachedAsyncResource.POSTI);
 
     @PreAuthorize("hasAnyRole('ROLE_APP_HAKEMUS_READ_UPDATE', 'ROLE_APP_HAKEMUS_READ', 'ROLE_APP_HAKEMUS_CRUD', 'ROLE_APP_HAKEMUS_LISATIETORU', 'ROLE_APP_HAKEMUS_LISATIETOCRUD')")
     @GET
@@ -90,9 +96,13 @@ public class HakemuksetResource {
             hakemusDTO.setKutsumanimi(hakemus.getAnswers().getHenkilotiedot().get("Kutsumanimi"));
             hakemusDTO.setSahkoposti(hakemus.getAnswers().getHenkilotiedot().get("Sähköposti"));
             hakemusDTO.setKatuosoite(hakemus.getAnswers().getHenkilotiedot().get("lahiosoite"));
-            hakemusDTO.setPostinumero(hakemus.getAnswers().getHenkilotiedot().get("Postinumero"));
-            //TODO: Hae postitoimipaikka koodistosta
-            hakemusDTO.setPostitoimipaikka("PUUTTUU");
+
+            String postinumero = hakemus.getAnswers().getHenkilotiedot().get("Postinumero");
+            hakemusDTO.setPostinumero(postinumero);
+            hakemusDTO.setPostitoimipaikka(KoodistoCachedAsyncResource.haeKoodistaArvo(
+                    postCodes.get(postinumero),
+                    KieliUtil.SUOMI,
+                    postinumero));
 
             List<HakukohdeDTO> hakukohdeDTOs = hakemus.getAnswers().getHakutoiveet().entrySet().stream()
                     .filter(entry -> entry.getKey().startsWith("preference") && entry.getKey().endsWith("-Koulutus-id"))
