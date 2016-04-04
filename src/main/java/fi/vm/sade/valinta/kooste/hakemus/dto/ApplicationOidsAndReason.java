425d1fbd95fade773e6f5dd5869409b0bf38d779
Jani Rahkola <jani.rahkola@reaktor.com>
2016-04-04 13:05:08 +0300

    Exclude conflicting jackson versions

--- src/main/java/fi/vm/sade/valinta/kooste/hakemus/dto/ApplicationOidsAndReason.java
+++ src/main/java/fi/vm/sade/valinta/kooste/hakemus/dto/ApplicationOidsAndReason.java
@@ -1,6 +1,6 @@
 package fi.vm.sade.valinta.kooste.hakemus.dto;
 
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
