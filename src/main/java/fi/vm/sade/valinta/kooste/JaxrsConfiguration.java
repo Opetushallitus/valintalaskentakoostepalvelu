c901af04ddc84a29c499f827c99ca0515c5462ee
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-05-19 12:10:43 +0300

    Fix customer serializer location

--- src/main/java/fi/vm/sade/valinta/kooste/JaxrsConfiguration.java
+++ src/main/java/fi/vm/sade/valinta/kooste/JaxrsConfiguration.java
@@ -3,6 +3,8 @@ package fi.vm.sade.valinta.kooste;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import fi.vm.sade.valinta.kooste.proxy.resource.valintatulosservice.ValintaTulosServiceProxyResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,7 @@ public class JaxrsConfiguration {
     public JacksonJsonProvider getJacksonJsonProvider() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new ValintaTulosServiceProxyResource.ValintaTulosServiceSerializersModule());
         return new JacksonJsonProvider(objectMapper);
     }
 }
--- src/main/java/fi/vm/sade/valinta/kooste/ObjectMapperProvider.java
+++ src/main/java/fi/vm/sade/valinta/kooste/ObjectMapperProvider.java
@@ -3,8 +3,6 @@ package fi.vm.sade.valinta.kooste;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import fi.vm.sade.valinta.kooste.proxy.resource.valintatulosservice.ValintaTulosServiceProxyResource;
-
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
@@ -16,7 +14,6 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
     public ObjectMapperProvider() {
         objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.registerModule(new ValintaTulosServiceProxyResource.ValintaTulosServiceSerializersModule());
     }
 
     @Override
