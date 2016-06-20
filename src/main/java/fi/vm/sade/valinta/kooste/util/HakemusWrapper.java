79b56cb23f1748645ee471b13513732d3e9381ff
Tommi Helander <tommi.helander@gofore.com>
2016-06-20 16:46:17 +0300

    Correct field 'lupatiedot-sahkoinen-viestinta'

--- src/main/java/fi/vm/sade/valinta/kooste/util/HakemusWrapper.java
+++ src/main/java/fi/vm/sade/valinta/kooste/util/HakemusWrapper.java
@@ -43,7 +43,7 @@ public class HakemusWrapper {
     private final static String NAINEN                          = "2";
     private final static String MIES                            = "1";
     private final static String POHJAKOULUTUSMAA_TOINEN_ASTE    = "pohjakoulutusmaa_toinen_aste";
-    private final static String LUPA_SAHKOISESTI                = "lupaSahkoisesti";
+    private final static String LUPA_SAHKOISEEN_VIESTINTAAN     = "lupatiedot-sahkoinen-viestinta";
 
     private Yhteystiedot yhteystiedot = null;
 
@@ -253,8 +253,8 @@ public class HakemusWrapper {
 
     public boolean getLupaSahkoiseenAsiointiin() {
         getLisatiedot(); // lazy load lisatiedot
-        if (lisatiedot.containsKey(LUPA_SAHKOISESTI)) {
-            String lupa = lisatiedot.get(LUPA_SAHKOISESTI);
+        if (lisatiedot.containsKey(LUPA_SAHKOISEEN_VIESTINTAAN)) {
+            String lupa = lisatiedot.get(LUPA_SAHKOISEEN_VIESTINTAAN);
             return Boolean.TRUE.equals(Boolean.valueOf(lupa));
         }
         return false;
