ec115a80d93036083fd353e1b59dab40d69212fe
Jessika Penttinen <jessika.penttinen@siili.com>
2016-06-21 09:49:07 +0300

    Lisätty julkaise/eposti-painikkeiden disablointi

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/dto/LetterBatchCountDto.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/dto/LetterBatchCountDto.java
@@ -5,11 +5,15 @@ public class LetterBatchCountDto {
     public final long letterTotalCount;
     public final long letterReadyCount;
     public final long letterErrorCount;
+    public final boolean readyForPublish;
+    public final boolean readyForEPosti;
 
-    public LetterBatchCountDto(long letterTotalCount, long letterReadyCount, long letterErrorCount) {
+    public LetterBatchCountDto(long letterTotalCount, long letterReadyCount, long letterErrorCount, boolean readyForPublish, boolean readyForEPosti) {
         this.letterTotalCount = letterTotalCount;
         this.letterReadyCount = letterReadyCount;
         this.letterErrorCount = letterErrorCount;
+        this.readyForPublish = readyForPublish;
+        this.readyForEPosti = readyForEPosti;
     }
 
 }
