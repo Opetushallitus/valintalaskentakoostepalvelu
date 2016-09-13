5830783d70100c6db3af5dfee23953ac576f75cc
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-09-13 13:28:18 +0300

    Tabs to spaces

--- src/test/java/fi/vm/sade/valinta/kooste/erillishaku/excel/ErillishakuExcelTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/erillishaku/excel/ErillishakuExcelTest.java
@@ -16,81 +16,81 @@ import static org.junit.Assert.assertTrue;
 
 public class ErillishakuExcelTest {
 
-	private static List<ErillishakuRivi> createErillishakuRivisWithTwoValidRows() {
-		List<ErillishakuRivi> rivit = Lists.newArrayList();
-		String syntymaAika = "11.11.2011";
-		ErillishakuRivi rivi = new ErillishakuRivi(
-				null, "sukunimi","etunimi1","hetu","test.email@example.com", syntymaAika, Sukupuoli.MIES.name(), "", "FI", "HYLATTY", false, null, "", "", false, false, "FI",
-				"040123456789", "Esimerkkitie 2", "00100", "HELSINKI", "FIN", "FIN", "HELSINKI", "SWE");
-		rivit.add(rivi);
-		ErillishakuRivi rivi2= new ErillishakuRivi(null, "sukunimi","etunimi2","hetu","test.email@example.com", syntymaAika, Sukupuoli.NAINEN.name(), "", "FI", "HYLATTY", false, null, "", "", true, false, "FI",
-				"040123456789", "Esimerkkitie 2", "00100", "HELSINKI", "FIN", "FIN", "HELSINKI", "FIN");
-		rivit.add(rivi2);
-		return rivit;
-	}
+    private static List<ErillishakuRivi> createErillishakuRivisWithTwoValidRows() {
+        List<ErillishakuRivi> rivit = Lists.newArrayList();
+        String syntymaAika = "11.11.2011";
+        ErillishakuRivi rivi = new ErillishakuRivi(
+                null, "sukunimi","etunimi1","hetu","test.email@example.com", syntymaAika, Sukupuoli.MIES.name(), "", "FI", "HYLATTY", false, null, "", "", false, false, "FI",
+                "040123456789", "Esimerkkitie 2", "00100", "HELSINKI", "FIN", "FIN", "HELSINKI", "SWE");
+        rivit.add(rivi);
+        ErillishakuRivi rivi2= new ErillishakuRivi(null, "sukunimi","etunimi2","hetu","test.email@example.com", syntymaAika, Sukupuoli.NAINEN.name(), "", "FI", "HYLATTY", false, null, "", "", true, false, "FI",
+                "040123456789", "Esimerkkitie 2", "00100", "HELSINKI", "FIN", "FIN", "HELSINKI", "FIN");
+        rivit.add(rivi2);
+        return rivit;
+    }
 
-	@Test
-	public void testaaTuontiKustomistaTiedostosta() throws Exception {
-		final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
-		ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", Collections.emptyList(), rivi -> tarkistusTapahtui.incrementAndGet());
-		eExcel.getExcel().tuoXlsx(new ClassPathResource("kustom_erillishaku.xlsx").getInputStream());
-		assertEquals(1, tarkistusTapahtui.get());
-	}
+    @Test
+    public void testaaTuontiKustomistaTiedostosta() throws Exception {
+        final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
+        ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", Collections.emptyList(), rivi -> tarkistusTapahtui.incrementAndGet());
+        eExcel.getExcel().tuoXlsx(new ClassPathResource("kustom_erillishaku.xlsx").getInputStream());
+        assertEquals(1, tarkistusTapahtui.get());
+    }
 
-	@Test
-	public void testaaTuontiKustomistaTiedostostaOtsikoilla() throws Exception {
-		final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
-		ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi",Collections.emptyList(), rivi -> tarkistusTapahtui.incrementAndGet());
-		eExcel.getExcel().tuoXlsx(new ClassPathResource("kustom_erillishaku_otsikoilla.xlsx").getInputStream());
-		assertEquals(1, tarkistusTapahtui.get());
-	}
+    @Test
+    public void testaaTuontiKustomistaTiedostostaOtsikoilla() throws Exception {
+        final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
+        ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi",Collections.emptyList(), rivi -> tarkistusTapahtui.incrementAndGet());
+        eExcel.getExcel().tuoXlsx(new ClassPathResource("kustom_erillishaku_otsikoilla.xlsx").getInputStream());
+        assertEquals(1, tarkistusTapahtui.get());
+    }
     
-	@Test
-	public void testaaVienti() throws Exception {
-		List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
-		ErillishakuRivi rivi3 = new ErillishakuRivi();
-		rivit.add(rivi3);
-		final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
-		ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", rivit, rv -> tarkistusTapahtui.incrementAndGet());
-		Excel excel = eExcel.getExcel();
-		excel.tuoXlsx(excel.vieXlsx());
+    @Test
+    public void testaaVienti() throws Exception {
+        List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
+        ErillishakuRivi rivi3 = new ErillishakuRivi();
+        rivit.add(rivi3);
+        final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
+        ErillishakuExcel eExcel = new ErillishakuExcel(null, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", rivit, rv -> tarkistusTapahtui.incrementAndGet());
+        Excel excel = eExcel.getExcel();
+        excel.tuoXlsx(excel.vieXlsx());
 
-		assertEquals(
-				2 // tavalliset rivit
-				+1 // tyhjärivi julkaisulupasyötteellä
-				, tarkistusTapahtui.get());
-		// Tulosta tiedostoksi testausta varten
-		// IOUtils.copy(excel.vieXlsx(), new FileOutputStream("erillishaku.xlsx"));
-	}
+        assertEquals(
+                2 // tavalliset rivit
+                +1 // tyhjärivi julkaisulupasyötteellä
+                , tarkistusTapahtui.get());
+        // Tulosta tiedostoksi testausta varten
+        // IOUtils.copy(excel.vieXlsx(), new FileOutputStream("erillishaku.xlsx"));
+    }
 
-	@Test
-	public void testaaVientiKKWithValidRows() throws Exception {
-		List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
-		final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
-		ErillishakuExcel eExcel = new ErillishakuExcel(Hakutyyppi.KORKEAKOULU, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", rivit, rv -> tarkistusTapahtui.incrementAndGet());
-		Excel excel = eExcel.getExcel();
-		excel.tuoXlsx(excel.vieXlsx());
+    @Test
+    public void testaaVientiKKWithValidRows() throws Exception {
+        List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
+        final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
+        ErillishakuExcel eExcel = new ErillishakuExcel(Hakutyyppi.KORKEAKOULU, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", rivit, rv -> tarkistusTapahtui.incrementAndGet());
+        Excel excel = eExcel.getExcel();
+        excel.tuoXlsx(excel.vieXlsx());
 
-		assertEquals(2, tarkistusTapahtui.get());
+        assertEquals(2, tarkistusTapahtui.get());
 
-		// Tulosta tiedostoksi testausta varten
-		// IOUtils.copy(excel.vieXlsx(), new FileOutputStream("erillishaku.xlsx"));
-	}
+        // Tulosta tiedostoksi testausta varten
+        // IOUtils.copy(excel.vieXlsx(), new FileOutputStream("erillishaku.xlsx"));
+    }
 
-	@Test
-	public void testaaVientiKKWithInvalidRow() throws Exception {
-		List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
+    @Test
+    public void testaaVientiKKWithInvalidRow() throws Exception {
+        List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
         ErillishakuRivi rivi3 = new ErillishakuRivi(null, "X-sukunimi", null, null, null, null, Sukupuoli.EI_SUKUPUOLTA, null, null, null, false,
-					null, null, null, false, false, null, null, null, null, null, null, null, null, null);
+                    null, null, null, false, false, null, null, null, null, null, null, null, null, null);
         rivit.add(rivi3);
         final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
         ErillishakuExcel eExcel = new ErillishakuExcel(Hakutyyppi.KORKEAKOULU, "Haun nimi", "Hakukohteen nimi", "Tarjoajan nimi", rivit, rv -> tarkistusTapahtui.incrementAndGet());
         Excel excel = eExcel.getExcel();
-		try {
-			excel.tuoXlsx(excel.vieXlsx());
-		} catch (ExcelValidointiPoikkeus e) {
-			assertTrue(e.getMessage().contains("Pakollinen arvo äidinkieli puuttuu riviltä"));
-		}
+        try {
+            excel.tuoXlsx(excel.vieXlsx());
+        } catch (ExcelValidointiPoikkeus e) {
+            assertTrue(e.getMessage().contains("Pakollinen arvo äidinkieli puuttuu riviltä"));
+        }
 
         assertEquals(
                 2 // tavalliset rivit
@@ -100,9 +100,9 @@ public class ErillishakuExcelTest {
         // IOUtils.copy(excel.vieXlsx(), new FileOutputStream("erillishaku.xlsx"));
     }
 
-	@Test
-	public void testaaVientiToinenAste() throws Exception {
-		List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
+    @Test
+    public void testaaVientiToinenAste() throws Exception {
+        List<ErillishakuRivi> rivit = createErillishakuRivisWithTwoValidRows();
         ErillishakuRivi rivi3 = new ErillishakuRivi();
         rivit.add(rivi3);
         final AtomicInteger tarkistusTapahtui = new AtomicInteger(0);
