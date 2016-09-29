7a31e8325b6e8a1f04629d3e2f7568090b02613d
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-09-29 16:50:29 +0300

    Remove jersey dependency totally
    Use CXF swagger

--- src/main/java/fi/vm/sade/valinta/kooste/KoosteTestProfileConfiguration.java
+++ src/main/java/fi/vm/sade/valinta/kooste/KoosteTestProfileConfiguration.java
@@ -64,7 +64,7 @@ public class KoosteTestProfileConfiguration {
         p0.setProperty("cas.service.organisaatio-service", "");
         p0.setProperty("cas.service.valintalaskenta-service", "");
         p0.setProperty("cas.service.dokumenttipalvelu", "");
-        p0.setProperty("valintalaskentakoostepalvelu.swagger.basepath", "");
+        p0.setProperty("valintalaskentakoostepalvelu.swagger.basepath", "/valintalaskentakoostepalvelu/resources");
         p0.setProperty("host.scheme", "http");
         p0.setProperty("host.virkailija", proxyServer);
         p0.setProperty("cas.service.valintalaskentakoostepalvelu", "");
