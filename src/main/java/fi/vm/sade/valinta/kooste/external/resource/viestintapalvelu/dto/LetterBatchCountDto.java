918f4101ea9593558b7f0e432f0dda0e8feaada6
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-11-02 17:02:33 +0200

    Show link to letter batch

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/dto/LetterBatchCountDto.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/dto/LetterBatchCountDto.java
@@ -2,13 +2,15 @@ package fi.vm.sade.valinta.kooste.external.resource.viestintapalvelu.dto;
 
 // {"letterTotalCount":3,"letterReadyCount":3,"letterErrorCount":0}
 public class LetterBatchCountDto {
+    public final Long letterBatchId;
     public final long letterTotalCount;
     public final long letterReadyCount;
     public final long letterErrorCount;
     public final boolean readyForPublish;
     public final boolean readyForEPosti;
 
-    public LetterBatchCountDto(long letterTotalCount, long letterReadyCount, long letterErrorCount, boolean readyForPublish, boolean readyForEPosti) {
+    public LetterBatchCountDto(Long letterBatchId, long letterTotalCount, long letterReadyCount, long letterErrorCount, boolean readyForPublish, boolean readyForEPosti) {
+        this.letterBatchId = letterBatchId;
         this.letterTotalCount = letterTotalCount;
         this.letterReadyCount = letterReadyCount;
         this.letterErrorCount = letterErrorCount;
