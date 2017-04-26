18629bf5513b8a45ff95597d87e870a5e7ff4feb
teo.mertanen <teo.mertanen@gofore.com>
2017-04-26 14:30:36 +0300

    Revert "use oppija objects passed by sure for resolving suoritukset"
    This reverts commit 3dceef9e6379da265802595e642b2ef833f09c69.

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/hakuapp/dto/HakemusHakija.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/hakuapp/dto/HakemusHakija.java
@@ -1,24 +1,23 @@
 package fi.vm.sade.valinta.kooste.external.resource.hakuapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.dto.Oppija;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HakemusHakija {
 
     private Hakemus hakemus;
-    private Oppija oppija;
+    private String opiskelijaOid;
 
-    public Oppija getOppija() {
-        return oppija;
+    public void setOpiskelijaOid(String opiskelijaOid) {
+        this.opiskelijaOid = opiskelijaOid;
     }
 
     public void setHakemus(Hakemus hakemus) {
         this.hakemus = hakemus;
     }
 
-    public void setOppija(Oppija oppija) {
-        this.oppija = oppija;
+    public String getOpiskelijaOid() {
+        return opiskelijaOid;
     }
 
     public Hakemus getHakemus() {
