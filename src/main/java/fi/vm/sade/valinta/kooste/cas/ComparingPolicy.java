de126f2823a8cd31580782b4c16bf9a117139c91
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-05-17 16:23:55 +0300

    test files removed

--- src/main/java/fi/vm/sade/valinta/kooste/cas/ComparingPolicy.java
+++
@@ -1,6 +0,0 @@
-package fi.vm.sade.valinta.kooste.cas;
-
-public interface ComparingPolicy {
-    String getTicketFromCacheExposed(String cacheKey);
-    void putTicketToCacheExposed(String cacheKey, String ticket);
-}
