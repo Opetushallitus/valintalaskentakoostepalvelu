81a92944a26ec8041f8e002e2767ce5e2571b07c
Joni Mäkelä <joni.makela@gmail.com>
2017-10-02 08:39:52 +0300

    Cache observables instead of using blocking observable calls in the cache loader lambda

--- src/main/java/fi/vm/sade/valinta/kooste/parametrit/service/HakuParametritService.java
+++ src/main/java/fi/vm/sade/valinta/kooste/parametrit/service/HakuParametritService.java
@@ -2,8 +2,6 @@ package fi.vm.sade.valinta.kooste.parametrit.service;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-
-import fi.vm.sade.valinta.http.HttpExceptionWithResponse;
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.OhjausparametritAsyncResource;
 import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
 import fi.vm.sade.valinta.kooste.parametrit.ParametritParser;
@@ -11,20 +9,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import rx.Observable;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class HakuParametritService {
 
     private static final Logger LOG = LoggerFactory.getLogger(HakuParametritService.class);
 
-    private String rootOrganisaatioOid;
+    private final String rootOrganisaatioOid;
     private OhjausparametritAsyncResource ohjausparametritAsyncResource;
 
-    private final Cache<String, ParametritParser> haunOhjausParametritCache;
-    private final int requestTimeoutSeconds;
+    private final Cache<String, Observable<ParametritDTO>> haunOhjausParametritCache;
 
     @Autowired
     public HakuParametritService(OhjausparametritAsyncResource ohjausparametritAsyncResource, @Value("${root.organisaatio.oid:1.2.246.562.10.00000000001}") String rootOrganisaatioOid,
@@ -33,19 +29,18 @@ public class HakuParametritService {
         this.ohjausparametritAsyncResource = ohjausparametritAsyncResource;
         this.rootOrganisaatioOid = rootOrganisaatioOid;
         haunOhjausParametritCache = CacheBuilder.newBuilder().expireAfterWrite(cacheTtlMinutes, TimeUnit.MINUTES).build();
-        this.requestTimeoutSeconds = requestTimeoutSeconds;
         LOG.info("Initialized with haunOhjausParametritCache ttl minutes " + cacheTtlMinutes + "and request timeout seconds " + requestTimeoutSeconds);
     }
 
     public ParametritParser getParametritForHaku(String hakuOid) {
         try {
-            return haunOhjausParametritCache.get(hakuOid, () -> {
-                ParametritDTO first = ohjausparametritAsyncResource.haeHaunOhjausparametrit(hakuOid).toBlocking().first();
-                return new ParametritParser(first,this.rootOrganisaatioOid);
-            });
-        } catch (ExecutionException e) {
+            Observable<ParametritDTO> parametritDTOObservable = haunOhjausParametritCache.get(hakuOid,
+                    () -> ohjausparametritAsyncResource.haeHaunOhjausparametrit(hakuOid).single());
+            ParametritDTO paramDto = parametritDTOObservable.toBlocking().single();
+            return new ParametritParser(paramDto, rootOrganisaatioOid);
+        } catch (Exception e) {
+            LOG.error(String.format("Error querying ParametritDTO for haku {}", hakuOid), e);
             throw new RuntimeException(e);
         }
     }
-
 }
