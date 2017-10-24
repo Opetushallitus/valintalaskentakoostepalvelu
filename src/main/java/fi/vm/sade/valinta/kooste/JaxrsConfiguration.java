8139337689e3c80561d802343ba253403e831eca
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-10-24 11:08:25 +0300

    BUG-1560 BUG-1562: LocalDate deserialisointi

--- src/main/java/fi/vm/sade/valinta/kooste/JaxrsConfiguration.java
+++ src/main/java/fi/vm/sade/valinta/kooste/JaxrsConfiguration.java
@@ -2,8 +2,11 @@ package fi.vm.sade.valinta.kooste;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import fi.vm.sade.valinta.kooste.proxy.resource.valintatulosservice.ValintaTulosServiceProxyResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +18,10 @@ public class JaxrsConfiguration {
     public JacksonJsonProvider getJacksonJsonProvider() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.registerModule(new ValintaTulosServiceProxyResource.ValintaTulosServiceSerializersModule());
+        objectMapper.registerModule(new ValintaTulosServiceProxyResource.ValintaTulosServiceSerializersModule())
+                .registerModule(new JavaTimeModule())
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module());
         return new JacksonJsonProvider(objectMapper);
     }
 }
