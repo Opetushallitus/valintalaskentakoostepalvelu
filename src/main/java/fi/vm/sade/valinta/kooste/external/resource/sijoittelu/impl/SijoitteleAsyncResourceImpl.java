b05b3b889576f9afdc161e24d096f1c5cdefbe05
Mikko Siukola <mikko.siukola@cybercom.com>
2017-11-01 11:44:57 +0200

    Pientä siistimistä PR-kommenttien pohjalta

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/sijoittelu/impl/SijoitteleAsyncResourceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/sijoittelu/impl/SijoitteleAsyncResourceImpl.java
@@ -1,43 +1,39 @@
 package fi.vm.sade.valinta.kooste.external.resource.sijoittelu.impl;
 
+import fi.vm.sade.sijoittelu.domain.SijoitteluajonTila;
+import fi.vm.sade.valinta.kooste.external.resource.UrlConfiguredResource;
+import fi.vm.sade.valinta.kooste.external.resource.sijoittelu.SijoitteleAsyncResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.MediaType;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
-
-import javax.ws.rs.core.MediaType;
-
-import fi.vm.sade.sijoittelu.domain.HaunSijoittelunTila;
-import fi.vm.sade.valinta.kooste.external.resource.UrlConfiguredResource;
-import fi.vm.sade.valinta.kooste.url.UrlConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.google.common.reflect.TypeToken;
-
-import fi.vm.sade.valinta.kooste.external.resource.sijoittelu.SijoitteleAsyncResource;
 
 @Service
 public class SijoitteleAsyncResourceImpl extends UrlConfiguredResource implements SijoitteleAsyncResource {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(SijoitteleAsyncResourceImpl.class);
+    private final static int MAX_POLL_INTERVALL_IN_SECONDS = 30;
+    private final static int ADDED_WAIT_PER_POLL_IN_SECONDS = 3;
     public SijoitteleAsyncResourceImpl() {
         super(TimeUnit.MINUTES.toMillis(50));
     }
 
     @Override
     public void sijoittele(String hakuOid, Consumer<String> callback, Consumer<Throwable> failureCallback) {
-        //LOGGER.info("Sijoitellaan pollaten haulle {}", hakuOid);
+        LOGGER.info("Sijoitellaan pollaten haulle {}", hakuOid);
         int secondsUntilNextPoll = 3;
         String status = "";
         AtomicReference<Boolean> done = new AtomicReference<>(false);
-        Long sijoitteluId = -1L;
+        Long sijoitteluajoId = -1L;
 
-        //Luodaan sijoittelu, saadaan palautusarvona sen id, jota käytetään pollattaessa toista rajapintaa.
+        //Luodaan sijoitteluajo, saadaan palautusarvona sen id, jota käytetään pollattaessa toista rajapintaa.
         String luontiUrl = getUrl("sijoittelu-service.sijoittele", hakuOid);
         try {
-            sijoitteluId = getWebClient()
+            sijoitteluajoId = getWebClient()
                     .path(luontiUrl)
                     .accept(MediaType.WILDCARD_TYPE)
                     .async()
@@ -45,23 +41,23 @@ public class SijoitteleAsyncResourceImpl extends UrlConfiguredResource implement
         } catch (Exception e) {
             LOGGER.info("(Haku {}) sijoittelun rajapintakutsu epäonnistui", hakuOid);
         }
-        //Jos rajapinta palauttaa -1 tai kutsu epäonnistuu, uutta sijoittelua ei luotu. Ei aloiteta pollausta.
-        if(sijoitteluId == -1) {
+        //Jos rajapinta palauttaa -1 tai kutsu epäonnistuu, uutta sijoitteluajoa ei luotu. Ei aloiteta pollausta.
+        if(sijoitteluajoId == -1) {
             LOGGER.error("Uuden sijoittelun luonti haulle {} epäonnistui", hakuOid);
             failureCallback.accept(new Exception());
             return;
         }
 
-        LOGGER.info("(Haku: {}) Sijoittelu on käynnistynyt id:llä {}. Pollataan kunnes se on päättynyt.", hakuOid, sijoitteluId);
-        String pollingUrl = getUrl("sijoittelu-service.sijoittele.ajontila", sijoitteluId);
+        LOGGER.info("(Haku: {}) Sijoittelu on käynnistynyt id:llä {}. Pollataan kunnes se on päättynyt.", hakuOid, sijoitteluajoId);
+        String pollingUrl = getUrl("sijoittelu-service.sijoittele.ajontila", sijoitteluajoId);
         while (!done.get()) {
             try {
                 TimeUnit.SECONDS.sleep(secondsUntilNextPoll);
             } catch (Exception e) {
                 throw new RuntimeException();
             }
-            if (secondsUntilNextPoll < 30) {
-                secondsUntilNextPoll += 3;
+            if (secondsUntilNextPoll < MAX_POLL_INTERVALL_IN_SECONDS) {
+                secondsUntilNextPoll += ADDED_WAIT_PER_POLL_IN_SECONDS;
             }
             try {
                 status = getWebClient()
@@ -71,20 +67,20 @@ public class SijoitteleAsyncResourceImpl extends UrlConfiguredResource implement
                         .get(String.class).get();
 
                 LOGGER.info("Saatiin ajontila-rajapinnalta palautusarvo {}", status);
-                if (HaunSijoittelunTila.VALMIS.equals(status)) {
-                    LOGGER.info("#### Sijoittelu {} haulle {} on valmistunut", sijoitteluId, hakuOid);
+                if (SijoitteluajonTila.VALMIS.toString().equals(status)) {
+                    LOGGER.info("#### Sijoittelu {} haulle {} on valmistunut", sijoitteluajoId, hakuOid);
                     callback.accept(status);
                     done.set(true);
                     return;
                 }
-                if (HaunSijoittelunTila.VIRHE.equals(status)) {
-                    LOGGER.info("#### Sijoittelu {} haulle {} päättyi virheeseen", sijoitteluId, hakuOid);
+                if (SijoitteluajonTila.VIRHE.toString().equals(status)) {
+                    LOGGER.info("#### Sijoittelu {} haulle {} päättyi virheeseen", sijoitteluajoId, hakuOid);
                     failureCallback.accept(new Exception());
                     done.set(true);
                     return;
                 }
             } catch (Exception e) {
-                LOGGER.info("Sijoittelussa {} haulle {} tapahtui virhe", sijoitteluId, hakuOid, e);
+                LOGGER.info("Sijoittelussa {} haulle {} tapahtui virhe", sijoitteluajoId, hakuOid, e);
                 failureCallback.accept(e);
                 return;
             }
