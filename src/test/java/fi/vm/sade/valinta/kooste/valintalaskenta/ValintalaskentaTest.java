075aa115a25ab9b71ec6407a17a49390b554788e
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-11-08 12:49:31 +0200

    BUG-1568 : Adapt failing test from OK-115

--- src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/ValintalaskentaTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/ValintalaskentaTest.java
@@ -1,178 +1,149 @@
 package fi.vm.sade.valinta.kooste.valintalaskenta;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetDTO;
+import fi.vm.sade.tarjonta.service.resources.v1.dto.HakuV1RDTO;
+import fi.vm.sade.valinta.kooste.external.resource.hakuapp.ApplicationAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.hakuapp.dto.Hakemus;
+import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.OhjausparametritAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.seuranta.LaskentaSeurantaAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.SuoritusrekisteriAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.dto.Oppija;
+import fi.vm.sade.valinta.kooste.external.resource.tarjonta.TarjontaAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.valintalaskenta.ValintalaskentaAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
+import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaActorFactory;
+import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaActorSystem;
+import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaStarter;
+import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.HakukohdeJaOrganisaatio;
+import fi.vm.sade.valinta.kooste.valintalaskenta.dto.LaskentaStartParams;
+import fi.vm.sade.valinta.seuranta.dto.HakukohdeTila;
+import fi.vm.sade.valinta.seuranta.dto.LaskentaTila;
+import fi.vm.sade.valinta.seuranta.dto.LaskentaTyyppi;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import rx.Observable;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
 
-import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.OhjausparametritAsyncResource;
-import fi.vm.sade.valinta.kooste.external.resource.tarjonta.TarjontaAsyncResource;
-import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaActorFactory;
-import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaStarter;
-import fi.vm.sade.tarjonta.service.resources.v1.dto.HakuV1RDTO;
-import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetHakijaryhmaDTO;
-import fi.vm.sade.valinta.kooste.external.resource.Peruutettava;
-import fi.vm.sade.valinta.kooste.external.resource.TyhjaPeruutettava;
-import fi.vm.sade.valinta.kooste.external.resource.hakuapp.ApplicationAsyncResource;
-import fi.vm.sade.valinta.kooste.external.resource.seuranta.LaskentaSeurantaAsyncResource;
-import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.SuoritusrekisteriAsyncResource;
-import fi.vm.sade.valinta.kooste.external.resource.valintalaskenta.ValintalaskentaAsyncResource;
-import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
-import fi.vm.sade.valinta.kooste.valintalaskenta.actor.LaskentaActorSystem;
-import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.HakukohdeJaOrganisaatio;
-import fi.vm.sade.valinta.kooste.valintalaskenta.dto.LaskentaStartParams;
-import fi.vm.sade.valinta.kooste.valintalaskenta.route.ValintalaskentaKerrallaRoute;
-import fi.vm.sade.valinta.seuranta.dto.HakukohdeTila;
-import fi.vm.sade.valinta.seuranta.dto.LaskentaTila;
-import fi.vm.sade.valinta.seuranta.dto.LaskentaTyyppi;
-
-/**
- * 
- * @author Jussi Jartamo
- * 
- * 
- */
 public class ValintalaskentaTest {
-	private final static Logger LOG = LoggerFactory
-			.getLogger(ValintalaskentaTest.class);
+    private static final Hakemus hakemus = new Hakemus();
+    private final ApplicationAsyncResource applicationAsyncResource = mock(ApplicationAsyncResource.class);
+    private final SuoritusrekisteriAsyncResource suoritusrekisteriAsyncResource = mock(SuoritusrekisteriAsyncResource.class);
+    private final ValintalaskentaAsyncResource valintalaskentaAsyncResource = mock(ValintalaskentaAsyncResource.class);
+    private final ValintaperusteetAsyncResource valintaperusteetAsyncResource = mock(ValintaperusteetAsyncResource.class);
+    private final LaskentaSeurantaAsyncResource seurantaAsyncResource = mock(LaskentaSeurantaAsyncResource.class);
+    private final TarjontaAsyncResource tarjontaAsyncResource = mock(TarjontaAsyncResource.class);
+    private final OhjausparametritAsyncResource ohjausparametritAsyncResource = mock(OhjausparametritAsyncResource.class);
+    private final LaskentaActorSystem laskentaActorSystem = new LaskentaActorSystem(seurantaAsyncResource, new LaskentaStarter(ohjausparametritAsyncResource, valintaperusteetAsyncResource, seurantaAsyncResource, tarjontaAsyncResource), new LaskentaActorFactory(
+        5,
+        valintalaskentaAsyncResource,
+        applicationAsyncResource,
+        valintaperusteetAsyncResource,
+        seurantaAsyncResource,
+        suoritusrekisteriAsyncResource,
+        tarjontaAsyncResource
+    ), 8);
+    private final String hakukohde1Oid = "h1";
+    private final String hakukohde2Oid = "h2";
+    private final String hakukohde3Oid = "h3";
+    private final String uuid = "uuid";
+    private final String hakuOid = "hakuOid";
+    private final HakuV1RDTO hakuDTO = new HakuV1RDTO();
 
-	@Test
-	public void testaaValintalaskentaa() {
-		List<HakukohdeJaOrganisaatio> hakukohdeOids = Arrays.asList(
-				new HakukohdeJaOrganisaatio("h1", "o1"),
-				new HakukohdeJaOrganisaatio("h2", "o2"),
-				new HakukohdeJaOrganisaatio("h3", "o3"));
-		String uuid = "uuid";
-		String hakuOid = "hakuOid";
-		final HakuV1RDTO hakuDTO = new HakuV1RDTO();
-		hakuDTO.setOid(hakuOid);
-		LaskentaSeurantaAsyncResource seurantaAsyncResource = createMockLaskentaSeurantaAsyncResource();
-		ValintaperusteetAsyncResource valintaperusteetAsyncResource = createMockValintaperusteetAsyncResource();
-		ValintalaskentaAsyncResource valintalaskentaAsyncResource = createMockValintalaskentaAsyncResource();
-		ApplicationAsyncResource applicationAsyncResource = createMockApplicationAsyncResource();
-		SuoritusrekisteriAsyncResource suoritusrekisteriAsyncResource = createMockSuoritusrekisteriAsyncResource();
-		TarjontaAsyncResource tarjontaAsyncResource = mock(TarjontaAsyncResource.class);
-        OhjausparametritAsyncResource ohjausparametritAsyncResource = mock(OhjausparametritAsyncResource.class);
-		LaskentaActorSystem laskentaActorSystem = new LaskentaActorSystem(seurantaAsyncResource, new LaskentaStarter(ohjausparametritAsyncResource, valintaperusteetAsyncResource,seurantaAsyncResource, tarjontaAsyncResource), new LaskentaActorFactory(
-				5,
-				valintalaskentaAsyncResource,
-				applicationAsyncResource,
-				valintaperusteetAsyncResource,
-				seurantaAsyncResource,
-				suoritusrekisteriAsyncResource,
-				tarjontaAsyncResource
-		), 8);
+    @Before
+    public void setUpTestData() {
+        hakemus.setPersonOid("personOid");
+        hakuDTO.setOid(hakuOid);
+    }
 
-		ValintalaskentaKerrallaRoute valintalaskentaKerrallaRoute = laskentaActorSystem;
-		LaskentaStartParams laskentaJaHaku = new LaskentaStartParams(uuid, hakuOid,false,
-				null, null, hakukohdeOids, LaskentaTyyppi.HAKUKOHDE);
-		valintalaskentaKerrallaRoute
-				.suoritaValintalaskentaKerralla(hakuDTO, null, laskentaJaHaku);
-	}
+    @Test
+    public void onnistuneestaValintalaskennastaPidetaanKirjaaSeurantapalveluun() throws InterruptedException {
+        List<HakukohdeJaOrganisaatio> hakukohdeOids = Arrays.asList(
+            new HakukohdeJaOrganisaatio(hakukohde1Oid, "o1"),
+            new HakukohdeJaOrganisaatio(hakukohde2Oid, "o2"),
+            new HakukohdeJaOrganisaatio(hakukohde3Oid, "o3"));
 
-	public static LaskentaSeurantaAsyncResource createMockLaskentaSeurantaAsyncResource() {
-		LaskentaSeurantaAsyncResource asyncResource = mock(LaskentaSeurantaAsyncResource.class);
+        when(valintaperusteetAsyncResource.haeValintaperusteet(any(), any())).thenReturn(
+            Observable.just(Collections.singletonList(new ValintaperusteetDTO())));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde1Oid)).thenReturn(Observable.just(Collections.emptyList()));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde2Oid)).thenReturn(Observable.just(Collections.emptyList()));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde3Oid)).thenReturn(Observable.just(Collections.emptyList()));
 
-		Mockito.doAnswer(new Answer<Object>() {
-			@Override
-			public Object answer(InvocationOnMock invocation) throws Throwable {
-				LOG.error("Laskennan seurantaa kutsuttiin hakukohteen merkkaukselle");
-				return null;
-			}
-		})
-				.when(asyncResource)
-				.merkkaaHakukohteenTila(anyString(), anyString(),
-						any(HakukohdeTila.class), any(Optional.class));
-		Mockito.doAnswer(new Answer<Object>() {
-			@Override
-			public Object answer(InvocationOnMock invocation) throws Throwable {
-				LOG.error("Laskennan seurantaa kutsuttiin hakukohteen merkkaukselle");
-				return null;
-			}
-		}).when(asyncResource)
-				.merkkaaLaskennanTila(anyString(), any(LaskentaTila.class), any(Optional.class));
+        when(valintalaskentaAsyncResource.laskeKaikki(any())).thenReturn(Observable.just("OK"));
 
-		return asyncResource;
-	}
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde1Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde2Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde3Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
 
-	public static ValintaperusteetAsyncResource createMockValintaperusteetAsyncResource() {
-		ValintaperusteetAsyncResource valintaperusteetAsyncResource = mock(ValintaperusteetAsyncResource.class);
-		when(
-				valintaperusteetAsyncResource.haeHakijaryhmat(anyString(),
-						any(), any())).then(new Answer<Peruutettava>() {
-			@SuppressWarnings("unchecked")
-			@Override
-			public Peruutettava answer(InvocationOnMock invocation)
-					throws Throwable {
-				LOG.info("Palvelukutsun I/O:n tekeva funktio!");
-				Consumer<List<ValintaperusteetHakijaryhmaDTO>> hakijaryhmatCallback = (Consumer<List<ValintaperusteetHakijaryhmaDTO>>) invocation
-						.getArguments()[1];
-				hakijaryhmatCallback.accept(Collections.emptyList());
-				return TyhjaPeruutettava.tyhjaPeruutettava();
-			}
-		});
-		return valintaperusteetAsyncResource;
-	}
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde1Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde2Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde3Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
 
-	public static ValintalaskentaAsyncResource createMockValintalaskentaAsyncResource() {
-		ValintalaskentaAsyncResource asyncResource = mock(ValintalaskentaAsyncResource.class);
-		// when(asyncResource.merkkaaHakukohteenTila(anyString(), anyString(),
-		// any(HakukohdeTila.class));
+        when(tarjontaAsyncResource.hakukohdeRyhmasForHakukohdes(hakuOid)).thenReturn(Observable.just(Collections.emptyMap()));
 
-		// then(new Answer<Void>() {
-		// @SuppressWarnings("unchecked")
-		// @Override
-		// public Void answer(InvocationOnMock invocation) throws Throwable {
-		// LOG.info("Palvelukutsun I/O:n tekeva funktio!");
-		// Consumer<List<ValintaperusteetHakijaryhmaDTO>> hakijaryhmatCallback =
-		// (Consumer<List<ValintaperusteetHakijaryhmaDTO>>) invocation
-		// .getArguments()[1];
-		// hakijaryhmatCallback.accept(Collections.emptyList());
-		// return null;
-		// }
-		// });
-		return asyncResource;
-	}
+        LaskentaStartParams laskentaJaHaku = new LaskentaStartParams(uuid, hakuOid, false, null, null, hakukohdeOids, LaskentaTyyppi.HAKUKOHDE);
 
-	public static SuoritusrekisteriAsyncResource createMockSuoritusrekisteriAsyncResource() {
-		SuoritusrekisteriAsyncResource suoritusrekisteriAsyncResource = mock(SuoritusrekisteriAsyncResource.class);
-		return suoritusrekisteriAsyncResource;
-	}
+        laskentaActorSystem.suoritaValintalaskentaKerralla(hakuDTO, null, laskentaJaHaku);
+        Thread.sleep(500);
 
-	public static ApplicationAsyncResource createMockApplicationAsyncResource() {
-		ApplicationAsyncResource asyncResource = mock(ApplicationAsyncResource.class);
-		// when(asyncResource.merkkaaHakukohteenTila(anyString(), anyString(),
-		// any(HakukohdeTila.class));
+        verify(seurantaAsyncResource).otaSeuraavaLaskentaTyonAlle(any(), any());
+        verify(seurantaAsyncResource).merkkaaHakukohteenTila(uuid, hakukohde1Oid, HakukohdeTila.VALMIS, Optional.empty());
+        verify(seurantaAsyncResource).merkkaaHakukohteenTila(uuid, hakukohde2Oid, HakukohdeTila.VALMIS, Optional.empty());
+        verify(seurantaAsyncResource).merkkaaHakukohteenTila(uuid, hakukohde3Oid, HakukohdeTila.VALMIS, Optional.empty());
+        verify(seurantaAsyncResource).merkkaaLaskennanTila(uuid, LaskentaTila.VALMIS, Optional.empty());
+        Mockito.verifyNoMoreInteractions(seurantaAsyncResource);
+    }
 
-		// then(new Answer<Void>() {
-		// @SuppressWarnings("unchecked")
-		// @Override
-		// public Void answer(InvocationOnMock invocation) throws Throwable {
-		// LOG.info("Palvelukutsun I/O:n tekeva funktio!");
-		// Consumer<List<ValintaperusteetHakijaryhmaDTO>> hakijaryhmatCallback =
-		// (Consumer<List<ValintaperusteetHakijaryhmaDTO>>) invocation
-		// .getArguments()[1];
-		// hakijaryhmatCallback.accept(Collections.emptyList());
-		// return null;
-		// }
-		// });
-		return asyncResource;
-	}
+
+    @Test
+    public void epaonnistuneetLaskennatKirjataanSeurantapalveluun() throws InterruptedException {
+        List<HakukohdeJaOrganisaatio> hakukohdeOids = Arrays.asList(
+            new HakukohdeJaOrganisaatio(hakukohde1Oid, "o1"),
+            new HakukohdeJaOrganisaatio(hakukohde2Oid, "o2"),
+            new HakukohdeJaOrganisaatio(hakukohde3Oid, "o3"));
+
+        when(valintaperusteetAsyncResource.haeValintaperusteet(any(), any())).thenReturn(
+            Observable.just(Collections.singletonList(new ValintaperusteetDTO())));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde1Oid)).thenReturn(Observable.just(Collections.emptyList()));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde2Oid)).thenReturn(Observable.just(Collections.emptyList()));
+        when(valintaperusteetAsyncResource.haeHakijaryhmat(hakukohde3Oid)).thenReturn(Observable.just(Collections.emptyList()));
+
+        when(valintalaskentaAsyncResource.laskeKaikki(any())).thenReturn(Observable.just("OK"));
+
+//        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde1Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde1Oid)).thenReturn(
+            Observable.error(new RuntimeException(getClass().getSimpleName() + " : Ei saatu haettua hakemuksia kohteelle " + hakukohde1Oid)));
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde2Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
+//        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde2Oid)).thenReturn(
+//            Observable.error(new RuntimeException(getClass().getSimpleName() + " : Ei saatu haettua hakemuksia kohteelle " + hakukohde2Oid)));
+        when(applicationAsyncResource.getApplicationsByOid(hakuOid, hakukohde3Oid)).thenReturn(Observable.just(Collections.singletonList(hakemus)));
+
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde1Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde2Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
+        when(suoritusrekisteriAsyncResource.getOppijatByHakukohde(hakukohde3Oid, hakuOid)).thenReturn(Observable.just(Collections.singletonList(new Oppija())));
+
+        when(tarjontaAsyncResource.hakukohdeRyhmasForHakukohdes(hakuOid)).thenReturn(Observable.just(Collections.emptyMap()));
+
+        LaskentaStartParams laskentaJaHaku = new LaskentaStartParams(uuid, hakuOid, false, null, null, hakukohdeOids, LaskentaTyyppi.HAKUKOHDE);
+
+        laskentaActorSystem.suoritaValintalaskentaKerralla(hakuDTO, null, laskentaJaHaku);
+        Thread.sleep(500);
+
+//        verify(seurantaAsyncResource).otaSeuraavaLaskentaTyonAlle(any(), any());
+//        verify(seurantaAsyncResource).merkkaaHakukohteenTila(uuid, hakukohde1Oid, HakukohdeTila.VALMIS, Optional.empty());
+        verify(seurantaAsyncResource).merkkaaHakukohteenTila(uuid, hakukohde2Oid, HakukohdeTila.VALMIS, Optional.empty());
+        verify(seurantaAsyncResource, times(2)).merkkaaHakukohteenTila(uuid, hakukohde3Oid, HakukohdeTila.VALMIS, Optional.empty()); // TODO one extra call
+        verify(seurantaAsyncResource, times(2)).merkkaaLaskennanTila(uuid, LaskentaTila.VALMIS, Optional.empty()); // TODO one extra call
+        //Mockito.verifyNoMoreInteractions(seurantaAsyncResource); // TODO FIXME
+    }
 }
