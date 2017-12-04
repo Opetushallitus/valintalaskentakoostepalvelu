eb701056ec83ed4a468ba86033cda883e692acaf
Joni Mäkelä <joni.makela@gmail.com>
2017-12-04 08:58:23 +0200

    Test code layout changes
    - Created helper function for dto creation
    - Split up tests into separate functions

--- src/test/java/fi/vm/sade/valinta/kooste/external/resource/oppijanumerorekisteri/dto/HenkiloCreateDTOTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/external/resource/oppijanumerorekisteri/dto/HenkiloCreateDTOTest.java
@@ -7,48 +7,37 @@ import static org.junit.Assert.*;
 public class HenkiloCreateDTOTest {
     @Test
     public void equals() throws Exception {
-        String aidinkieli = "fi";
-        String sukupuoli = "MIES";
-        String etunimet = "Matti";
-        String sukunimi = "Meikäläinen";
-        String hetu = "123456-789A";
-        String syntymaAika = "11.11.2011";
-        String henkiloOid = "henkilo1";
-        HenkiloTyyppi henkiloTyyppi = HenkiloTyyppi.OPPIJA;
-        String asiointikieli = "fi";
-        String kansalaisuus = "FIN";
-
-        HenkiloCreateDTO dto1 = new HenkiloCreateDTO(
-                aidinkieli,
-                sukupuoli,
-                etunimet,
-                sukunimi,
-                hetu,
-                syntymaAika,
-                henkiloOid,
-                henkiloTyyppi,
-                asiointikieli,
-                kansalaisuus);
-        HenkiloCreateDTO dto2 = new HenkiloCreateDTO(
-                aidinkieli,
-                sukupuoli,
-                etunimet,
-                sukunimi,
-                hetu,
-                syntymaAika,
-                henkiloOid,
-                henkiloTyyppi,
-                asiointikieli,
-                kansalaisuus);
+        HenkiloCreateDTO dto1 = getHenkiloWithFirstname("Matti");
+        HenkiloCreateDTO dto2 = getHenkiloWithFirstname("Matti");
 
         assertTrue(dto1.equals(dto2));
     }
 
     @Test
-    public void multipleFirstNames() throws Exception {
+    public void multipleFirstNames() {
+        HenkiloCreateDTO dto = getHenkiloWithFirstname("Henrikki Aapeli Testi");
+        assertEquals("Kutsumanimi should be the first one of first names","Henrikki", dto.kutsumanimi);
+    }
+
+    @Test
+    public void multipleFirstNamesWithHyphen(){
+        HenkiloCreateDTO dto = getHenkiloWithFirstname("Henrikki-Aapeli Testi");
+        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
+    }
+
+    @Test
+    public void multipleFirstNamesWithHyphenTrimming() {
+        //testaa konstruktorissa tapahtuvaa nimen trimmausta, huomioi välilyönti edessä
+        HenkiloCreateDTO dto = getHenkiloWithFirstname(" Henrikki-Aapeli Testi");
+        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
+
+        dto = getHenkiloWithFirstname(" Henrikki-Aapeli Testi  ");
+        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
+    }
+
+    private HenkiloCreateDTO getHenkiloWithFirstname(String firstname) {
         String aidinkieli = "fi";
         String sukupuoli = "MIES";
-        String etunimet = "Henrikki Aapeli Testi";
         String sukunimi = "Hakija";
         String hetu = "123456-789A";
         String syntymaAika = "11.11.2011";
@@ -57,10 +46,10 @@ public class HenkiloCreateDTOTest {
         String asiointikieli = "fi";
         String kansalaisuus = "FIN";
 
-        HenkiloCreateDTO dto = new HenkiloCreateDTO(
+        return new HenkiloCreateDTO(
                 aidinkieli,
                 sukupuoli,
-                etunimet,
+                firstname,
                 sukunimi,
                 hetu,
                 syntymaAika,
@@ -68,54 +57,6 @@ public class HenkiloCreateDTOTest {
                 henkiloTyyppi,
                 asiointikieli,
                 kansalaisuus);
-
-        assertEquals("Kutsumanimi should be the first one of first names","Henrikki", dto.kutsumanimi);
-
-        etunimet = "Henrikki-Aapeli Testi";
-        dto = new HenkiloCreateDTO(
-                aidinkieli,
-                sukupuoli,
-                etunimet,
-                sukunimi,
-                hetu,
-                syntymaAika,
-                henkiloOid,
-                henkiloTyyppi,
-                asiointikieli,
-                kansalaisuus);
-
-        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
-
-        //testaa konstruktorissa tapahtuvaa nimen trimmausta, huomioi välilyönti edessä
-        etunimet = " Henrikki-Aapeli Testi";
-        dto = new HenkiloCreateDTO(
-                aidinkieli,
-                sukupuoli,
-                etunimet,
-                sukunimi,
-                hetu,
-                syntymaAika,
-                henkiloOid,
-                henkiloTyyppi,
-                asiointikieli,
-                kansalaisuus);
-
-        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
-
-        etunimet = " Henrikki-Aapeli Testi  ";
-        dto = new HenkiloCreateDTO(
-                aidinkieli,
-                sukupuoli,
-                etunimet,
-                sukunimi,
-                hetu,
-                syntymaAika,
-                henkiloOid,
-                henkiloTyyppi,
-                asiointikieli,
-                kansalaisuus);
-
-        assertEquals("Kutsumanimi should be the first one of first names","Henrikki-Aapeli", dto.kutsumanimi);
     }
 
 }
