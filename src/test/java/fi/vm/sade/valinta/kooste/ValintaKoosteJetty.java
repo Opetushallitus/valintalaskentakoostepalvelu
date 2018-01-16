40ac0909b6cd0b40b05db7cb57c807a3dd120330
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-01-16 15:28:35 +0200

    Add instructions for local development

--- src/test/java/fi/vm/sade/valinta/kooste/ValintaKoosteJetty.java
+++ src/test/java/fi/vm/sade/valinta/kooste/ValintaKoosteJetty.java
@@ -10,12 +10,53 @@ import fi.vm.sade.integrationtest.util.ProjectRootFinder;
  *  Starts valintalaskentakoostepalvelu locally.
  *
  *  Use -Dvalintakooste.profile=it for local mocks.
+ *  It is practical to set a stable HTTP port with e.g. -Dvalintakooste.port=56748
+ *
+ *  Remote usage with a test environment:
+ *
+ *    * add necessary tunnels to your test environments, e.g. have something like this in you ~/.ssh/config :
+ *        Host bastion.pallero
+ *            HostName %h.aws.opintopolku.fi
+ *            ...
+ *            LocalForward 2389 ldap.ldap.pallero.aws.opintopolku.fi:389
+ *            LocalForward 18888 alb.pallero.aws.opintopolku.fi:80
+ *
+ *    * copy oph-configuration directory from the container of valinta to your machine
+ *
+ *    * tweak your oph-configuration for local usage as follows:
+ *        diff -r valinta-pallero-oph-configuration/common.properties local-valinta-pallero-oph-configuration/common.properties
+ *        12a13
+ *        14,16c15,19
+ *        < host.ilb=https://virkailija.pallero.aws.opintopolku.fi
+ *        < host.alb=http://alb.pallero.aws.opintopolku.fi
+ *        < host.ldap=ldap.ldap.pallero.aws.opintopolku.fi
+ *        ---
+ *        > host.ilb=http://localhost:18888
+ *        > host.alb=localhost:18888
+ *        > host.ldap=localhost:2389
+ *        84c87,88
+ *        < cas.service.valintalaskentakoostepalvelu=https://${host.virkailija}/valintalaskentakoostepalvelu
+ *        ---
+ *        > cas.service.valintalaskentakoostepalvelu=http://localhost:56748/valintalaskentakoostepalvelu
+ *        115a120
+ *        > jatkuvasijoittelu.autostart=false
+ *        120c125
+ *        < valintalaskentakoostepalvelu.valintalaskentaService.url=https://${host.virkailija}/valintalaskenta-laskenta-service/services/valintalaskentaService
+ *        ---
+ *        > valintalaskentakoostepalvelu.valintalaskentaService.url=${host.ilb}/valintalaskenta-laskenta-service/services/valintalaskentaService
+ *        143c148
+ *        < valintalaskentakoostepalvelu.maxWorkerCount=4
+ *        ---
+ *        > valintalaskentakoostepalvelu.maxWorkerCount=0
+ *
+ *
+ *    * If something does not work as expected, make it work and improve this documentation accordingly ;)
  */
 public class ValintaKoosteJetty {
     public final static int port = Integer.parseInt(System.getProperty("valintakooste.port", String.valueOf(PortChecker.findFreeLocalPort())));
     private static Server server = new Server(port);
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         startShared("it".equals(System.getProperty("valintakooste.profile")));
     }
 
