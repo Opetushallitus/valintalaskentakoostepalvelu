73590f7c692ae33f4d1de3d89960a12a9e55dbfd
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-01-17 13:35:51 +0200

    Add timeout to BlockingObservable.
    It is dangerous to use BlockingObservables without
    explicit timeouts, as the calls seem to block forever
    without that.

--- src/main/java/fi/vm/sade/valinta/kooste/tarjonta/sync/impl/TarjontaSyncServiceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/tarjonta/sync/impl/TarjontaSyncServiceImpl.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.valinta.kooste.tarjonta.sync.impl;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static rx.observables.BlockingObservable.from;
+
 import fi.vm.sade.valinta.kooste.external.resource.tarjonta.TarjontaAsyncResource;
 import fi.vm.sade.valinta.kooste.hakuimport.route.HakuImportRoute;
 import fi.vm.sade.valinta.kooste.tarjonta.sync.TarjontaSyncService;
@@ -23,7 +25,7 @@ public class TarjontaSyncServiceImpl implements TarjontaSyncService {
     HakuImportRoute hakuImportAktivointiRoute;
 
     public void syncHakukohteetFromTarjonta() {
-        Set<String> hakuOids = from(tarjontaAsyncResource.findHakuOidsForAutosyncTarjonta()).first();
+        Set<String> hakuOids = from(tarjontaAsyncResource.findHakuOidsForAutosyncTarjonta().timeout(1, MINUTES)).first();
         if (hakuOids != null) {
             for (String hakuOid : hakuOids) {
                 LOG.info("Starting synchronization for haku: " + hakuOid);
