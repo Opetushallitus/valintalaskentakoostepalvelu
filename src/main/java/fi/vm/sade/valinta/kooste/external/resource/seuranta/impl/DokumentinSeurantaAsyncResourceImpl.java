c4df919c13d92a74e192a5476db5145e3e02f131
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-01-17 13:54:54 +0200

    BUG-1613 : Use lazy call for dokumentinseuranta

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/seuranta/impl/DokumentinSeurantaAsyncResourceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/seuranta/impl/DokumentinSeurantaAsyncResourceImpl.java
@@ -1,22 +1,19 @@
 package fi.vm.sade.valinta.kooste.external.resource.seuranta.impl;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-
 import fi.vm.sade.valinta.kooste.external.resource.UrlConfiguredResource;
-import fi.vm.sade.valinta.kooste.url.UrlConfiguration;
+import fi.vm.sade.valinta.kooste.external.resource.seuranta.DokumentinSeurantaAsyncResource;
+import fi.vm.sade.valinta.seuranta.dto.DokumenttiDto;
+import fi.vm.sade.valinta.seuranta.dto.VirheilmoitusDto;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
-import fi.vm.sade.valinta.kooste.external.resource.seuranta.DokumentinSeurantaAsyncResource;
-import fi.vm.sade.valinta.seuranta.dto.DokumenttiDto;
-import fi.vm.sade.valinta.seuranta.dto.VirheilmoitusDto;
 import rx.Observable;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DokumentinSeurantaAsyncResourceImpl extends UrlConfiguredResource implements DokumentinSeurantaAsyncResource {
@@ -27,19 +24,19 @@ public class DokumentinSeurantaAsyncResourceImpl extends UrlConfiguredResource i
     }
 
     public Observable<DokumenttiDto> paivitaDokumenttiId(String uuid, String dokumenttiId) {
-        return postAsObservable(getUrl("seuranta-service.dokumentinseuranta.paivitadokumenttiid", uuid), DokumenttiDto.class, Entity.entity(dokumenttiId, MediaType.TEXT_PLAIN));
+        return postAsObservableLazily(getUrl("seuranta-service.dokumentinseuranta.paivitadokumenttiid", uuid), DokumenttiDto.class, Entity.entity(dokumenttiId, MediaType.TEXT_PLAIN));
     }
 
     public Observable<String> luoDokumentti(String kuvaus) {
-        return postAsObservable(getUrl("seuranta-service.dokumentinseuranta"), String.class, Entity.entity(kuvaus, MediaType.TEXT_PLAIN));
+        return postAsObservableLazily(getUrl("seuranta-service.dokumentinseuranta"), String.class, Entity.entity(kuvaus, MediaType.TEXT_PLAIN));
     }
 
     public Observable<DokumenttiDto> paivitaKuvaus(String uuid, String kuvaus) {
-        return postAsObservable(getUrl("seuranta-service.dokumentinseuranta.paivitakuvaus", uuid), DokumenttiDto.class, Entity.entity(kuvaus, MediaType.TEXT_PLAIN));
+        return postAsObservableLazily(getUrl("seuranta-service.dokumentinseuranta.paivitakuvaus", uuid), DokumenttiDto.class, Entity.entity(kuvaus, MediaType.TEXT_PLAIN));
     }
 
     public Observable<DokumenttiDto> lisaaVirheilmoituksia(String uuid, List<VirheilmoitusDto> virheilmoitukset) {
-        return postAsObservable(getUrl("seuranta-service.dokumentinseuranta.lisaavirheita", uuid), DokumenttiDto.class, Entity.json(virheilmoitukset));
+        return postAsObservableLazily(getUrl("seuranta-service.dokumentinseuranta.lisaavirheita", uuid), DokumenttiDto.class, Entity.json(virheilmoitukset));
     }
 }
 
