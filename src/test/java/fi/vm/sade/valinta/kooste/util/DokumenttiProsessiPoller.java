e4b3fc1937200619f4ff6dfe5ed888158fde6151
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-02-18 10:28:56 +0200

    Increase test timeout + relax poll interval
    Let's see if HyvaksymiskirjeetKokoHaulleServiceE2ETest
    would fail less on Travis with these changes.

--- src/test/java/fi/vm/sade/valinta/kooste/util/DokumenttiProsessiPoller.java
+++ src/test/java/fi/vm/sade/valinta/kooste/util/DokumenttiProsessiPoller.java
@@ -3,22 +3,26 @@ package fi.vm.sade.valinta.kooste.util;
 import fi.vm.sade.valinta.sharedutils.http.HttpResourceBuilder;
 import fi.vm.sade.valinta.kooste.erillishaku.resource.dto.Prosessi;
 import fi.vm.sade.valinta.kooste.viestintapalvelu.dto.ProsessiId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.function.Function;
 
 public class DokumenttiProsessiPoller {
-    private static final Duration TIME_TO_WAIT_IN_TOTAL = Duration.ofSeconds(60);
-    private static final Duration REQUEST_INTERVAL = Duration.ofMillis(10);
+    private static final Duration TIME_TO_WAIT_IN_TOTAL = Duration.ofSeconds(150);
+    private static final Duration REQUEST_INTERVAL = Duration.ofMillis(100);
+    private static Logger LOG = LoggerFactory.getLogger(DokumenttiProsessiPoller.class);
 
     public static Prosessi pollDokumenttiProsessi(String rootUrl, ProsessiId prosessiId, Function<Prosessi,Boolean> responseProcessor) {
         final HttpResourceBuilder.WebClientExposingHttpResource dokumenttiProsessiResource = new HttpResourceBuilder()
             .address(rootUrl + "/dokumenttiprosessi/" + prosessiId.getId())
             .buildExposingWebClientDangerously();
         long pollStarted = System.currentTimeMillis();
-        sleepOneInterval(); // give the server some time to get started
+        sleep(REQUEST_INTERVAL.multipliedBy(8)); // give the server some time to get started
         while (System.currentTimeMillis() < pollStarted + TIME_TO_WAIT_IN_TOTAL.toMillis()) {
             Prosessi prosessiStatusResponse = dokumenttiProsessiResource.getWebClient().get(Prosessi.class);
+            LOG.info("prosessiStatusResponse = " + prosessiStatusResponse);
             if (responseProcessor.apply(prosessiStatusResponse)) {
                 return prosessiStatusResponse;
             }
@@ -28,8 +32,12 @@ public class DokumenttiProsessiPoller {
     }
 
     private static void sleepOneInterval() {
+        sleep(REQUEST_INTERVAL);
+    }
+
+    private static void sleep(Duration duration) {
         try {
-            Thread.sleep(REQUEST_INTERVAL.toMillis());
+            Thread.sleep(duration.toMillis());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
