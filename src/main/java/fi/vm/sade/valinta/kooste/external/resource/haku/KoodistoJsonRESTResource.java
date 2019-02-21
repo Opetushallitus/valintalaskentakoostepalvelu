803dbcb3348a4447d1db5700b858a9236d0fb5bd
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-02-21 16:08:01 +0200

    Clean up
    No functional changes

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/haku/KoodistoJsonRESTResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/haku/KoodistoJsonRESTResource.java
@@ -2,19 +2,21 @@ package fi.vm.sade.valinta.kooste.external.resource.haku;
 
 import fi.vm.sade.valinta.kooste.external.resource.haku.dto.KoodistoUrheilija;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/json")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public interface KoodistoJsonRESTResource {
-
-    public static final String KOODI_VERSIO = "koodiVersio";
-    public static final String KOODI_URI = "koodiUri";
-    public static final int ONE_HOUR = 60 * 60;
+    String KOODI_VERSIO = "koodiVersio";
+    String KOODI_URI = "koodiUri";
 
     @GET
     @Path("/relaatio/sisaltyy-alakoodit/{koodiUri}")
-    public List<KoodistoUrheilija> getAlakoodis(@PathParam(KOODI_URI) String koodiUri, @QueryParam(KOODI_VERSIO) Integer koodiVersio);
+    List<KoodistoUrheilija> getAlakoodis(@PathParam(KOODI_URI) String koodiUri, @QueryParam(KOODI_VERSIO) Integer koodiVersio);
 }
