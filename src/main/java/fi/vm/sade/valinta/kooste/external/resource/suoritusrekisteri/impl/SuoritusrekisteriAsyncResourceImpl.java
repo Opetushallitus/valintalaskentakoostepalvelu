41251a1d3d497ccf87672f2c44c0d2b70ff457bd
teo.mertanen <teo.mertanen@gofore.com>
2019-04-02 16:00:57 +0300

    BUG-1962 return logging to batch POST, without redundant collect call!

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/suoritusrekisteri/impl/SuoritusrekisteriAsyncResourceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/suoritusrekisteri/impl/SuoritusrekisteriAsyncResourceImpl.java
@@ -110,7 +110,14 @@ public class SuoritusrekisteriAsyncResourceImpl extends UrlConfiguredResource im
         );
 
         // Add the elements returned by each response to one master list
-        return Observable.concat(obses);
+        Observable<List<Oppija>> allOppijas = Observable
+            .concat(obses);
+
+        allOppijas.subscribe(l -> {
+            LOG.info("Finished batched POST with {} results", l.size());
+        });
+
+        return allOppijas;
     }
 
     @Override
