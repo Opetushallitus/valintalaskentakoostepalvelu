e80abccfc693c31f8d491588ad7c364b9f16107a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-11-05 14:07:43 +0200

    VTKU-98 : Lisää toStringit logituksen helpottamiseksi

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/koski/KoskiOppija.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/koski/KoskiOppija.java
@@ -2,6 +2,8 @@ package fi.vm.sade.valinta.kooste.external.resource.koski;
 
 import com.google.gson.JsonArray;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class KoskiOppija {
     private KoskiHenkilö henkilö;
     private JsonArray opiskeluoikeudet;
@@ -16,5 +18,15 @@ public class KoskiOppija {
 
     public static class KoskiHenkilö {
         public String oid;
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
