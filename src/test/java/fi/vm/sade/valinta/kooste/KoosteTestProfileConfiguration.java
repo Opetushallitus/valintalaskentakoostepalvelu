f8206b04c7ca269e4c29ae7b61e580766a978315
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-02-24 11:19:17 +0200

    VTKU-103 : Uudet funktiot Koskifunktio-whitelistiin

--- src/test/java/fi/vm/sade/valinta/kooste/KoosteTestProfileConfiguration.java
+++ src/test/java/fi/vm/sade/valinta/kooste/KoosteTestProfileConfiguration.java
@@ -48,7 +48,7 @@ public class KoosteTestProfileConfiguration {
         p0.setProperty("valintalaskentakoostepalvelu.koski.username", "koostepalvelu2koski");
         p0.setProperty("valintalaskentakoostepalvelu.koski.password", "secret");
         p0.setProperty("valintalaskentakoostepalvelu.koski.max.oppijat.post.size", "1000");
-        p0.setProperty("valintalaskentakoostepalvelu.laskenta.funktionimet.joille.haetaan.tiedot.koskesta", "HAEAMMATILLINENYTOARVOSANA,HAEAMMATILLINENYTOARVIOINTIASTEIKKO");
+        p0.setProperty("valintalaskentakoostepalvelu.laskenta.funktionimet.joille.haetaan.tiedot.koskesta", "HAEAMMATILLINENYTOARVOSANA,HAEAMMATILLINENYTOARVIOINTIASTEIKKO,ITEROIAMMATILLISETTUTKINNOT,ITEROIAMMATILLISETOSAT,ITEROIAMMATILLISETYTOOSAALUEET,HAEAMMATILLISENOSANLAAJUUS,HAEAMMATILLISENOSANARVOSANA,HAEAMMATILLISENYTOOSAALUEENLAAJUUS,HAEAMMATILLISENYTOOSAALUEENARVOSANA,HAEAMMATILLISENTUTKINNONKESKIARVO,HAEAMMATILLISENTUTKINNONSUORITUSTAPA");
 
         p0.setProperty("valintalaskentakoostepalvelu.seuranta.rest.url", "http://localhost");
         p0.setProperty("valintalaskentakoostepalvelu.organisaatioService.rest.url", "http://" + proxyServer + "/organisaatio-service/rest");
