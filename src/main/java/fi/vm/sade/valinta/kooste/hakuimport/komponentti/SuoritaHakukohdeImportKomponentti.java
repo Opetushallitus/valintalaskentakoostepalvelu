a8cc8deed2f5640cca305c48b47fe82c6ce4136d
August Kilponen <august.kilponen@gmail.com>
2022-01-21 12:46:33 +0200

    Spotless formatointia

--- src/main/java/fi/vm/sade/valinta/kooste/hakuimport/komponentti/SuoritaHakukohdeImportKomponentti.java
+++ src/main/java/fi/vm/sade/valinta/kooste/hakuimport/komponentti/SuoritaHakukohdeImportKomponentti.java
@@ -246,7 +246,7 @@ public class SuoritaHakukohdeImportKomponentti {
         v.setTyyppiUri(valintakoe.valintakokeentyyppiUri);
         valintakoeDTOs.add(v);
       }
-      
+
       List<HakukohteenValintakoeDTO> uniqueValintakokeet =
           valintakoeDTOs.stream()
               .collect(Collectors.groupingBy(HakukohteenValintakoeDTO::getTyyppiUri))
