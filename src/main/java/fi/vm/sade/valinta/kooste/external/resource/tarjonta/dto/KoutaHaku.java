9a2d2e66c7bbcdbf18844ff652f4d4e9fe73411c
Mikko Siukola <mikko.siukola@cybercom.com>
2022-03-30 13:32:02 +0300

    HLE-1555 Add fields to koutahakus, parse hakukausiVuosi from hakuaikas

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/tarjonta/dto/KoutaHaku.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/tarjonta/dto/KoutaHaku.java
@@ -1,5 +1,6 @@
 package fi.vm.sade.valinta.kooste.external.resource.tarjonta.dto;
 
+import java.util.List;
 import java.util.Map;
 
 public class KoutaHaku {
@@ -8,6 +9,9 @@ public class KoutaHaku {
   public final String organisaatioOid;
   public final String kohdejoukkoKoodiUri;
   public final String hakulomakeAtaruId;
+  public final List<KoutaHakuaika> hakuajat;
+  public final String alkamiskausiKoodiUri;
+  public final String alkamisvuosi;
 
   private KoutaHaku() {
     this.oid = null;
@@ -15,5 +19,8 @@ public class KoutaHaku {
     this.organisaatioOid = null;
     this.kohdejoukkoKoodiUri = null;
     this.hakulomakeAtaruId = null;
+    this.hakuajat = null;
+    this.alkamiskausiKoodiUri = null;
+    this.alkamisvuosi = null;
   }
 }
