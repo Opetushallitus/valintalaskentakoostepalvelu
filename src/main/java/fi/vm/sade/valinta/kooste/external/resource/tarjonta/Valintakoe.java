5d31cd8a6547e1fa69df36fa98a4abd2e59feaff
Jaakko Ketola <jaakko.ketola@rebase.fi>
2022-04-05 10:23:55 +0300

    Add new fields for KoutaHakukohde

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/tarjonta/Valintakoe.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/tarjonta/Valintakoe.java
@@ -3,18 +3,23 @@ package fi.vm.sade.valinta.kooste.external.resource.tarjonta;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.ValintakoeV1RDTO;
 import fi.vm.sade.valinta.kooste.external.resource.tarjonta.dto.KoutaHakukohde;
 
+import java.math.BigDecimal;
+
 public class Valintakoe {
   public final String id;
   public final String valintakokeentyyppiUri;
+  public final BigDecimal vahimmaispisteet;
 
   public Valintakoe(ValintakoeV1RDTO dto) {
     this.id = dto.getOid();
     this.valintakokeentyyppiUri = dto.getValintakoetyyppi();
+    this.vahimmaispisteet = dto.getPisterajat()
   }
 
   public Valintakoe(KoutaHakukohde.KoutaValintakoe dto) {
     this.id = dto.id;
     this.valintakokeentyyppiUri = dto.tyyppi;
+    this.vahimmaispisteet = dto.vahimmaispisteet;
   }
 
   @Override
