8197bbd8a47d1ffe4bee9f90a0d7c0424e35fb4c
Jaakko Ketola <jaakko.ketola@rebase.fi>
2022-04-14 14:38:42 +0300

    Format code

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/harkinnanvaraisuus/impl/HarkinnanvaraisuusAsyncResourceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/harkinnanvaraisuus/impl/HarkinnanvaraisuusAsyncResourceImpl.java
@@ -144,8 +144,7 @@ public class HarkinnanvaraisuusAsyncResourceImpl implements HarkinnanvaraisuusAs
   }
 
   private void setHarkinnanvaraisuudenSyyForHakutoive(
-      HakutoiveenHarkinnanvaraisuus ht,
-      HarkinnanvaraisuudenSyy syy) {
+      HakutoiveenHarkinnanvaraisuus ht, HarkinnanvaraisuudenSyy syy) {
     if (ht.getHarkinnanvaraisuudenSyy() != HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN_HAKUKOHDE) {
       ht.setHarkinnanvaraisuudenSyy(syy);
     }
@@ -180,7 +179,9 @@ public class HarkinnanvaraisuusAsyncResourceImpl implements HarkinnanvaraisuusAs
           "Hakemus {} on suren mukaan harkinnanvarainen, koska oppijan viimeisin peruskoulusuoritus on keskeytynyt",
           hakemusOid);
       tiedotAtarusta.forEach(
-          ht -> setHarkinnanvaraisuudenSyyForHakutoive(ht, HarkinnanvaraisuudenSyy.SURE_EI_PAATTOTODISTUSTA));
+          ht ->
+              setHarkinnanvaraisuudenSyyForHakutoive(
+                  ht, HarkinnanvaraisuudenSyy.SURE_EI_PAATTOTODISTUSTA));
       result = new HakemuksenHarkinnanvaraisuus(hakemusOid, tiedotAtarusta);
     } else if (LocalDateTime.now().isAfter(suoritusValmisDeadline)
         && !hasValmisPeruskoulu(oppijas)) {
@@ -188,12 +189,15 @@ public class HarkinnanvaraisuusAsyncResourceImpl implements HarkinnanvaraisuusAs
           "Hakemus {} on suren mukaan harkinnanvarainen, koska suressa ei päättötodistusta ja deadline on ohitettu",
           hakemusOid);
       tiedotAtarusta.forEach(
-          ht -> setHarkinnanvaraisuudenSyyForHakutoive(ht, HarkinnanvaraisuudenSyy.SURE_EI_PAATTOTODISTUSTA));
+          ht ->
+              setHarkinnanvaraisuudenSyyForHakutoive(
+                  ht, HarkinnanvaraisuudenSyy.SURE_EI_PAATTOTODISTUSTA));
       result = new HakemuksenHarkinnanvaraisuus(hakemusOid, tiedotAtarusta);
     } else if (hasYksilollistettyMatAi(oppijas)) {
       LOG.info("Hakemuksella {} on suressa yksilollistetty MA_AI!", hakemusOid);
       tiedotAtarusta.forEach(
-          ht -> setHarkinnanvaraisuudenSyyForHakutoive(ht, HarkinnanvaraisuudenSyy.SURE_YKS_MAT_AI));
+          ht ->
+              setHarkinnanvaraisuudenSyyForHakutoive(ht, HarkinnanvaraisuudenSyy.SURE_YKS_MAT_AI));
       result = new HakemuksenHarkinnanvaraisuus(hakemusOid, tiedotAtarusta);
     } else {
       LOG.info(
@@ -211,14 +215,16 @@ public class HarkinnanvaraisuusAsyncResourceImpl implements HarkinnanvaraisuusAs
                     "Hakemuksella {} harkinnanvaraiseksi merkitty hakutoive {} ei ole harkinnanvarainen, koska suresta löytyy valmis pohjakoulutus!",
                     hakemusOid,
                     hh.getHakukohdeOid());
-                setHarkinnanvaraisuudenSyyForHakutoive(hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
+                setHarkinnanvaraisuudenSyyForHakutoive(
+                    hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
               } else if (LocalDateTime.now().isBefore(suoritusValmisDeadline)
                   && hasKeskenPeruskoulu(oppijas)) {
                 LOG.info(
                     "Hakemuksella {} harkinnanvaraiseksi merkitty hakutoive {} ei ole harkinnanvarainen, koska suresta löytyy kesken-tilainen peruskoulusuoritus ja deadlinea ei ole ohitettu!",
                     hakemusOid,
                     hh.getHakukohdeOid());
-                setHarkinnanvaraisuudenSyyForHakutoive(hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
+                setHarkinnanvaraisuudenSyyForHakutoive(
+                    hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
               }
             }
             if (hh.getHarkinnanvaraisuudenSyy().equals(HarkinnanvaraisuudenSyy.ATARU_YKS_MAT_AI)
@@ -228,7 +234,8 @@ public class HarkinnanvaraisuusAsyncResourceImpl implements HarkinnanvaraisuusAs
                       + "tai myöhäisempi suoritus ilman yksilöllistettyä matematiikkaa ja äidinkieltä!",
                   hakemusOid,
                   hh.getHakukohdeOid());
-              setHarkinnanvaraisuudenSyyForHakutoive(hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
+              setHarkinnanvaraisuudenSyyForHakutoive(
+                  hh, HarkinnanvaraisuudenSyy.EI_HARKINNANVARAINEN);
             }
           });
       result = new HakemuksenHarkinnanvaraisuus(hakemusOid, tiedotAtarusta);
