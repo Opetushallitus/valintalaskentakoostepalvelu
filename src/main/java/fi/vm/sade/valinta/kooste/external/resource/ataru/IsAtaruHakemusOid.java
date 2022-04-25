fb48be4c202f863bdf71483935b2d0ac318908e1
August Kilponen <august.kilponen@gmail.com>
2022-04-25 16:07:03 +0300

    Ajettu Spotless

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/ataru/IsAtaruHakemusOid.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/ataru/IsAtaruHakemusOid.java
@@ -2,7 +2,7 @@ package fi.vm.sade.valinta.kooste.external.resource.ataru;
 
 public class IsAtaruHakemusOid {
 
-    public static boolean isAtaruHakemusOid(String oid) {
-        return oid != null && 35 == oid.length();
-    }
+  public static boolean isAtaruHakemusOid(String oid) {
+    return oid != null && 35 == oid.length();
+  }
 }
