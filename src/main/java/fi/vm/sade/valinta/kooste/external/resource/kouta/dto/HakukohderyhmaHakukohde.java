8b60a85599ef509bdeec05132b917702bdff9379
Mikko Siukola <mikko.siukola@cybercom.com>
2022-05-12 11:48:37 +0300

    OY-3358 Fix field name

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/kouta/dto/HakukohderyhmaHakukohde.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/kouta/dto/HakukohderyhmaHakukohde.java
@@ -3,16 +3,16 @@ package fi.vm.sade.valinta.kooste.external.resource.kouta.dto;
 import java.util.List;
 
 public class HakukohderyhmaHakukohde {
-  public String hakukohdeOid;
+  public String oid;
   public List<String> hakukohderyhmat;
 
   public HakukohderyhmaHakukohde(String hakukohdeOid, List<String> hakukohderyhmat) {
-    this.hakukohdeOid = hakukohdeOid;
+    this.oid = hakukohdeOid;
     this.hakukohderyhmat = hakukohderyhmat;
   }
 
   public String getHakukohdeOid() {
-    return hakukohdeOid;
+    return oid;
   }
 
   public List<String> getHakukohderyhmat() {
