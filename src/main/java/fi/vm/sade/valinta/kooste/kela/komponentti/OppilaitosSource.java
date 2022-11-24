f60e8af99a24993dde4b812eb476dbea3cf1f221
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-11-24 22:16:14 +0200

    remove kela routes

--- src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/OppilaitosSource.java
+++
@@ -1,5 +0,0 @@
-package fi.vm.sade.valinta.kooste.kela.komponentti;
-
-public interface OppilaitosSource {
-  String getOppilaitosnumero(String tarjoajaOid);
-}
--- src/main/java/fi/vm/sade/valinta/kooste/kela/komponentti/TutkinnontasoSource.java
+++
@@ -1,9 +0,0 @@
-package fi.vm.sade.valinta.kooste.kela.komponentti;
-
-import fi.vm.sade.organisaatio.resource.api.TasoJaLaajuusDTO;
-
-public interface TutkinnontasoSource {
-  TasoJaLaajuusDTO getTutkinnontaso(String hakukohdeOid);
-
-  String getKoulutusaste(String hakukohdeOid);
-}
