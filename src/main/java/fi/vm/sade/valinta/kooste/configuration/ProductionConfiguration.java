13087102affc0f000354c39c769774f862c94a0e
Santeri Korri <santeri.korri@knowit.fi>
2023-08-22 13:11:01 +0300

    OK-318 Lisätty annotaatiopohjainen Spring Security -konfiguraatio

--- src/main/java/fi/vm/sade/valinta/kooste/configuration/ProductionConfiguration.java
+++ src/main/java/fi/vm/sade/valinta/kooste/configuration/ProductionConfiguration.java
@@ -1,10 +1,8 @@
 package fi.vm.sade.valinta.kooste.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 
 @Profile("default")
 @Configuration
-@ImportResource("/spring/application-context-production.xml")
 public class ProductionConfiguration {}
