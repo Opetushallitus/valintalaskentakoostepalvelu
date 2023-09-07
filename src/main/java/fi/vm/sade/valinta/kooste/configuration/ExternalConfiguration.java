5accd7423a9b1bf46d3b755acd8c8c39a2f33f00
Santeri Korri <santeri.korri@knowit.fi>
2023-09-07 13:10:53 +0300

    OK-318 Poistettu käyttämätöntä service-koodia

--- src/main/java/fi/vm/sade/valinta/kooste/configuration/ExternalConfiguration.java
+++ src/main/java/fi/vm/sade/valinta/kooste/configuration/ExternalConfiguration.java
@@ -2,13 +2,11 @@ package fi.vm.sade.valinta.kooste.configuration;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import fi.vm.sade.javautils.cxf.OphRequestHeadersCxfInterceptor;
-import fi.vm.sade.tarjonta.service.TarjontaPublicService;
 import fi.vm.sade.valinta.kooste.ObjectMapperProvider;
 import fi.vm.sade.valinta.kooste.tarjonta.api.OrganisaatioResource;
 import java.util.*;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactoryBean;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,16 +40,5 @@ public class ExternalConfiguration {
     bean.setProviders(List.of(jacksonJsonProvider, objectMapperProvider));
     bean.setServiceClass(cls);
     return (OrganisaatioResource) bean.create(cls);
-  }
-
-  @Bean
-  public TarjontaPublicService tarjontaPublicService(
-      @Value("${valintalaskentakoostepalvelu.tarjontaService.url}") String address) {
-    Class cls = TarjontaPublicService.class;
-    JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
-    bean.setAddress(address);
-    bean.setOutInterceptors(List.of(requestHeaders));
-    bean.setServiceClass(cls);
-    return (TarjontaPublicService) bean.create(cls);
   }
 }
