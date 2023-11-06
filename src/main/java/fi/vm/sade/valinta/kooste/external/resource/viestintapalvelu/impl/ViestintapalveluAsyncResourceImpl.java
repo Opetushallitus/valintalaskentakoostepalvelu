e93db6988bb31cbc6f6e7b18942c3379675f4661
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-11-06 16:25:45 +0200

    OK-323 debug accept header

--- src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/impl/ViestintapalveluAsyncResourceImpl.java
+++ src/main/java/fi/vm/sade/valinta/kooste/external/resource/viestintapalvelu/impl/ViestintapalveluAsyncResourceImpl.java
@@ -89,7 +89,7 @@ public class ViestintapalveluAsyncResourceImpl implements ViestintapalveluAsyncR
             .post(
                 this.urlConfiguration.url("viestintapalvelu.addresslabel.sync.pdf"),
                 osoitteet,
-                Map.of("Accept", "application/octet-stream, */*;q=0.9"),
+                Map.of("Accept", "*/*"),
                 10 * 60 * 1000 // TODO: mikä on oikea timeout?
                 )
             .thenApply(r -> r.getResponseBodyAsStream()));
