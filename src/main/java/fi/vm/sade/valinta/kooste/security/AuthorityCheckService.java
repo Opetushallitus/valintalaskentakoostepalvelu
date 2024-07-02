fbc7cdb4f5e0f27b7c8c4932a8dac71e71b8b7b2
Mikko Siukola <mikko.siukola@cybercom.com>
2024-07-02 13:23:25 +0300

    OY-4897 Cleanup

--- src/main/java/fi/vm/sade/valinta/kooste/security/AuthorityCheckService.java
+++ src/main/java/fi/vm/sade/valinta/kooste/security/AuthorityCheckService.java
@@ -115,28 +115,6 @@ public class AuthorityCheckService {
     }
   }
 
-  public boolean checkAuthorizationForAnyHakukohdeWithContext(
-      Context context, Collection<String> hakukohdeOids, Collection<String> requiredRoles) {
-    setContext(context);
-
-    Collection<? extends GrantedAuthority> userRoles = getRoles();
-
-    if (containsOphRole(userRoles)) {
-      // on OPH-käyttäjä, ei tarvitse käydä läpi organisaatioita
-      return true;
-    }
-
-    boolean isAuthorized =
-        Observable.fromFuture(getAuthorityCheckForRoles(requiredRoles))
-            .map(authorityCheck -> hakukohdeOids.stream().anyMatch(authorityCheck))
-            .timeout(2, MINUTES)
-            .blockingFirst();
-
-    clearContext();
-
-    return isAuthorized;
-  }
-
   public HakukohdeOIDAuthorityCheck getHakukohdeOidBasedAuthCheck(Context context, Collection<String> requiredRoles) {
     setContext(context);
 
