16df464c354fe98f34b9fd2c792e7fc9168f3267
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2024-07-11 14:13:59 +0300

    OK-450: Uusi koski-rajapinta otettu käyttöön

--- src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/KoskiServiceTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/KoskiServiceTest.java
@@ -7,13 +7,11 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetDTO;
 import fi.vm.sade.service.valintaperusteet.dto.model.Funktionimi;
 import fi.vm.sade.valinta.kooste.external.resource.koski.KoskiAsyncResource;
@@ -24,10 +22,7 @@ import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.apache.commons.io.IOUtils;
@@ -54,12 +49,12 @@ public class KoskiServiceTest {
   private Set<KoskiOppija> koskioppijat = Collections.singleton(koskiOppija);
   private final SuoritustiedotDTO suoritustiedotDTO = new SuoritustiedotDTO();
 
+  private LocalDate paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan = LocalDate.now();
+
   @BeforeEach
   public void setUpTestdata() {
     when(hakemusWrapper.getPersonOid()).thenReturn(oppijanumero);
-    KoskiOppija.KoskiHenkilö koskiHenkilo = new KoskiOppija.KoskiHenkilö();
-    koskiHenkilo.oid = oppijanumero;
-    koskiOppija.setHenkilö(koskiHenkilo);
+    koskiOppija.setOppijaOid(oppijanumero);
     koskiOppija.setOpiskeluoikeudet(
         GSON.fromJson(
             classpathResourceAsString(
@@ -70,15 +65,22 @@ public class KoskiServiceTest {
   @Test
   public void koskestaHaettavienHakukohteidenListallaVoiRajoittaaMilleHakukohteilleHaetaanKoskesta()
       throws ExecutionException, InterruptedException {
+    KoskiOpiskeluoikeusHistoryService koskiOpiskeluoikeusHistoryService =
+        new KoskiOpiskeluoikeusHistoryService();
     KoskiService service =
         new KoskiService(
             "hakukohdeoid1,hakukohdeoid2",
             koskifuntionimet,
             "ammatillinenkoulutus",
-            5,
-            koskiAsyncResource);
+            koskiAsyncResource,
+            koskiOpiskeluoikeusHistoryService);
+    LocalDate paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan =
+        koskiOpiskeluoikeusHistoryService.etsiKoskiDatanLeikkuriPvm(
+            koskiFunktionSisaltavaValintaperuste.join(), "jokumuuhakukohde");
 
-    when(koskiAsyncResource.findKoskiOppijat(Collections.singletonList(oppijanumero)))
+    when(koskiAsyncResource.findKoskiOppijat(
+            Collections.singletonList(oppijanumero),
+            paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan))
         .thenReturn(CompletableFuture.completedFuture(koskioppijat));
 
     Map<String, KoskiOppija> koskiOppijatOppijanumeroittain =
@@ -114,10 +116,22 @@ public class KoskiServiceTest {
   public void
       koskidataaKayttavienFunktionimienListallaVoiRajoittaaMilleHakukohteilleHaetaanKoskesta()
           throws ExecutionException, InterruptedException {
+    KoskiOpiskeluoikeusHistoryService koskiOpiskeluoikeusHistoryService =
+        new KoskiOpiskeluoikeusHistoryService();
     KoskiService service =
-        new KoskiService("ALL", koskifuntionimet, "ammatillinenkoulutus", 5, koskiAsyncResource);
+        new KoskiService(
+            "ALL",
+            koskifuntionimet,
+            "ammatillinenkoulutus",
+            koskiAsyncResource,
+            koskiOpiskeluoikeusHistoryService);
 
-    when(koskiAsyncResource.findKoskiOppijat(Collections.singletonList(oppijanumero)))
+    LocalDate paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan =
+        koskiOpiskeluoikeusHistoryService.etsiKoskiDatanLeikkuriPvm(
+            koskiFunktionSisaltavaValintaperuste.join(), "hakukohdeoid1");
+    when(koskiAsyncResource.findKoskiOppijat(
+            Collections.singletonList(oppijanumero),
+            paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan))
         .thenReturn(CompletableFuture.completedFuture(koskioppijat));
 
     Map<String, KoskiOppija> koskiOppijatOppijanumeroittain =
@@ -150,8 +164,15 @@ public class KoskiServiceTest {
       throws ExecutionException, InterruptedException {
     final java.util.Date eilinen =
         Date.from(LocalDate.now().minusDays(1).atStartOfDay().toInstant(ZoneOffset.UTC));
+    KoskiOpiskeluoikeusHistoryService koskiOpiskeluoikeusHistoryService =
+        new KoskiOpiskeluoikeusHistoryService();
     KoskiService service =
-        new KoskiService("ALL", koskifuntionimet, "ammatillinenkoulutus", 5, koskiAsyncResource);
+        new KoskiService(
+            "ALL",
+            koskifuntionimet,
+            "ammatillinenkoulutus",
+            koskiAsyncResource,
+            koskiOpiskeluoikeusHistoryService);
     koskiFunktionSisaltavaValintaperuste
         .get()
         .forEach(
@@ -161,7 +182,13 @@ public class KoskiServiceTest {
                     .getValintatapajono()
                     .forEach(jono -> jono.setEiLasketaPaivamaaranJalkeen(eilinen)));
 
-    when(koskiAsyncResource.findKoskiOppijat(Collections.singletonList(oppijanumero)))
+    LocalDate paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan =
+        koskiOpiskeluoikeusHistoryService.etsiKoskiDatanLeikkuriPvm(
+            koskiFunktionSisaltavaValintaperuste.join(), "hakukohdeoid1");
+
+    when(koskiAsyncResource.findKoskiOppijat(
+            Collections.singletonList(oppijanumero),
+            paivaJonkaMukaisiaTietojaKoskiDatastaKaytetaan))
         .thenReturn(
             CompletableFuture.failedFuture(new RuntimeException("Tätähän ei pitänyt kutsua!")));
 
@@ -177,107 +204,6 @@ public class KoskiServiceTest {
             .entrySet(),
         is(empty()));
 
-    verifyNoMoreInteractions(koskiAsyncResource);
-  }
-
-  @Test
-  public void josOpiskeluoikeudenTiedotOnPaivitettyLeikkuriPvmJalkeenHaetaanRiittävänVanhaVersio()
-      throws ExecutionException, InterruptedException {
-    KoskiService service =
-        new KoskiService("ALL", koskifuntionimet, "ammatillinenkoulutus", 5, koskiAsyncResource);
-
-    when(koskiAsyncResource.findKoskiOppijat(Collections.singletonList(oppijanumero)))
-        .thenReturn(CompletableFuture.completedFuture(koskioppijat));
-    when(koskiAsyncResource.findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 2))
-        .thenReturn(
-            CompletableFuture.completedFuture(
-                GSON.fromJson(
-                    classpathResourceAsString(
-                        "fi/vm/sade/valinta/kooste/valintalaskenta/opiskeluoikeus-1.2.246.562.15.12442534343-versio-2019-10-05.json"),
-                    JsonElement.class)));
-    when(koskiAsyncResource.findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 1))
-        .thenReturn(
-            CompletableFuture.completedFuture(
-                GSON.fromJson(
-                    classpathResourceAsString(
-                        "fi/vm/sade/valinta/kooste/valintalaskenta/opiskeluoikeus-1.2.246.562.15.12442534343-versio-2019-10-01.json"),
-                    JsonElement.class)));
-
-    CompletableFuture<List<ValintaperusteetDTO>> valintaperuste =
-        koskiDataaKayttavaValintaperuste("1.10.2019");
-    Map<String, KoskiOppija> koskiOppijatOppijanumeroittain =
-        service
-            .haeKoskiOppijat("hakukohdeoid1", valintaperuste, hakemukset, suoritustiedotDTO, NYT)
-            .get();
-    assertThat(koskiOppijatOppijanumeroittain.entrySet(), hasSize(1));
-    assertEquals(koskiOppijatOppijanumeroittain.get(oppijanumero), koskiOppija);
-    assertTrue(suoritustiedotDTO.onKoskiopiskeluoikeudet(oppijanumero));
-    assertEquals(
-        GSON.toJson(koskiOppija.getOpiskeluoikeudet()),
-        suoritustiedotDTO.haeKoskiOpiskeluoikeudetJson(oppijanumero));
-
-    assertThat(
-        service
-            .haeKoskiOppijat(
-                "hakukohdeoid1", kosketonValintaperuste, hakemukset, suoritustiedotDTO, NYT)
-            .get()
-            .entrySet(),
-        is(empty()));
-
-    verify(koskiAsyncResource).findKoskiOppijat(Collections.singletonList(oppijanumero));
-    verify(koskiAsyncResource).findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 2);
-    verify(koskiAsyncResource).findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 1);
-    verifyNoMoreInteractions(koskiAsyncResource);
-  }
-
-  @Test
-  public void
-      josOpiskeluoikeudenVanhinVersioOnLeikkuriPvmJalkeenJatetaanopiskeluoikeusHuomioimatta()
-          throws ExecutionException, InterruptedException {
-    KoskiService service =
-        new KoskiService("ALL", koskifuntionimet, "ammatillinenkoulutus", 5, koskiAsyncResource);
-
-    when(koskiAsyncResource.findKoskiOppijat(Collections.singletonList(oppijanumero)))
-        .thenReturn(CompletableFuture.completedFuture(koskioppijat));
-    when(koskiAsyncResource.findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 2))
-        .thenReturn(
-            CompletableFuture.completedFuture(
-                GSON.fromJson(
-                    classpathResourceAsString(
-                        "fi/vm/sade/valinta/kooste/valintalaskenta/opiskeluoikeus-1.2.246.562.15.12442534343-versio-2019-10-05.json"),
-                    JsonElement.class)));
-    when(koskiAsyncResource.findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 1))
-        .thenReturn(
-            CompletableFuture.completedFuture(
-                GSON.fromJson(
-                    classpathResourceAsString(
-                        "fi/vm/sade/valinta/kooste/valintalaskenta/opiskeluoikeus-1.2.246.562.15.12442534343-versio-2019-10-01.json"),
-                    JsonElement.class)));
-
-    CompletableFuture<List<ValintaperusteetDTO>> valintaperuste =
-        koskiDataaKayttavaValintaperuste("30.9.2019");
-    Map<String, KoskiOppija> koskiOppijatOppijanumeroittain =
-        service
-            .haeKoskiOppijat("hakukohdeoid1", valintaperuste, hakemukset, suoritustiedotDTO, NYT)
-            .get();
-    assertThat(koskiOppijatOppijanumeroittain.entrySet(), hasSize(1));
-    assertEquals(koskiOppijatOppijanumeroittain.get(oppijanumero), koskiOppija);
-    assertTrue(suoritustiedotDTO.onKoskiopiskeluoikeudet(oppijanumero));
-    assertEquals(
-        GSON.toJson(koskiOppija.getOpiskeluoikeudet()),
-        suoritustiedotDTO.haeKoskiOpiskeluoikeudetJson(oppijanumero));
-
-    assertThat(
-        service
-            .haeKoskiOppijat(
-                "hakukohdeoid1", kosketonValintaperuste, hakemukset, suoritustiedotDTO, NYT)
-            .get()
-            .entrySet(),
-        is(empty()));
-
-    verify(koskiAsyncResource).findKoskiOppijat(Collections.singletonList(oppijanumero));
-    verify(koskiAsyncResource).findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 2);
-    verify(koskiAsyncResource).findVersionOfOpiskeluoikeus("1.2.246.562.15.12442534343", 1);
     verifyNoMoreInteractions(koskiAsyncResource);
   }
 
