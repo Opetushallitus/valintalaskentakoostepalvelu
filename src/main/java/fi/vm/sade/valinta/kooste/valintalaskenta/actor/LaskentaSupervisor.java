59290996a41662b2ff689ba5bb594d6b6e4e4c6a
Santeri Korri <santeri.korri@knowit.fi>
2024-10-28 18:10:20 +0200

    OK-611 Poistettu valintalaskennan käynnistämiseen liittyvä toiminnallisuus

--- src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/LaskentaSupervisor.java
+++
@@ -1,9 +0,0 @@
-package fi.vm.sade.valinta.kooste.valintalaskenta.actor;
-
-import fi.vm.sade.valinta.kooste.valintalaskenta.route.ValintalaskentaKerrallaRouteValvomo;
-
-public interface LaskentaSupervisor extends ValintalaskentaKerrallaRouteValvomo {
-  void ready(String uuid);
-
-  void fetchAndStartLaskenta();
-}
