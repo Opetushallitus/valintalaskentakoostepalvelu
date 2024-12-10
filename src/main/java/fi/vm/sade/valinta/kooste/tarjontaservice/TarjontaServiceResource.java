91bfe50d226af71f36e425c932d6b1ae69a46d3d
August Kilponen <august.kilponen@gmail.com>
2024-12-10 12:50:01 +0200

    Lisätty vielä yksi tarjonnan proxy-rajapinta suoritusrekisterille

--- src/main/java/fi/vm/sade/valinta/kooste/tarjontaservice/TarjontaServiceResource.java
+++ src/main/java/fi/vm/sade/valinta/kooste/tarjontaservice/TarjontaServiceResource.java
@@ -2,6 +2,9 @@ package fi.vm.sade.valinta.kooste.tarjontaservice;
 
 import com.google.gson.reflect.TypeToken;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.*;
+import fi.vm.sade.tarjonta.service.types.KoulutusasteTyyppi;
+import fi.vm.sade.tarjonta.service.types.KoulutusmoduuliTyyppi;
+import fi.vm.sade.tarjonta.shared.types.ToteutustyyppiEnum;
 import fi.vm.sade.valinta.kooste.external.resource.HttpClient;
 import fi.vm.sade.valinta.kooste.url.UrlConfiguration;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +14,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.core.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -130,5 +136,68 @@ public class TarjontaServiceResource {
         urlConfiguration.url("tarjonta-service.haku.find") + "?" + queryString,
         Duration.ofMinutes(5),
         new com.google.gson.reflect.TypeToken<ResultV1RDTO<List<HakuV1RDTO>>>() {}.getType());
+  }
+
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
+  @Operation(summary = "Palauttaa listan hakukohteista annetuilla parametreilla", description = "Palauttaa listan hakukohteista annetuilla parametreilla.")
+  public CompletableFuture<ResultV1RDTO<HakutuloksetV1RDTO<HakukohdeHakutulosV1RDTO>>> search(
+          @RequestParam(value ="searchTerms", required = false) String searchTerms,
+          @RequestParam(value ="hakukohteenNimiUri", required = false) String hakukohteenNimiUri,
+          @RequestParam(value ="organisationOid", required = false) List<String> organisationOids,
+          @RequestParam(value ="tila", required = false) List<String> hakukohdeTilas,
+          @RequestParam(value ="alkamisKausi", required = false) String alkamisKausi,
+          @RequestParam(value ="alkamisVuosi", required = false) Integer alkamisVuosi,
+          @RequestParam(value ="hakukohdeOid", required = false) String hakukohdeOid,
+          @RequestParam(value ="koulutusastetyyppi", required = false) List<KoulutusasteTyyppi> koulutusastetyyppi,
+          @RequestParam(value ="hakuOid", required = false) String hakuOid,
+          @RequestParam(value ="organisaatioRyhmaOid", required = false) List<String> organisaatioRyhmaOid,
+          @RequestParam(value ="koulutustyyppi", required = false) List<ToteutustyyppiEnum> koulutustyypit,
+          @RequestParam(value ="koulutusmoduuliTyyppi", required = false) List<KoulutusmoduuliTyyppi> koulutusmoduulityypit,
+          @RequestParam(value ="defaultTarjoaja", required = false) String defaultTarjoaja,
+          @RequestParam(value ="hakutapa", required = false) String hakutapa,
+          @RequestParam(value ="hakutyyppi", required = false) String hakutyyppi,
+          @RequestParam(value ="koulutuslaji", required = false) String koulutuslaji,
+          @RequestParam(value ="kohdejoukko", required = false) String kohdejoukko,
+          @RequestParam(value ="oppilaitostyyppi", required = false) String oppilaitostyyppi,
+          @RequestParam(value ="kunta", required = false) String kunta,
+          @RequestParam(value ="opetuskielet", required = false) List<String> opetuskielet,
+          @RequestParam(value ="koulutusOid", required = false) List<String> koulutusOids,
+          @RequestParam(value ="offset", required = false) Integer offset,
+          @RequestParam(value ="limit", required = false) Integer limit) {
+
+    String queryString =
+            UriComponentsBuilder.newInstance()
+                    .queryParamIfPresent("searchTerms", Optional.ofNullable(searchTerms))
+                    .queryParamIfPresent("hakukohteenNimiUri", Optional.ofNullable(hakukohteenNimiUri))
+                    .queryParamIfPresent("organisationOid", Optional.ofNullable(organisationOids))
+                    .queryParamIfPresent("tila", Optional.ofNullable(hakukohdeTilas))
+                    .queryParamIfPresent("alkamisKausi", Optional.ofNullable(alkamisKausi))
+                    .queryParamIfPresent("alkamisVuosi", Optional.ofNullable(alkamisVuosi))
+                    .queryParamIfPresent("hakukohdeOid", Optional.ofNullable(hakukohdeOid))
+                    .queryParamIfPresent("koulutusastetyyppi", Optional.ofNullable(koulutusastetyyppi))
+                    .queryParamIfPresent("hakuOid", Optional.ofNullable(hakuOid))
+                    .queryParamIfPresent("organisaatioRyhmaOid", Optional.ofNullable(organisaatioRyhmaOid))
+                    .queryParamIfPresent("koulutustyyppi", Optional.ofNullable(koulutustyypit))
+                    .queryParamIfPresent("koulutusmoduuliTyyppi", Optional.ofNullable(koulutusmoduulityypit))
+                    .queryParamIfPresent("defaultTarjoaja", Optional.ofNullable(defaultTarjoaja))
+                    .queryParamIfPresent("hakutapa", Optional.ofNullable(hakutapa))
+                    .queryParamIfPresent("hakutyyppi", Optional.ofNullable(hakutyyppi))
+                    .queryParamIfPresent("koulutuslaji", Optional.ofNullable(koulutuslaji))
+                    .queryParamIfPresent("kohdejoukko", Optional.ofNullable(kohdejoukko))
+                    .queryParamIfPresent("oppilaitostyyppi", Optional.ofNullable(oppilaitostyyppi))
+                    .queryParamIfPresent("kunta", Optional.ofNullable(kunta))
+                    .queryParamIfPresent("opetuskielet", Optional.ofNullable(opetuskielet))
+                    .queryParamIfPresent("koulutusOid", Optional.ofNullable(koulutusOids))
+                    .queryParamIfPresent("offset", Optional.ofNullable(offset))
+                    .queryParamIfPresent("limit", Optional.ofNullable(limit))
+                    .build()
+                    .toUri()
+                    .getQuery();
+
+    return this.tarjontaClient.<ResultV1RDTO<HakutuloksetV1RDTO<HakukohdeHakutulosV1RDTO>>>getJson(
+            urlConfiguration.url("tarjonta-service.haku.find") + "?" + queryString,
+            Duration.ofMinutes(5),
+            new com.google.gson.reflect.TypeToken<ResultV1RDTO<HakutuloksetV1RDTO<HakukohdeHakutulosV1RDTO>>>() {}.getType());
+
   }
 }
