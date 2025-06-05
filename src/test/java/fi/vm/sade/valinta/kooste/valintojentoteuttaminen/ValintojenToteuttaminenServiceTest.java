b6b053c4787e9c610e6a7562898b08e37c2be567
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-06-05 16:27:10 +0300

    OK-986: Poistettu varasijatäytön päättymisen tarkistaminen ohjausparametreja käyttäen
    - Jos valintaperusteista ei palaudu hakukohteen tietoja, varasijatäytön päättymistä ei voida päätellä oikein ilman kaikkien hakukohteiden tietojen noutamista. Palautetaan pelkkä valintaperusteista tuleva varasijatäytön päättymistieto ja päätellään lopullinen päivämäärä käyttöliittymässä ohjausparametrien avulla.

--- src/test/java/fi/vm/sade/valinta/kooste/valintojentoteuttaminen/ValintojenToteuttaminenServiceTest.java
+++ src/test/java/fi/vm/sade/valinta/kooste/valintojentoteuttaminen/ValintojenToteuttaminenServiceTest.java
@@ -7,8 +7,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import fi.vm.sade.service.valintaperusteet.dto.HakukohdeKoosteTietoDTO;
-import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
-import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.impl.OhjausparametritAsyncResourceImpl;
 import fi.vm.sade.valinta.kooste.external.resource.valintalaskenta.ValintalaskentaAsyncResource;
 import fi.vm.sade.valinta.kooste.external.resource.valintalaskenta.impl.ValintalaskentaAsyncResourceImpl;
 import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.impl.ValintaperusteetAsyncResourceImpl;
@@ -30,54 +28,32 @@ public class ValintojenToteuttaminenServiceTest {
   private final ValintaperusteetAsyncResourceImpl valintaperusteetAsyncResource =
       mock(ValintaperusteetAsyncResourceImpl.class);
 
-  private final OhjausparametritAsyncResourceImpl ohjausparametritAsyncResource =
-      mock(OhjausparametritAsyncResourceImpl.class);
-
   private final ValintojenToteuttaminenService service =
       new ValintojenToteuttaminenServiceImpl(
-          valintaperusteetAsyncResource,
-          valintalaskentaAsyncResource,
-          ohjausparametritAsyncResource);
+          valintaperusteetAsyncResource, valintalaskentaAsyncResource);
 
   private static final String HAKU_OID = "123.123.123.123";
   private static final String HAKUKOHDE_OID_1 = "123.123.123.111";
   private static final String HAKUKOHDE_OID_2 = "123.123.123.222";
-
-  private static Date getDate() {
-    try {
-      return new SimpleDateFormat("dd.MM.yyyy HH:mm").parse("31.01.2020 00:00");
-    } catch (ParseException e) {
-      System.out.println(e.getMessage());
-      return new Date();
-    }
-  }
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
   @Before
-  public void setUp() {
-    Date date = getDate();
+  public void setUp() throws ParseException {
+    Date varasijatayttoPaattyy = dateFormat.parse("31.01.2020 00:00");
     HakukohdeKoosteTietoDTO hakukohdeKooste =
-        new HakukohdeKoosteTietoDTO(HAKUKOHDE_OID_2, true, date);
+        new HakukohdeKoosteTietoDTO(HAKUKOHDE_OID_2, true, varasijatayttoPaattyy);
 
     when(valintalaskentaAsyncResource.hakukohteidenLaskennanTila(eq(HAKU_OID)))
         .thenReturn(
             CompletableFuture.completedFuture(
-                List.of(
-                    new HakukohdeLaskentaTehty(HAKUKOHDE_OID_1, date),
-                    new HakukohdeLaskentaTehty(HAKUKOHDE_OID_2, date))));
-    when(valintalaskentaAsyncResource.hakukohteidenLaskennanTila(eq(HAKU_OID)))
-        .thenReturn(
-            CompletableFuture.completedFuture(
-                List.of(new HakukohdeLaskentaTehty(HAKUKOHDE_OID_1, date))));
-
-    when(ohjausparametritAsyncResource.haeHaunOhjausparametrit(eq(HAKU_OID)))
-        .thenReturn(CompletableFuture.completedFuture(new ParametritDTO()));
+                List.of(new HakukohdeLaskentaTehty(HAKUKOHDE_OID_1, new Date()))));
 
     when(valintaperusteetAsyncResource.haunHakukohdeTiedot(eq(HAKU_OID)))
         .thenReturn(CompletableFuture.completedFuture(List.of(hakukohdeKooste)));
   }
 
   @Test
-  public void fetchesValintatiedotHakukohteittain() {
+  public void fetchesValintatiedotHakukohteittain() throws ParseException {
     Map<String, HakukohteenValintatiedot> result =
         service.valintatiedotHakukohteittain(HAKU_OID).join();
     assertNotNull(result);
@@ -86,7 +62,8 @@ public class ValintojenToteuttaminenServiceTest {
         new HakukohteenValintatiedot(HAKUKOHDE_OID_1, false, true, null),
         result.get(HAKUKOHDE_OID_1));
     assertEquals(
-        new HakukohteenValintatiedot(HAKUKOHDE_OID_2, true, false, getDate()),
+        new HakukohteenValintatiedot(
+            HAKUKOHDE_OID_2, true, false, dateFormat.parse("31.01.2020 00:00")),
         result.get(HAKUKOHDE_OID_2));
   }
 }
